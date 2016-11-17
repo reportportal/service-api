43aa8dd29668ea055f2c867d83884c14a37d309e
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2016-11-17 15:26:17 +0300

    update project builder

--- src/main/java/com/epam/ta/reportportal/ws/converter/builders/ProjectBuilder.java
+++ src/main/java/com/epam/ta/reportportal/ws/converter/builders/ProjectBuilder.java
@@ -17,17 +17,9 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
- */ 
+ */
 
 package com.epam.ta.reportportal.ws.converter.builders;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
 
 import com.epam.ta.reportportal.commons.SendCase;
 import com.epam.ta.reportportal.database.entity.Project;
@@ -37,17 +29,25 @@ import com.epam.ta.reportportal.database.entity.project.EntryType;
 import com.epam.ta.reportportal.database.entity.project.InterruptionJobDelay;
 import com.epam.ta.reportportal.database.entity.project.KeepLogsDelay;
 import com.epam.ta.reportportal.database.entity.project.KeepScreenshotsDelay;
+import com.epam.ta.reportportal.exception.ReportPortalException;
+import com.epam.ta.reportportal.ws.model.ErrorType;
 import com.epam.ta.reportportal.ws.model.project.CreateProjectRQ;
 import com.epam.ta.reportportal.ws.model.project.email.EmailSenderCase;
 import com.epam.ta.reportportal.ws.model.project.email.ProjectEmailConfig;
-import com.google.common.collect.Lists;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Optional;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static java.util.Collections.emptyList;
 
 /**
  * New project object builder
- * 
+ *
  * @author Andrei_Ramanchuk
  */
 @Service
@@ -72,8 +72,10 @@ public class ProjectBuilder extends Builder<Project> {
 			getObject().getConfiguration().setKeepLogs(KeepLogsDelay.THREE_MONTHS.getValue());
 			getObject().getConfiguration().setKeepScreenshots(KeepScreenshotsDelay.TWO_WEEKS.getValue());
 			getObject().getConfiguration().setIsAutoAnalyzerEnabled(false);
-			getObject().getConfiguration().setStatisticsCalculationStrategy(StatisticsCalculationStrategy
-					.fromString(createProjectRQ.getStatsCalculationStrategy()).orElse(StatisticsCalculationStrategy.STEP_BASED));
+			getObject().getConfiguration().setStatisticsCalculationStrategy(
+					Optional.of(createProjectRQ.getStatsCalculationStrategy()).flatMap(StatisticsCalculationStrategy::fromString)
+							.orElseThrow(
+									() -> new ReportPortalException(ErrorType.PROJECT_NOT_CONFIGURED, createProjectRQ.getProjectName())));
 
 			// Email settings by default
 			EmailSenderCase defaultOne = new EmailSenderCase(newArrayList("OWNER"), SendCase.ALWAYS.name(), emptyList(), emptyList());

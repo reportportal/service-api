339c2089f710ef9d4e26b63e47002168054a5db8
Andrei Varabyeu <andrei_varabyeu@epam.com>
2016-11-17 22:05:57 +0300

    add migration scripts support

---
+++ src/main/java/com/epam/ta/reportportal/migration/ChangeSets_2_7.java
@@ -0,0 +1,19 @@
+package com.epam.ta.reportportal.migration;
+
+import com.github.mongobee.changeset.ChangeLog;
+import com.github.mongobee.changeset.ChangeSet;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+/**
+ * v.2.7 Migration scripts
+ *
+ * @author Andrei Varabyeu
+ */
+@ChangeLog(order = "2.7")
+public class ChangeSets_2_7 {
+
+//    @ChangeSet(order = "2.7-1", id = "v2.7-1-Description", author = "avarabyeu")
+//    public void initLoad(MongoTemplate mongoTemplate) {
+//        //...
+//    }
+}

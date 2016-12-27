f3b7c4f8036df5f1b691923cb79d26701308fa7c
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2016-12-27 16:57:48 +0300

    fix findbug issues

--- src/main/java/com/epam/ta/reportportal/ws/converter/OAuthDetailsConverters.java
+++ src/main/java/com/epam/ta/reportportal/ws/converter/OAuthDetailsConverters.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2016 EPAM Systems
+ *
+ *
+ * This file is part of EPAM Report Portal.
+ * https://github.com/reportportal/service-api
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.epam.ta.reportportal.ws.converter;
 
 import com.epam.ta.reportportal.database.entity.OAuth2LoginDetails;
@@ -13,7 +34,7 @@ import java.util.function.Function;
  */
 public class OAuthDetailsConverters {
 
-	public static Function<OAuthDetailsResource, OAuth2LoginDetails> FROM_RESOURCE = resource -> {
+	public final static Function<OAuthDetailsResource, OAuth2LoginDetails> FROM_RESOURCE = resource -> {
 		OAuth2LoginDetails db = new OAuth2LoginDetails();
 		db.setClientAuthenticationScheme(resource.getClientAuthenticationScheme());
 		db.setUserAuthorizationUri(resource.getUserAuthorizationUri());
@@ -28,7 +49,7 @@ public class OAuthDetailsConverters {
 		return db;
 	};
 
-	public static Function<OAuth2LoginDetails, OAuthDetailsResource> TO_RESOURCE = db -> {
+	public final static Function<OAuth2LoginDetails, OAuthDetailsResource> TO_RESOURCE = db -> {
 		OAuthDetailsResource resource = new OAuthDetailsResource();
 		resource.setClientAuthenticationScheme(db.getClientAuthenticationScheme());
 		resource.setUserAuthorizationUri(db.getUserAuthorizationUri());

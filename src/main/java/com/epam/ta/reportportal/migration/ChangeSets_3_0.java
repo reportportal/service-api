9bb9af61cda0bc651be0356a128ca9d102eb1a4b
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2017-02-28 11:25:12 +0300

    add update script

--- src/main/java/com/epam/ta/reportportal/migration/ChangeSets_3_0.java
+++ src/main/java/com/epam/ta/reportportal/migration/ChangeSets_3_0.java
@@ -41,4 +41,9 @@ public class ChangeSets_3_0 {
 	public void useStepBasedCalcStrategy(MongoTemplate mongoTemplate) throws IOException {
 		mongoTemplate.getDb().doEval(asCharSource(getResource("migration/v3_0_0.js"), Charsets.UTF_8).read());
 	}
+
+	@ChangeSet(order = "3.0.0-2", id = "v3.0.0-Drop Favorites Resources collection", author = "avarabyeu")
+	public void dropFavoritesCollection(MongoTemplate mongoTemplate) throws IOException {
+		mongoTemplate.getDb().doEval(asCharSource(getResource("migration/v3_0_0-2.js"), Charsets.UTF_8).read());
+	}
 }

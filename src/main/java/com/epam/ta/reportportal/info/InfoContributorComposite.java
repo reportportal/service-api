2cf32368927b0ec0565da926cf24cb967ffc0cda
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-03-07 13:20:31 +0300

    added license info

--- src/main/java/com/epam/ta/reportportal/info/InfoContributorComposite.java
+++ src/main/java/com/epam/ta/reportportal/info/InfoContributorComposite.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2017 EPAM Systems
+ *
+ *
+ * This file is part of EPAM Report Portal.
+ * https://github.com/reportportal/service-api
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.epam.ta.reportportal.info;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +28,10 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+/**
+ * @author Pavel Bortnik
+ */
 
 @Component
 public class InfoContributorComposite implements InfoContributor {
@@ -22,7 +46,7 @@ public class InfoContributorComposite implements InfoContributor {
     @Override
     public void contribute(Info.Builder builder) {
         builder.withDetail("extensions", infoContributors.stream()
-                        .map(ExtensionContributor::contribute).flatMap(map -> map.entrySet().stream())
-                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+                .map(ExtensionContributor::contribute).flatMap(map -> map.entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
     }
 }

7805da2fb01552e8fdf64e1e65c7e896f33c6cd1
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2017-04-21 14:24:34 +0300

    update registration template

--- src/main/java/com/epam/ta/reportportal/util/email/EmailService.java
+++ src/main/java/com/epam/ta/reportportal/util/email/EmailService.java
@@ -28,6 +28,7 @@ import com.epam.ta.reportportal.database.entity.statistics.StatisticSubType;
 import com.epam.ta.reportportal.database.entity.user.UserUtils;
 import com.epam.ta.reportportal.ws.model.user.CreateUserRQFull;
 import com.google.common.base.Strings;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -53,216 +54,218 @@ import static com.epam.ta.reportportal.commons.EntityUtils.normalizeUsername;
  */
 public class EmailService extends JavaMailSenderImpl {
 
-    private static final String FINISH_LAUNCH_EMAIL_SUBJECT = " Report Portal Notification: launch '%s' #%s finished";
-    private static final String LOGO = "templates/email/rp_io_logo.png";
-    private TemplateEngine templateEngine;
+	private static final String FINISH_LAUNCH_EMAIL_SUBJECT = " Report Portal Notification: launch '%s' #%s finished";
+	private static final String EMAIL_TEMPLATE_PREFIX = "templates/email/";
+	private static final String LOGO = "templates/email/rp_io_logo.png";
+	private TemplateEngine templateEngine;
 
-    /*
-     * Static email FROM field for server level notifications. Put in ext.
-     * config?
-     */
-    private static final String RP_EMAIL = "ReportPortal@service.com";
+	/*
+	 * Static email FROM field for server level notifications. Put in ext.
+	 * config?
+	 */
+	private static final String RP_EMAIL = "ReportPortal@service.com";
 
-    /* Default value for FROM project notifications field */
-    private String from;
+	/* Default value for FROM project notifications field */
+	private String from;
 
-    public EmailService(Properties javaMailProperties) {
-        super.setJavaMailProperties(javaMailProperties);
-    }
+	public EmailService(Properties javaMailProperties) {
+		super.setJavaMailProperties(javaMailProperties);
+	}
 
-    /**
-     * User creation confirmation email
-     *
-     * @param subject    Letter's subject
-     * @param recipients Letter's recipients
-     * @param url        ReportPortal URL
-     */
-    public void sendConfirmationEmail(final String subject, final String[] recipients, final String url) {
-        MimeMessagePreparator preparator = mimeMessage -> {
-            URL logoImg = this.getClass().getClassLoader().getResource(LOGO);
-            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "utf-8");
-            message.setSubject(subject);
-            message.setTo(recipients);
-            setFrom(message);
+	/**
+	 * User creation confirmation email
+	 *
+	 * @param subject    Letter's subject
+	 * @param recipients Letter's recipients
+	 * @param url        ReportPortal URL
+	 */
+	public void sendConfirmationEmail(final String subject, final String[] recipients, final String url) {
+		MimeMessagePreparator preparator = mimeMessage -> {
+			URL logoImg = this.getClass().getClassLoader().getResource(LOGO);
+			MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "utf-8");
+			message.setSubject(subject);
+			message.setTo(recipients);
+			setFrom(message);
 
-            Map<String, Object> email = new HashMap<>();
-            email.put("url", url);
-            String text = templateEngine.merge("registration-template.ftl", email);
-            message.setText(text, true);
+			Map<String, Object> email = new HashMap<>();
+			email.put("url", url);
+			String text = templateEngine.merge("registration-template.ftl", email);
+			message.setText(text, true);
 
-            message.addInline("rp_io_logo.png", new UrlResource("rp_io_logo.png"));
-            message.addInline("create-user.png", new UrlResource("create-user.png"));
+			message.addInline("rp_io_logo.png", emailTemplateResource("rp_io_logo.png"));
+			message.addInline("create-user.png", emailTemplateResource("create-user.png"));
 
-            message.addInline("ic-github.png", new UrlResource("ic-github.png"));
-            message.addInline("ic-fb.png", new UrlResource("ic-fb.png"));
-            message.addInline("ic-twitter.png", new UrlResource("ic-twitter.png"));
-            message.addInline("ic-vk.png", new UrlResource("ic-vk.png"));
-        };
-        this.send(preparator);
-    }
+			message.addInline("ic-github.png", emailTemplateResource("ic-github.png"));
+			message.addInline("ic-fb.png", emailTemplateResource("ic-fb.png"));
+			message.addInline("ic-twitter.png", emailTemplateResource("ic-twitter.png"));
+			message.addInline("ic-vk.png", emailTemplateResource("ic-vk.png"));
+		};
+		this.send(preparator);
+	}
 
-    /**
-     * Finish launch notification
-     *
-     * @param recipients List of recipients
-     * @param url        ReportPortal URL
-     * @param launch     Launch
-     */
-    public void sendLaunchFinishNotification(final String[] recipients, final String url, final Launch launch,
-            final Project.Configuration settings) {
-        String subject = String.format(FINISH_LAUNCH_EMAIL_SUBJECT, launch.getName(), launch.getNumber());
-        MimeMessagePreparator preparator = mimeMessage -> {
-            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "utf-8");
-            message.setSubject(subject);
-            message.setTo(recipients);
-            setFrom(message);
+	/**
+	 * Finish launch notification
+	 *
+	 * @param recipients List of recipients
+	 * @param url        ReportPortal URL
+	 * @param launch     Launch
+	 */
+	public void sendLaunchFinishNotification(final String[] recipients, final String url, final Launch launch,
+			final Project.Configuration settings) {
+		String subject = String.format(FINISH_LAUNCH_EMAIL_SUBJECT, launch.getName(), launch.getNumber());
+		MimeMessagePreparator preparator = mimeMessage -> {
+			MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "utf-8");
+			message.setSubject(subject);
+			message.setTo(recipients);
+			setFrom(message);
 
-            Map<String, Object> email = new HashMap<>();
-            /* Email fields values */
-            email.put("name", launch.getName());
-            email.put("number", String.valueOf(launch.getNumber()));
-            email.put("description", launch.getDescription());
-            email.put("url", url);
+			Map<String, Object> email = new HashMap<>();
+			/* Email fields values */
+			email.put("name", launch.getName());
+			email.put("number", String.valueOf(launch.getNumber()));
+			email.put("description", launch.getDescription());
+			email.put("url", url);
 
 			/* Launch execution statistics */
-            email.put("total", launch.getStatistics().getExecutionCounter().getTotal().toString());
-            email.put("passed", launch.getStatistics().getExecutionCounter().getPassed().toString());
-            email.put("failed", launch.getStatistics().getExecutionCounter().getFailed().toString());
-            email.put("skipped", launch.getStatistics().getExecutionCounter().getSkipped().toString());
+			email.put("total", launch.getStatistics().getExecutionCounter().getTotal().toString());
+			email.put("passed", launch.getStatistics().getExecutionCounter().getPassed().toString());
+			email.put("failed", launch.getStatistics().getExecutionCounter().getFailed().toString());
+			email.put("skipped", launch.getStatistics().getExecutionCounter().getSkipped().toString());
 
 			/* Launch issue statistics global counters */
-            email.put("productBugTotal", launch.getStatistics().getIssueCounter().getProductBugTotal().toString());
-            email.put("automationBugTotal",
-                    launch.getStatistics().getIssueCounter().getAutomationBugTotal().toString());
-            email.put("systemIssueTotal", launch.getStatistics().getIssueCounter().getSystemIssueTotal().toString());
-            email.put("noDefectTotal", launch.getStatistics().getIssueCounter().getNoDefectTotal().toString());
-            email.put("toInvestigateTotal",
-                    launch.getStatistics().getIssueCounter().getToInvestigateTotal().toString());
+			email.put("productBugTotal", launch.getStatistics().getIssueCounter().getProductBugTotal().toString());
+			email.put("automationBugTotal", launch.getStatistics().getIssueCounter().getAutomationBugTotal().toString());
+			email.put("systemIssueTotal", launch.getStatistics().getIssueCounter().getSystemIssueTotal().toString());
+			email.put("noDefectTotal", launch.getStatistics().getIssueCounter().getNoDefectTotal().toString());
+			email.put("toInvestigateTotal", launch.getStatistics().getIssueCounter().getToInvestigateTotal().toString());
 
 			/* Launch issue statistics custom sub-types */
-            if (launch.getStatistics().getIssueCounter().getProductBug().entrySet().size() > 1) {
-                Map<StatisticSubType, String> pb = new LinkedHashMap<>();
-                launch.getStatistics().getIssueCounter().getProductBug().forEach((k, v) -> {
-                    if (!k.equalsIgnoreCase(IssueCounter.GROUP_TOTAL))
-                        pb.put(settings.getByLocator(k), v.toString());
-                });
-                email.put("pbInfo", pb);
-            }
-            if (launch.getStatistics().getIssueCounter().getAutomationBug().entrySet().size() > 1) {
-                Map<StatisticSubType, String> ab = new LinkedHashMap<>();
-                launch.getStatistics().getIssueCounter().getAutomationBug().forEach((k, v) -> {
-                    if (!k.equalsIgnoreCase(IssueCounter.GROUP_TOTAL))
-                        ab.put(settings.getByLocator(k), v.toString());
-                });
-                email.put("abInfo", ab);
-            }
-            if (launch.getStatistics().getIssueCounter().getSystemIssue().entrySet().size() > 1) {
-                Map<StatisticSubType, String> si = new LinkedHashMap<>();
-                launch.getStatistics().getIssueCounter().getSystemIssue().forEach((k, v) -> {
-                    if (!k.equalsIgnoreCase(IssueCounter.GROUP_TOTAL))
-                        si.put(settings.getByLocator(k), v.toString());
-                });
-                email.put("siInfo", si);
-            }
-            if (launch.getStatistics().getIssueCounter().getNoDefect().entrySet().size() > 1) {
-                Map<StatisticSubType, String> nd = new LinkedHashMap<>();
-                launch.getStatistics().getIssueCounter().getNoDefect().forEach((k, v) -> {
-                    if (!k.equalsIgnoreCase(IssueCounter.GROUP_TOTAL))
-                        nd.put(settings.getByLocator(k), v.toString());
-                });
-                email.put("ndInfo", nd);
-            }
-            if (launch.getStatistics().getIssueCounter().getToInvestigate().entrySet().size() > 1) {
-                Map<StatisticSubType, String> ti = new LinkedHashMap<>();
-                launch.getStatistics().getIssueCounter().getToInvestigate().forEach((k, v) -> {
-                    if (!k.equalsIgnoreCase(IssueCounter.GROUP_TOTAL))
-                        ti.put(settings.getByLocator(k), v.toString());
-                });
-                email.put("tiInfo", ti);
-            }
+			if (launch.getStatistics().getIssueCounter().getProductBug().entrySet().size() > 1) {
+				Map<StatisticSubType, String> pb = new LinkedHashMap<>();
+				launch.getStatistics().getIssueCounter().getProductBug().forEach((k, v) -> {
+					if (!k.equalsIgnoreCase(IssueCounter.GROUP_TOTAL))
+						pb.put(settings.getByLocator(k), v.toString());
+				});
+				email.put("pbInfo", pb);
+			}
+			if (launch.getStatistics().getIssueCounter().getAutomationBug().entrySet().size() > 1) {
+				Map<StatisticSubType, String> ab = new LinkedHashMap<>();
+				launch.getStatistics().getIssueCounter().getAutomationBug().forEach((k, v) -> {
+					if (!k.equalsIgnoreCase(IssueCounter.GROUP_TOTAL))
+						ab.put(settings.getByLocator(k), v.toString());
+				});
+				email.put("abInfo", ab);
+			}
+			if (launch.getStatistics().getIssueCounter().getSystemIssue().entrySet().size() > 1) {
+				Map<StatisticSubType, String> si = new LinkedHashMap<>();
+				launch.getStatistics().getIssueCounter().getSystemIssue().forEach((k, v) -> {
+					if (!k.equalsIgnoreCase(IssueCounter.GROUP_TOTAL))
+						si.put(settings.getByLocator(k), v.toString());
+				});
+				email.put("siInfo", si);
+			}
+			if (launch.getStatistics().getIssueCounter().getNoDefect().entrySet().size() > 1) {
+				Map<StatisticSubType, String> nd = new LinkedHashMap<>();
+				launch.getStatistics().getIssueCounter().getNoDefect().forEach((k, v) -> {
+					if (!k.equalsIgnoreCase(IssueCounter.GROUP_TOTAL))
+						nd.put(settings.getByLocator(k), v.toString());
+				});
+				email.put("ndInfo", nd);
+			}
+			if (launch.getStatistics().getIssueCounter().getToInvestigate().entrySet().size() > 1) {
+				Map<StatisticSubType, String> ti = new LinkedHashMap<>();
+				launch.getStatistics().getIssueCounter().getToInvestigate().forEach((k, v) -> {
+					if (!k.equalsIgnoreCase(IssueCounter.GROUP_TOTAL))
+						ti.put(settings.getByLocator(k), v.toString());
+				});
+				email.put("tiInfo", ti);
+			}
 
-            String text = templateEngine.merge("finish-launch-template.ftl", email);
-            message.setText(text, true);
-            message.addInline("logoimg", new UrlResource(getClass().getClassLoader().getResource(LOGO)));
-        };
-        this.send(preparator);
-    }
+			String text = templateEngine.merge("finish-launch-template.ftl", email);
+			message.setText(text, true);
+			message.addInline("logoimg", new UrlResource(getClass().getClassLoader().getResource(LOGO)));
+		};
+		this.send(preparator);
+	}
 
-    /**
-     * Restore password email
-     *
-     * @param subject
-     * @param recipients
-     * @param url
-     */
-    public void sendRestorePasswordEmail(final String subject, final String[] recipients, final String url,
-            final String login) {
-        MimeMessagePreparator preparator = mimeMessage -> {
-            URL logoImg = this.getClass().getClassLoader().getResource(LOGO);
-            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "utf-8");
-            message.setSubject(subject);
-            message.setTo(recipients);
+	/**
+	 * Restore password email
+	 *
+	 * @param subject
+	 * @param recipients
+	 * @param url
+	 */
+	public void sendRestorePasswordEmail(final String subject, final String[] recipients, final String url, final String login) {
+		MimeMessagePreparator preparator = mimeMessage -> {
+			URL logoImg = this.getClass().getClassLoader().getResource(LOGO);
+			MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "utf-8");
+			message.setSubject(subject);
+			message.setTo(recipients);
 
-            setFrom(message);
+			setFrom(message);
 
-            Map<String, Object> email = new HashMap<>();
-            email.put("login", login);
-            email.put("url", url);
-            String text = templateEngine.merge("restore-password-template.ftl", email);
-            message.setText(text, true);
-            message.addInline("logoimg", new UrlResource(logoImg));
-        };
-        this.send(preparator);
-    }
+			Map<String, Object> email = new HashMap<>();
+			email.put("login", login);
+			email.put("url", url);
+			String text = templateEngine.merge("restore-password-template.ftl", email);
+			message.setText(text, true);
+			message.addInline("logoimg", new UrlResource(logoImg));
+		};
+		this.send(preparator);
+	}
 
-    public void setTemplateEngine(TemplateEngine templateEngine) {
-        this.templateEngine = templateEngine;
-    }
+	public void setTemplateEngine(TemplateEngine templateEngine) {
+		this.templateEngine = templateEngine;
+	}
 
-    public void setFrom(String from) {
-        this.from = from;
-    }
+	public void setFrom(String from) {
+		this.from = from;
+	}
 
-    public void sendConfirmationEmail(CreateUserRQFull req, String basicUrl) {
-        MimeMessagePreparator preparator = mimeMessage -> {
-            URL logoImg = this.getClass().getClassLoader().getResource(LOGO);
-            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "utf-8");
-            message.setSubject("Welcome to Report Portal");
-            message.setTo(req.getEmail());
-            setFrom(message);
+	public void sendConfirmationEmail(CreateUserRQFull req, String basicUrl) {
+		MimeMessagePreparator preparator = mimeMessage -> {
+			URL logoImg = this.getClass().getClassLoader().getResource(LOGO);
+			MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "utf-8");
+			message.setSubject("Welcome to Report Portal");
+			message.setTo(req.getEmail());
+			setFrom(message);
 
-            Map<String, Object> email = new HashMap<>();
-            email.put("url", basicUrl);
-            email.put("login", normalizeUsername(req.getLogin()));
-            email.put("password", req.getPassword());
-            String text = templateEngine.merge("create-user-template.ftl", email);
-            message.setText(text, true);
-            message.addInline("logoimg", new UrlResource(logoImg));
-        };
-        this.send(preparator);
-    }
+			Map<String, Object> email = new HashMap<>();
+			email.put("url", basicUrl);
+			email.put("login", normalizeUsername(req.getLogin()));
+			email.put("password", req.getPassword());
+			String text = templateEngine.merge("create-user-template.ftl", email);
+			message.setText(text, true);
+			message.addInline("logoimg", new UrlResource(logoImg));
+		};
+		this.send(preparator);
+	}
 
-    /**
-     * Builds FROM field
-     * If username is email, format will be "from \<email\>"
-     */
-    private void setFrom(MimeMessageHelper message) throws MessagingException, UnsupportedEncodingException {
-        if (!Strings.isNullOrEmpty(this.from)) {
-            if (isAddressValid(this.from)) {
-                message.setFrom(this.from);
-            } else if (UserUtils.isEmailValid(getUsername())) {
-                message.setFrom(getUsername(), this.from);
-            }
-        }
-        //otherwise generate automatically
-    }
+	/**
+	 * Builds FROM field
+	 * If username is email, format will be "from \<email\>"
+	 */
+	private void setFrom(MimeMessageHelper message) throws MessagingException, UnsupportedEncodingException {
+		if (!Strings.isNullOrEmpty(this.from)) {
+			if (isAddressValid(this.from)) {
+				message.setFrom(this.from);
+			} else if (UserUtils.isEmailValid(getUsername())) {
+				message.setFrom(getUsername(), this.from);
+			}
+		}
+		//otherwise generate automatically
+	}
 
-    private boolean isAddressValid(String from) {
-        try {
-            InternetAddress.parse(from);
-            return true;
-        } catch (AddressException e) {
-            return false;
-        }
-    }
+	private boolean isAddressValid(String from) {
+		try {
+			InternetAddress.parse(from);
+			return true;
+		} catch (AddressException e) {
+			return false;
+		}
+	}
+
+	private ClassPathResource emailTemplateResource(String resource) {
+		return new ClassPathResource(EMAIL_TEMPLATE_PREFIX + resource);
+	}
 }

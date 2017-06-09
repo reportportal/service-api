4370567c69ab0407510fb4b5ce730ea1154cffbe
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-06-09 15:20:14 +0300

    Headers and more tests

--- src/test/java/com/epam/ta/reportportal/ws/converter/converters/RestorePasswordBidConverterTest.java
+++ src/test/java/com/epam/ta/reportportal/ws/converter/converters/RestorePasswordBidConverterTest.java
@@ -21,30 +21,27 @@
 
 package com.epam.ta.reportportal.ws.converter.converters;
 
-import com.epam.ta.reportportal.database.entity.Project;
-import com.epam.ta.reportportal.database.entity.project.EntryType;
-import com.epam.ta.reportportal.ws.model.project.ProjectInfoResource;
-import com.google.common.collect.ImmutableMap;
+import com.epam.ta.reportportal.database.entity.user.RestorePasswordBid;
+import com.epam.ta.reportportal.ws.model.user.RestorePasswordRQ;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Date;
 
 /**
  * @author Pavel_Bortnik
  */
-public class ProjectInfoConverterTest {
+public class RestorePasswordBidConverterTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testNull() {
+        RestorePasswordBidConverter.TO_BID.apply(null);
+    }
 
     @Test
     public void testConvert() {
-        Project project = new Project();
-        project.setUsers(ImmutableMap.<String, Project.UserConfig>builder().put("", new Project.UserConfig()).build());
-        project.setCreationDate(new Date());
-        ProjectInfoResource resource = ProjectInfoConverter.TO_RESOURCE.apply(project);
-        Assert.assertTrue(resource.getUsersQuantity() == project.getUsers().size());
-        Assert.assertEquals(resource.getCreationDate(), project.getCreationDate());
-        Assert.assertEquals(resource.getEntryType(), EntryType.INTERNAL.name());
-        Assert.assertNull(resource.getProjectId());
+        RestorePasswordRQ rq = new RestorePasswordRQ();
+        rq.setEmail("email@email.com");
+        RestorePasswordBid bid = RestorePasswordBidConverter.TO_BID.apply(rq);
+        Assert.assertEquals(bid.getEmail(), rq.getEmail());
+        Assert.assertNotNull(bid.getId());
     }
-
 }
--- src/test/java/com/epam/ta/reportportal/ws/converter/converters/UserCreationBidConverterTest.java
+++ src/test/java/com/epam/ta/reportportal/ws/converter/converters/UserCreationBidConverterTest.java
@@ -21,30 +21,33 @@
 
 package com.epam.ta.reportportal.ws.converter.converters;
 
-import com.epam.ta.reportportal.database.entity.Project;
-import com.epam.ta.reportportal.database.entity.project.EntryType;
-import com.epam.ta.reportportal.ws.model.project.ProjectInfoResource;
-import com.google.common.collect.ImmutableMap;
+import com.epam.ta.reportportal.database.entity.user.UserCreationBid;
+import com.epam.ta.reportportal.ws.model.user.CreateUserRQ;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Date;
 
 /**
  * @author Pavel_Bortnik
  */
-public class ProjectInfoConverterTest {
+public class UserCreationBidConverterTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testNull() {
+        UserCreationBidConverter.TO_USER.apply(null);
+    }
 
     @Test
     public void testConvert() {
-        Project project = new Project();
-        project.setUsers(ImmutableMap.<String, Project.UserConfig>builder().put("", new Project.UserConfig()).build());
-        project.setCreationDate(new Date());
-        ProjectInfoResource resource = ProjectInfoConverter.TO_RESOURCE.apply(project);
-        Assert.assertTrue(resource.getUsersQuantity() == project.getUsers().size());
-        Assert.assertEquals(resource.getCreationDate(), project.getCreationDate());
-        Assert.assertEquals(resource.getEntryType(), EntryType.INTERNAL.name());
-        Assert.assertNull(resource.getProjectId());
+        CreateUserRQ rq = new CreateUserRQ();
+        rq.setDefaultProject("default_personal");
+        rq.setEmail("email@email.com");
+        rq.setRole("USER");
+        UserCreationBid bid = UserCreationBidConverter.TO_USER.apply(rq);
+
+        Assert.assertEquals(bid.getDefaultProject(), rq.getDefaultProject());
+        Assert.assertEquals(bid.getEmail(), rq.getEmail());
+        Assert.assertEquals(bid.getRole(), rq.getRole());
+        Assert.assertNotNull(bid.getId());
     }
 
 }

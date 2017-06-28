9a8c21f0b817d04a5c84ca66e27c7d2b464b9727
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2017-06-28 13:07:18 +0300

    organize imports

--- src/main/java/com/epam/ta/reportportal/job/SaveBinaryDataJob.java
+++ src/main/java/com/epam/ta/reportportal/job/SaveBinaryDataJob.java
@@ -30,13 +30,11 @@ import com.epam.ta.reportportal.database.entity.BinaryContent;
 import com.epam.ta.reportportal.database.entity.Log;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
-import com.google.common.io.Files;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;

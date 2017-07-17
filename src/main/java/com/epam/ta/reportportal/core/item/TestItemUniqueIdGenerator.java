5c9cf9448d3069674167acfde7d998b16792a05a
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-07-17 15:18:01 +0300

    Added utf-8 charset

--- src/main/java/com/epam/ta/reportportal/core/item/TestItemUniqueIdGenerator.java
+++ src/main/java/com/epam/ta/reportportal/core/item/TestItemUniqueIdGenerator.java
@@ -65,7 +65,7 @@ public class TestItemUniqueIdGenerator implements UniqueIdGenerator {
 
     @Override
     public boolean validate(String encoded) {
-        return !Strings.isNullOrEmpty(encoded) && new String(decoder.decode(encoded)).startsWith(SECRET);
+        return !Strings.isNullOrEmpty(encoded) && new String(decoder.decode(encoded), StandardCharsets.UTF_8).startsWith(SECRET);
     }
 
     private String prepareForEncoding(TestItem testItem, String projectName) {

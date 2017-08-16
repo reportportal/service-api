f25d135a8d2243657665bf6b5f43adbbf4a29011
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-08-16 15:30:54 +0300

    Added empty widgets

--- src/main/java/com/epam/ta/reportportal/core/widget/content/GadgetTypes.java
+++ src/main/java/com/epam/ta/reportportal/core/widget/content/GadgetTypes.java
@@ -48,8 +48,9 @@ public enum GadgetTypes {
 	LAUNCHES_TABLE("launches_table"),
 	MOST_FAILED_TEST_CASES("most_failed_test_cases"),
 	PASSING_RATE_SUMMARY("passing_rate_summary"),
-	PASSING_RATE_PER_LAUNCH("passing_rate_per_launch");
-	// @formatter:on
+    PASSING_RATE_PER_LAUNCH("passing_rate_per_launch"),
+    PRODUCT_STATUS("product_status");
+    // @formatter:on
 
 	private final String type;
 

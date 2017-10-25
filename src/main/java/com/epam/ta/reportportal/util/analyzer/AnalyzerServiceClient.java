a9f0cc98c3a14342408e3194990334e38ff0c831
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-10-25 11:53:40 +0300

    Add correct handling of exceptions from analyzers

--- src/main/java/com/epam/ta/reportportal/util/analyzer/AnalyzerServiceClient.java
+++ src/main/java/com/epam/ta/reportportal/util/analyzer/AnalyzerServiceClient.java
@@ -25,19 +25,21 @@ import com.epam.ta.reportportal.commons.validation.BusinessRule;
 import com.epam.ta.reportportal.util.analyzer.model.IndexLaunch;
 import com.epam.ta.reportportal.util.analyzer.model.IndexRs;
 import com.epam.ta.reportportal.ws.model.ErrorType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 
+import static java.util.Comparator.comparingLong;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -47,6 +49,8 @@ import static java.util.stream.Collectors.toList;
  */
 @Service("analyzerServiceClient")
 public class AnalyzerServiceClient {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AnalyzerServiceClient.class);
 
 	private static final String INDEX_PATH = "/_index";
 	private static final String ANALYZE_PATH = "/_analyze";
@@ -70,30 +74,71 @@ public class AnalyzerServiceClient {
 	}
 
 	public List<IndexRs> index(List<IndexLaunch> rq) {
-		List<String> analyzerServiceInstances = getAnalyzerServiceInstances().stream()
-				.filter(it -> Boolean.valueOf(it.getMetadata().get(DOES_NEED_INDEX)))
-				.map(it -> it.getUri().toString())
-				.collect(toList());
-		return analyzerServiceInstances.stream()
-				.map(serviceUrl -> restTemplate.postForEntity(serviceUrl + INDEX_PATH, rq, IndexRs.class))
-				.map(HttpEntity::getBody)
+		List<ServiceInstance> analyzerInstances = getAnalyzerServiceInstances();
+		return analyzerInstances.stream()
+				.filter(instance -> Boolean.valueOf(instance.getMetadata().get(DOES_NEED_INDEX)))
+				.map(instance -> index(instance, rq))
+				.filter(Optional::isPresent)
+				.map(Optional::get)
 				.collect(toList());
 	}
 
 	public IndexLaunch analyze(IndexLaunch rq) {
 		List<ServiceInstance> analyzerInstances = getAnalyzerServiceInstances();
-
-		analyzerInstances.sort(Comparator.comparingLong(it -> Long.parseLong(it.getMetadata().get(PRIORITY))));
-		Collections.reverse(analyzerInstances);
-
+		analyzerInstances.sort(comparingLong((ServiceInstance it) -> Long.parseLong(it.getMetadata().get(PRIORITY))).reversed());
 		for (ServiceInstance instance : analyzerInstances) {
-			ResponseEntity<IndexLaunch[]> responseEntity = restTemplate.postForEntity(
-					instance.getUri().toString() + ANALYZE_PATH, Collections.singletonList(rq), IndexLaunch[].class);
-			rq = responseEntity.getBody()[0];
+			Optional<IndexLaunch> analyzed = analyze(instance, rq);
+			if (analyzed.isPresent()) {
+				rq = analyzed.get();
+			}
 		}
 		return rq;
 	}
 
+	/**
+	 * Indexes test items logs by specified analyzer instance.
+	 *
+	 * @param analyzer Analyzer
+	 * @param rq       Request {@link List<IndexLaunch>} to index
+	 * @return {@link Optional} of {@link IndexRs} with indexed items
+	 */
+	private Optional<IndexRs> index(ServiceInstance analyzer, List<IndexLaunch> rq) {
+		try {
+			ResponseEntity<IndexRs> responseEntity = restTemplate.postForEntity(
+					analyzer.getUri().toString() + INDEX_PATH, rq, IndexRs.class);
+			return Optional.ofNullable(responseEntity.getBody());
+		} catch (Exception e) {
+			LOGGER.warn("Indexing failed. Cannot interact with {} analyzer. Error: {}", analyzer.getMetadata().get(ANALYZER_KEY), e);
+		}
+		return Optional.empty();
+	}
+
+	/**
+	 * Analyze test items by specified analyzer instance.
+	 *
+	 * @param analyzer Analyzer
+	 * @param rq       Request {@link IndexLaunch} to analyze
+	 * @return {@link Optional} of {@link IndexLaunch} with analyzed items
+	 */
+	private Optional<IndexLaunch> analyze(ServiceInstance analyzer, IndexLaunch rq) {
+		try {
+			ResponseEntity<IndexLaunch[]> responseEntity = restTemplate.postForEntity(
+					analyzer.getUri().toString() + ANALYZE_PATH, Collections.singletonList(rq), IndexLaunch[].class);
+			IndexLaunch[] rs = responseEntity.getBody();
+			if (rs.length > 0) {
+				return Optional.ofNullable(rs[0]);
+			}
+		} catch (Exception e) {
+			LOGGER.warn("Analyzing failed. Cannot interact with {} analyzer. Error: {}", analyzer.getMetadata().get(ANALYZER_KEY), e);
+		}
+		return Optional.empty();
+	}
+
+	/**
+	 * Get list of available analyzers instances
+	 *
+	 * @return {@link List} of instances
+	 */
 	private List<ServiceInstance> getAnalyzerServiceInstances() {
 		return discoveryClient.getServices()
 				.stream()

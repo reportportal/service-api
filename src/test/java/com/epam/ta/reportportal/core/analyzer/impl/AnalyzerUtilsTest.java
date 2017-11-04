5ca6b959d63dbe0b7dd5d292dad6aecd3d96c339
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-11-04 13:29:10 +0300

    more unit tests

--- src/test/java/com/epam/ta/reportportal/core/analyzer/impl/AnalyzerUtilsTest.java
+++ src/test/java/com/epam/ta/reportportal/core/analyzer/impl/AnalyzerUtilsTest.java
@@ -22,6 +22,7 @@
 package com.epam.ta.reportportal.core.analyzer.impl;
 
 import com.epam.ta.reportportal.core.analyzer.model.IndexTestItem;
+import com.epam.ta.reportportal.core.analyzer.model.InvestigationType;
 import com.epam.ta.reportportal.database.entity.Log;
 import com.epam.ta.reportportal.database.entity.LogLevel;
 import com.epam.ta.reportportal.database.entity.item.TestItem;
@@ -41,15 +42,35 @@ public class AnalyzerUtilsTest {
 
 	@Test
 	public void testConverting() {
-		TestItem testItem = new TestItem();
-		testItem.setId("item");
-		testItem.setUniqueId("uniqueId");
-		testItem.setIssue(new TestItemIssue());
+		TestItem testItem = createTest();
+		testItem.setIssue(createIssue(false));
 		IndexTestItem indexTestItem = fromTestItem(testItem, createSameLogs(5));
 		Assert.assertEquals(testItem.getId(), indexTestItem.getTestItemId());
 		Assert.assertEquals(testItem.getUniqueId(), indexTestItem.getUniqueId());
 		Assert.assertEquals(testItem.getIssue().getIssueType(), indexTestItem.getIssueType());
-		Assert.assertEquals(indexTestItem.getLogs().size(), 1);
+		Assert.assertEquals(1, indexTestItem.getLogs().size());
+		Assert.assertEquals(InvestigationType.HUMAN, indexTestItem.getInvestigationType());
+	}
+
+	@Test
+	public void testConvertingAnalyzed() {
+		TestItem test = createTest();
+		test.setIssue(createIssue(true));
+		IndexTestItem indexTestItem = fromTestItem(test, createSameLogs(1));
+		Assert.assertEquals(InvestigationType.ANALYZER, indexTestItem.getInvestigationType());
+	}
+
+	private TestItem createTest(){
+		TestItem testItem = new TestItem();
+		testItem.setId("item");
+		testItem.setUniqueId("uniqueId");
+		return testItem;
+	}
+
+	private TestItemIssue createIssue(boolean isAutoAnalyzed) {
+		TestItemIssue issue = new TestItemIssue();
+		issue.setAutoAnalyzed(isAutoAnalyzed);
+		return issue;
 	}
 
 	private List<Log> createSameLogs(int count) {

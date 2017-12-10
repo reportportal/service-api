68e0dda53d048d73cae77ad36aec7fb394784665
pbortnik <pavel_bortnik@epam.com>
2017-12-10 15:06:11 +0300

    docs

--- src/main/java/com/epam/ta/reportportal/core/launch/impl/RetriesLaunchHandler.java
+++ src/main/java/com/epam/ta/reportportal/core/launch/impl/RetriesLaunchHandler.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2017 EPAM Systems
+ *
+ *
+ * This file is part of EPAM Report Portal.
+ * https://github.com/reportportal/service-api
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.epam.ta.reportportal.core.launch.impl;
 
 import com.epam.ta.reportportal.commons.Predicates;
@@ -21,6 +42,9 @@ import java.util.List;
 
 import static org.apache.commons.lang3.BooleanUtils.isTrue;
 
+/**
+ * @author Pavel Bortnik
+ */
 @Service
 public class RetriesLaunchHandler implements IRetriesLaunchHandler {
 
@@ -45,7 +69,8 @@ public class RetriesLaunchHandler implements IRetriesLaunchHandler {
 			retries.forEach(retry -> {
 				List<TestItem> rtr = retry.getRetries();
 				TestItem retryRoot = rtr.stream().filter(it -> it.getRetryType().equals(RetryType.ROOT)).findFirst().orElse(null);
-				BusinessRule.expect(retryRoot, Predicates.notNull()).verify(ErrorType.FORBIDDEN_OPERATION);
+				BusinessRule.expect(retryRoot, Predicates.notNull())
+						.verify(ErrorType.FORBIDDEN_OPERATION, "Retry root should exist in retries");
 
 				statisticsFacade.resetExecutionStatistics(retryRoot);
 				if (retryRoot.getIssue() != null) {

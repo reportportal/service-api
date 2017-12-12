809b3e47b4637362d154c82955dd40fc48b838d7
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-12-12 14:26:53 +0300

    more unit tests

--- src/test/java/com/epam/ta/reportportal/util/PredicatesTest.java
+++ src/test/java/com/epam/ta/reportportal/util/PredicatesTest.java
@@ -1,7 +1,12 @@
 package com.epam.ta.reportportal.util;
 
+import com.epam.ta.reportportal.database.entity.item.TestItem;
+import com.epam.ta.reportportal.database.entity.item.issue.TestItemIssue;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static com.epam.ta.reportportal.util.Predicates.CAN_BE_INDEXED;
+import static com.epam.ta.reportportal.util.Predicates.IS_RETRY;
 
 /**
  * @author Andrei Varabyeu
@@ -13,5 +18,45 @@ public class PredicatesTest {
 		Assert.assertTrue("Incorrect predicate behavior: only spec chars", Predicates.SPECIAL_CHARS_ONLY.test("_"));
 		Assert.assertFalse("Incorrect predicate behavior: spec chars after ASCII", Predicates.SPECIAL_CHARS_ONLY.test("a_"));
 		Assert.assertFalse("Incorrect predicate behavior: spec chars before ASCII", Predicates.SPECIAL_CHARS_ONLY.test("_a"));
+	}
+
+	@Test
+	public void checkIsRetry() {
+		TestItem testItem = new TestItem();
+		testItem.setRetryProcessed(false);
+		Assert.assertTrue("Item should be a retry", IS_RETRY.test(testItem));
+	}
+
+	@Test
+	public void negativeCheckIsRetry() {
+		TestItem testItem = new TestItem();
+		Assert.assertFalse("Item should not be a retry", IS_RETRY.test(testItem));
+	}
+
+	@Test
+	public void checkCanBeIndexed() {
+		TestItem testItem = new TestItem();
+		TestItemIssue issue = new TestItemIssue();
+		issue.setIgnoreAnalyzer(false);
+		issue.setIssueType("PB001");
+		testItem.setIssue(issue);
+		Assert.assertTrue("Item should be available for indexing", CAN_BE_INDEXED.test(testItem));
+	}
+
+	@Test
+	public void checkTIIndexed() {
+		TestItem testItem = new TestItem();
+		TestItemIssue issue = new TestItemIssue();
+		testItem.setIssue(issue);
+		Assert.assertFalse("Item with TI issue shouldn't be available for indexing", CAN_BE_INDEXED.test(testItem));
+	}
+	@Test
+	public void checkIgnoreIndexed() {
+		TestItem testItem = new TestItem();
+		TestItemIssue issue = new TestItemIssue();
+		issue.setIssueType("PB001");
+		issue.setIgnoreAnalyzer(true);
+		testItem.setIssue(issue);
+		Assert.assertFalse("Item with ignore flag shouldn't be available for indexing", CAN_BE_INDEXED.test(testItem));
 	}
 }

824ab6de3fef52ae88b5c3321afeadd2bfb66c05
Andrei Varabyeu <andrei_varabyeu@epam.com>
2017-12-13 16:01:04 +0300

    add quartz support

--- src/main/java/com/epam/ta/reportportal/job/InterruptBrokenLaunchesJob.java
+++ src/main/java/com/epam/ta/reportportal/job/InterruptBrokenLaunchesJob.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/service-api
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -31,8 +31,9 @@ import com.epam.ta.reportportal.database.entity.Project;
 import com.epam.ta.reportportal.database.entity.Status;
 import com.epam.ta.reportportal.database.entity.item.TestItem;
 import com.epam.ta.reportportal.database.entity.project.InterruptionJobDelay;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -49,7 +50,7 @@ import static java.time.Duration.ofHours;
  * @author Andrei Varabyeu
  */
 @Service
-public class InterruptBrokenLaunchesJob implements Runnable {
+public class InterruptBrokenLaunchesJob implements Job {
 
 	@Autowired
 	private LaunchRepository launchRepository;
@@ -67,8 +68,8 @@ public class InterruptBrokenLaunchesJob implements Runnable {
 	private ProjectRepository projectRepository;
 
 	@Override
-	@Scheduled(cron = "${com.ta.reportportal.job.interrupt.broken.launches.cron}")
-	public void run() {
+//	@Scheduled(cron = "${com.ta.reportportal.job.interrupt.broken.launches.cron}")
+	public void execute(JobExecutionContext context) {
 		try (Stream<Project> projects = projectRepository.streamAllIdsAndConfiguration()) {
 			projects.forEach(project -> {
 				Duration maxDuration = ofHours(

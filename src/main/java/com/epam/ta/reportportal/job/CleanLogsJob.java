7d54c0dedd5ac34465df253dc916c6f627af29b3
avarabyeu <andrei.varabyeu@gmail.com>
2018-05-02 18:55:14 +0300

    replace streams(cursors) with paging in jobs
    Signed-off-by: avarabyeu <andrei.varabyeu@gmail.com>

--- src/main/java/com/epam/ta/reportportal/job/CleanLogsJob.java
+++ src/main/java/com/epam/ta/reportportal/job/CleanLogsJob.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/service-api
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,7 +22,6 @@
 package com.epam.ta.reportportal.job;
 
 import com.epam.ta.reportportal.database.dao.*;
-import com.epam.ta.reportportal.database.entity.Launch;
 import com.epam.ta.reportportal.database.entity.Project;
 import com.epam.ta.reportportal.database.entity.item.TestItem;
 import com.epam.ta.reportportal.database.entity.project.KeepLogsDelay;
@@ -34,10 +33,12 @@ import org.springframework.stereotype.Service;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.epam.ta.reportportal.database.entity.project.KeepLogsDelay.findByName;
+import static com.epam.ta.reportportal.job.PageUtil.iterateOverPages;
 import static java.time.Duration.ofDays;
 
 /**
@@ -69,32 +70,40 @@ public class CleanLogsJob implements Job {
 	private ActivityRepository activityRepository;
 
 	@Override
-//	@Scheduled(cron = "${com.ta.reportportal.job.clean.logs.cron}")
+	//	@Scheduled(cron = "${com.ta.reportportal.job.clean.logs.cron}")
 	public void execute(JobExecutionContext context) {
-		try (Stream<Project> stream = projectRepository.streamAllIdsAndConfiguration()) {
-			stream.forEach(project -> {
+		iterateOverPages(projectRepository::findAllIdsAndConfiguration, this::processProjects);
+	}
+
+	private void processProjects(List<Project> projects) {
+		projects.forEach(project -> {
+			try {
 				Duration period = ofDays(findByName(project.getConfiguration().getKeepLogs()).getDays());
 				if (!period.isZero()) {
 					activityRepository.deleteModifiedLaterAgo(project.getId(), period);
 					removeOutdatedLogs(project.getId(), period);
 				}
-			});
-		}
+			} catch (Exception e) {
+				//do nothing
+			}
+		});
 	}
 
 	private void removeOutdatedLogs(String projectId, Duration period) {
-		try (Stream<Launch> launchStream = streamLaunches(projectId)) {
-			launchStream.forEach(launch -> {
-				try (Stream<TestItem> testItemStream = testItemRepo.streamIdsByLaunch(launch.getId())) {
-					logRepo.deleteByPeriodAndItemsRef(period, testItemStream.map(TestItem::getId).collect(Collectors.toList()));
-				}
-			});
-		}
-	}
-
-	private Stream<Launch> streamLaunches(String projectId) {
 		Date beginDate = Date.from(Instant.now().minusSeconds(MAX_DELAY.getSeconds()));
 		Date endDate = Date.from(Instant.now().minusSeconds(MIN_DELAY.getSeconds()));
-		return launchRepo.streamModifiedInRange(projectId, beginDate, endDate);
+
+		iterateOverPages(pageable -> launchRepo.getModifiedInRange(projectId, beginDate, endDate, pageable), launches -> {
+			launches.forEach(launch -> {
+				try (Stream<TestItem> testItemStream = testItemRepo.streamIdsByLaunch(launch.getId())) {
+					logRepo.deleteByPeriodAndItemsRef(period, testItemStream.map(TestItem::getId).collect(Collectors.toList()));
+				} catch (Exception e) {
+					//do nothing
+				}
+			});
+
+		});
+
 	}
+
 }
--- src/main/java/com/epam/ta/reportportal/job/CleanScreenshotsJob.java
+++ src/main/java/com/epam/ta/reportportal/job/CleanScreenshotsJob.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/service-api
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,7 +24,6 @@ package com.epam.ta.reportportal.job;
 import com.epam.ta.reportportal.database.DataStorage;
 import com.epam.ta.reportportal.database.dao.LogRepository;
 import com.epam.ta.reportportal.database.dao.ProjectRepository;
-import com.epam.ta.reportportal.database.entity.Project;
 import com.epam.ta.reportportal.database.entity.project.KeepScreenshotsDelay;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -32,7 +31,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.util.stream.Stream;
 
 import static java.time.Duration.ofDays;
 
@@ -54,10 +52,10 @@ public class CleanScreenshotsJob implements Job {
 	private LogRepository logRepository;
 
 	@Override
-//	@Scheduled(cron = "${com.ta.reportportal.job.clean.screenshots.cron}")
+	//	@Scheduled(cron = "${com.ta.reportportal.job.clean.screenshots.cron}")
 	public void execute(JobExecutionContext context) {
-		try (Stream<Project> projects = projectRepository.streamAllIdsAndConfiguration()) {
-			projects.forEach(project -> {
+		PageUtil.iterateOverPages(projectRepository::findAllIdsAndConfiguration, projects -> projects.forEach(project -> {
+			try {
 				Duration period = ofDays(KeepScreenshotsDelay.findByName(project.getConfiguration().getKeepScreenshots()).getDays());
 				if (!period.isZero()) {
 					gridFS.findModifiedLaterAgo(period, project.getId()).forEach(file -> {
@@ -66,7 +64,11 @@ public class CleanScreenshotsJob implements Job {
 						logRepository.removeBinaryContent(file.getId().toString());
 					});
 				}
-			});
-		}
+			} catch (Exception e) {
+				//do nothing
+			}
+
+		}));
+
 	}
 }

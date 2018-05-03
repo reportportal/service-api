438e3f6001ba2f95b6c4abc230e3dc3185adff60
Pavel_Bortnik <pavel_bortnik@epam.com>
2018-05-03 18:05:49 +0300

    fix possible npe

--- src/main/java/com/epam/ta/reportportal/core/project/impl/UpdateProjectHandler.java
+++ src/main/java/com/epam/ta/reportportal/core/project/impl/UpdateProjectHandler.java
@@ -191,7 +191,7 @@ public class UpdateProjectHandler implements IUpdateProjectHandler {
 		ofNullable(modelConfig.getAnalyzerConfig()).ifPresent(analyzerConfig -> {
 			expect(analyzerStatusCache.getAnalyzerStatus().asMap().containsValue(projectName), equalTo(false)).verify(
 					ErrorType.FORBIDDEN_OPERATION, "Project settings can not be updated until auto-analysis proceeds");
-			ProjectAnalyzerConfig dbAnalyzerConfig = dbConfig.getAnalyzerConfig();
+			ProjectAnalyzerConfig dbAnalyzerConfig = ofNullable(dbConfig.getAnalyzerConfig()).orElse(new ProjectAnalyzerConfig());
 			ofNullable(analyzerConfig.getAnalyzerMode()).ifPresent(mode -> dbAnalyzerConfig.setAnalyzerMode(AnalyzeMode.fromString(mode)));
 			ofNullable(analyzerConfig.getIsAutoAnalyzerEnabled()).ifPresent(dbAnalyzerConfig::setIsAutoAnalyzerEnabled);
 			ofNullable(analyzerConfig.getMinDocFreq()).ifPresent(dbAnalyzerConfig::setMinDocFreq);

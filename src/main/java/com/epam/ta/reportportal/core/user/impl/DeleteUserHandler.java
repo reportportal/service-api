78b774025daefecf3a951349a7699998168c94d2
Ivan_Budayeu <budaevqwerty@gmail.com>
2018-07-04 13:32:09 +0300

    *Code style refactoring
    AutoAnalyzed flag will depend on data, that was received from Client, instead of always being "false". If no data received from Client, flag will be "false" by default.

--- src/main/java/com/epam/ta/reportportal/core/user/impl/DeleteUserHandler.java
+++ src/main/java/com/epam/ta/reportportal/core/user/impl/DeleteUserHandler.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/service-api
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -60,7 +60,7 @@ public class DeleteUserHandler implements IDeleteUserHandler {
 	private UatClient uatClient;
 
 	@Autowired
-    private ILogIndexer logIndexer;
+	private ILogIndexer logIndexer;
 
 	@Override
 	public OperationCompletionRS deleteUser(String userId, String principal) {
@@ -84,7 +84,7 @@ public class DeleteUserHandler implements IDeleteUserHandler {
 			throw new ReportPortalException("Error while deleting user", exp);
 		}
 
-		logIndexer.deleteIndex(user.getLogin().toLowerCase()+PersonalProjectService.PERSONAL_PROJECT_POSTFIX.toLowerCase());
+		logIndexer.deleteIndex(user.getLogin().toLowerCase() + PersonalProjectService.PERSONAL_PROJECT_POSTFIX.toLowerCase());
 
 		return new OperationCompletionRS("User with ID = '" + userId + "' successfully deleted.");
 	}

7a91801de313ad83231e6e5aaed5b592e428dad5
Pavel_Bortnik <pavel_bortnik@epam.com>
2018-07-11 20:22:00 +0300

    basic implementation of overall statistics widget

--- src/main/java/com/epam/ta/reportportal/core/widget/content/loader/OverallStatisticsContentLoader.java
+++ src/main/java/com/epam/ta/reportportal/core/widget/content/loader/OverallStatisticsContentLoader.java
@@ -21,11 +21,23 @@
 
 package com.epam.ta.reportportal.core.widget.content.loader;
 
+import com.epam.ta.reportportal.commons.querygen.Condition;
+import com.epam.ta.reportportal.commons.querygen.Filter;
+import com.epam.ta.reportportal.commons.querygen.FilterCondition;
 import com.epam.ta.reportportal.core.widget.content.LoadContentStrategy;
+import com.epam.ta.reportportal.dao.UserFilterRepository;
+import com.epam.ta.reportportal.dao.WidgetContentRepository;
+import com.epam.ta.reportportal.entity.filter.UserFilter;
+import com.epam.ta.reportportal.entity.launch.Launch;
 import com.epam.ta.reportportal.entity.widget.Widget;
+import com.epam.ta.reportportal.entity.widget.content.StatisticsContent;
+import com.epam.ta.reportportal.exception.ReportPortalException;
+import com.epam.ta.reportportal.ws.model.ErrorType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -34,8 +46,32 @@ import java.util.Map;
 @Service
 public class OverallStatisticsContentLoader implements LoadContentStrategy {
 
+	@Autowired
+	private UserFilterRepository filterRepository;
+
+	@Autowired
+	private WidgetContentRepository widgetContentRepository;
+
 	@Override
 	public Map<String, ?> loadContent(Widget widget) {
-		return Collections.emptyMap();
+
+		UserFilter userFilter = filterRepository.findById(widget.getFilters().iterator().next().getId())
+				.orElseThrow(() -> new ReportPortalException(ErrorType.USER_FILTER_NOT_FOUND));
+
+		Filter filter = Filter.builder()
+				.withTarget(Launch.class)
+				.withCondition(FilterCondition.builder()
+						.withSearchCriteria("name")
+						.withCondition(Condition.CONTAINS)
+						.withValue("launch")
+						.withNegative(false)
+						.build())
+				.build();
+
+		List<StatisticsContent> contents = widgetContentRepository.overallStatisticsContent(filter);
+
+		Map<String, List<StatisticsContent>> result = new HashMap<>();
+		result.put("result", contents);
+		return result;
 	}
 }

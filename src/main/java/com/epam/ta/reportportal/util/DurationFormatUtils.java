e737b8d25ab6011a8e27653ff8224eeaa12637c3
pbortnik <rrrizon@gmail.com>
2018-07-24 17:12:39 +0300

    add duration into jasper report

---
+++ src/main/java/com/epam/ta/reportportal/util/DurationFormatUtils.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2018 EPAM Systems
+ *
+ *
+ * This file is part of EPAM Report Portal.
+ * https://github.com/reportportal/service-api
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.epam.ta.reportportal.util;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.*;
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+/**
+ * Utils for properly formatting of duration provided in nanos
+ *
+ * @author Pavel Bortnik
+ */
+public class DurationFormatUtils {
+
+	public static String formatDuration(long nanos) {
+		TimeUnit unit = chooseUnit(nanos);
+		double value = (double) nanos / NANOSECONDS.convert(1, unit);
+		return formatCompact4Digits(value) + " " + abbreviate(unit);
+	}
+
+	private static TimeUnit chooseUnit(long nanos) {
+		if (DAYS.convert(nanos, NANOSECONDS) > 0) {
+			return DAYS;
+		}
+		if (HOURS.convert(nanos, NANOSECONDS) > 0) {
+			return HOURS;
+		}
+		if (MINUTES.convert(nanos, NANOSECONDS) > 0) {
+			return MINUTES;
+		}
+		if (SECONDS.convert(nanos, NANOSECONDS) > 0) {
+			return SECONDS;
+		}
+		if (MILLISECONDS.convert(nanos, NANOSECONDS) > 0) {
+			return MILLISECONDS;
+		}
+		if (MICROSECONDS.convert(nanos, NANOSECONDS) > 0) {
+			return MICROSECONDS;
+		}
+		return NANOSECONDS;
+	}
+
+	static String formatCompact4Digits(double value) {
+		return String.format(Locale.ROOT, "%.4g", value);
+	}
+
+	private static String abbreviate(TimeUnit unit) {
+		switch (unit) {
+			case NANOSECONDS:
+				return "ns";
+			case MICROSECONDS:
+				return "\u03bcs"; // μs
+			case MILLISECONDS:
+				return "ms";
+			case SECONDS:
+				return "s";
+			case MINUTES:
+				return "min";
+			case HOURS:
+				return "h";
+			case DAYS:
+				return "d";
+			default:
+				throw new AssertionError();
+		}
+	}
+}

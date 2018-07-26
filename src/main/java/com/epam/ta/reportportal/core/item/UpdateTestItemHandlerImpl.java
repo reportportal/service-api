3f3b6840cf1138acfcf0ac55d494921b2005f0e3
Pavel Bortnik <pavel_bortnik@epam.com>
2018-07-26 17:49:30 +0300

    add duration into jasper report (#349)
    * add duration into jasper report
    
    * put relevant item id into the action done by analyzer
    
    * Update application-dev-mac.yaml

--- src/main/java/com/epam/ta/reportportal/core/item/UpdateTestItemHandlerImpl.java
+++ src/main/java/com/epam/ta/reportportal/core/item/UpdateTestItemHandlerImpl.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2017 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/service-api
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -196,7 +196,7 @@ public class UpdateTestItemHandlerImpl implements UpdateTestItemHandler {
 
 		expect(!errors.isEmpty(), equalTo(FALSE)).verify(FAILED_TEST_ITEM_ISSUE_TYPE_DEFINITION, errors.toString());
 
-		eventPublisher.publishEvent(new ItemIssueTypeDefined(eventData.build(), userName, projectName));
+		eventPublisher.publishEvent(new ItemIssueTypeDefined(eventData.build(), userName, projectName, null));
 		return updated;
 	}
 

d34b45082621f2e549fb08c9b56c52a133368a5f
pbortnik <rrrizon@gmail.com>
2018-07-31 15:13:28 +0300

    validation on unshared filter in user preference

--- src/main/java/com/epam/ta/reportportal/core/preference/impl/UpdatePreferenceHandler.java
+++ src/main/java/com/epam/ta/reportportal/core/preference/impl/UpdatePreferenceHandler.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2016 EPAM Systems
- * 
- * 
+ *
+ *
  * This file is part of EPAM Report Portal.
  * https://github.com/reportportal/service-api
- * 
+ *
  * Report Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Report Portal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,12 +22,17 @@
 package com.epam.ta.reportportal.core.preference.impl;
 
 import com.epam.ta.reportportal.core.preference.IUpdatePreferenceHandler;
+import com.epam.ta.reportportal.database.dao.UserFilterRepository;
 import com.epam.ta.reportportal.database.dao.UserPreferenceRepository;
 import com.epam.ta.reportportal.database.entity.UserPreference;
+import com.epam.ta.reportportal.database.entity.filter.UserFilter;
 import com.epam.ta.reportportal.ws.model.OperationCompletionRS;
 import com.epam.ta.reportportal.ws.model.preference.UpdatePreferenceRQ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Default implementation of
@@ -40,6 +45,9 @@ public class UpdatePreferenceHandler implements IUpdatePreferenceHandler {
 
 	@Autowired
 	private UserPreferenceRepository userPreferenceRepository;
+
+	@Autowired
+	private UserFilterRepository filterRepository;
 
 	@Override
 	public OperationCompletionRS updatePreference(String userName, String projectName, UpdatePreferenceRQ rq) {
@@ -55,7 +63,14 @@ public class UpdatePreferenceHandler implements IUpdatePreferenceHandler {
 		if (null != rq.getActive()) {
 			preference.getLaunchTabs().setActive(rq.getActive());
 		}
-		preference.getLaunchTabs().setFilters(rq.getFilters());
+
+		List<String> filters = filterRepository.find(rq.getFilters())
+				.stream()
+				.filter(it -> it.getAcl().getOwnerUserId().equalsIgnoreCase(userName) || !it.getAcl().getEntries().isEmpty())
+				.map(UserFilter::getId)
+				.collect(Collectors.toList());
+
+		preference.getLaunchTabs().setFilters(filters);
 		OperationCompletionRS operationCompletionRS = new OperationCompletionRS();
 		userPreferenceRepository.save(preference);
 		operationCompletionRS.setResultMessage("Filter tabs for '" + userName + "' user have been updated");

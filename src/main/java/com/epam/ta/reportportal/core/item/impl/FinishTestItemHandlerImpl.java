4a36bc7fcf5efbd7754d72f2616bf091a56cfaaa
pbortnik <rrrizon@gmail.com>
2018-08-28 20:21:42 +0300

    introduce ltree type for working with item structure

--- src/main/java/com/epam/ta/reportportal/core/item/impl/FinishTestItemHandlerImpl.java
+++ src/main/java/com/epam/ta/reportportal/core/item/impl/FinishTestItemHandlerImpl.java
@@ -85,7 +85,7 @@ class FinishTestItemHandlerImpl implements FinishTestItemHandler {
 		TestItem testItem = testItemRepository.findById(testItemId)
 				.orElseThrow(() -> new ReportPortalException(TEST_ITEM_NOT_FOUND, testItemId));
 
-		boolean hasChildren = testItemRepository.hasChildren(testItem.getPath());
+		boolean hasChildren = testItemRepository.hasChildren(testItem.getItemId(), testItem.getPath());
 		verifyTestItem(user, testItem, fromValue(finishExecutionRQ.getStatus()), hasChildren);
 
 		TestItemResults testItemResults = processItemResults(projectDetails.getProjectId(), testItem, finishExecutionRQ, hasChildren);

67274b6ed1e92cd57294227147183d127e1b8fbf
yumfriez <budaevqwerty@gmail.com>
2018-09-26 15:32:05 +0300

    flaky widget refactoring

--- src/main/java/com/epam/ta/reportportal/core/widget/content/loader/FlakyCasesTableContentLoader.java
+++ src/main/java/com/epam/ta/reportportal/core/widget/content/loader/FlakyCasesTableContentLoader.java
@@ -5,8 +5,14 @@ import com.epam.ta.reportportal.commons.querygen.Filter;
 import com.epam.ta.reportportal.commons.querygen.FilterCondition;
 import com.epam.ta.reportportal.commons.validation.BusinessRule;
 import com.epam.ta.reportportal.core.widget.content.LoadContentStrategy;
+import com.epam.ta.reportportal.dao.LaunchRepository;
 import com.epam.ta.reportportal.dao.WidgetContentRepository;
+import com.epam.ta.reportportal.entity.launch.Launch;
+import com.epam.ta.reportportal.entity.widget.content.FlakyCasesTableContent;
+import com.epam.ta.reportportal.entity.widget.content.LatestLaunchContent;
+import com.epam.ta.reportportal.exception.ReportPortalException;
 import com.epam.ta.reportportal.ws.model.ErrorType;
+import com.google.common.collect.ImmutableMap;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +23,9 @@ import java.util.List;
 import java.util.Map;
 
 import static com.epam.ta.reportportal.commons.Predicates.equalTo;
-import static com.epam.ta.reportportal.core.widget.content.constant.ContentLoaderConstants.LAUNCH_NAME_FIELD;
-import static com.epam.ta.reportportal.core.widget.content.constant.ContentLoaderConstants.RESULT;
+import static com.epam.ta.reportportal.core.widget.content.constant.ContentLoaderConstants.*;
 import static com.epam.ta.reportportal.core.widget.util.WidgetFilterUtil.GROUP_FILTERS;
 import static com.epam.ta.reportportal.dao.constant.WidgetContentRepositoryConstants.NAME;
-import static java.util.Collections.singletonMap;
 
 /**
  * @author Ivan Budayeu
@@ -31,6 +35,9 @@ public class FlakyCasesTableContentLoader implements LoadContentStrategy {
 
 	@Autowired
 	private WidgetContentRepository widgetRepository;
+
+	@Autowired
+	private LaunchRepository launchRepository;
 
 	@Override
 	public Map<String, ?> loadContent(List<String> contentFields, Map<Filter, Sort> filterSortMapping, Map<String, String> widgetOptions,
@@ -42,9 +49,18 @@ public class FlakyCasesTableContentLoader implements LoadContentStrategy {
 
 		Filter filter = GROUP_FILTERS.apply(filterSortMapping.keySet());
 
-		filter.withCondition(new FilterCondition(Condition.EQUALS, false, widgetOptions.get(LAUNCH_NAME_FIELD), NAME));
+		String launchName = widgetOptions.get(LAUNCH_NAME_FIELD);
 
-		return singletonMap(RESULT, widgetRepository.flakyCasesStatistics(filter, limit));
+		Launch launch = launchRepository.findLatestByNameAndFilter(launchName, filter)
+				.orElseThrow(() -> new ReportPortalException(ErrorType.LAUNCH_NOT_FOUND, "No launch with name: " + launchName));
+
+		filter.withCondition(new FilterCondition(Condition.EQUALS, false, launch.getName(), NAME));
+
+		LatestLaunchContent latestLaunchContent = new LatestLaunchContent(launch);
+
+		List<FlakyCasesTableContent> flakyCasesTableContent = widgetRepository.flakyCasesStatistics(filter, limit);
+
+		return ImmutableMap.<String, Object>builder().put(LATEST_LAUNCH, latestLaunchContent).put(FLAKY, flakyCasesTableContent).build();
 	}
 
 	/**

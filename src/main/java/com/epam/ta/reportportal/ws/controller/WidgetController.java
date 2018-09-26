54cb231bfb0871b18a1e349ae787b3ec5601b7fa
pbortnik <rrrizon@gmail.com>
2018-09-26 22:30:59 +0300

    add missed rest endpoints to widget

--- src/main/java/com/epam/ta/reportportal/ws/controller/WidgetController.java
+++ src/main/java/com/epam/ta/reportportal/ws/controller/WidgetController.java
@@ -27,15 +27,22 @@ import com.epam.ta.reportportal.core.widget.IGetWidgetHandler;
 import com.epam.ta.reportportal.core.widget.IUpdateWidgetHandler;
 import com.epam.ta.reportportal.ws.model.EntryCreatedRS;
 import com.epam.ta.reportportal.ws.model.OperationCompletionRS;
+import com.epam.ta.reportportal.ws.model.widget.WidgetPreviewRQ;
 import com.epam.ta.reportportal.ws.model.widget.WidgetRQ;
 import com.epam.ta.reportportal.ws.model.widget.WidgetResource;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+
+import static com.epam.ta.reportportal.commons.EntityUtils.normalizeId;
 import static com.epam.ta.reportportal.util.ProjectUtils.extractProjectDetails;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
@@ -84,6 +91,40 @@ public class WidgetController {
 	public OperationCompletionRS updateWidget(@PathVariable String projectName, @PathVariable Long widgetId,
 			@RequestBody @Validated WidgetRQ updateRQ, @AuthenticationPrincipal ReportPortalUser user) {
 		return updateWidgetHandler.updateWidget(widgetId, updateRQ, extractProjectDetails(user, projectName), user);
+	}
+
+	@Transactional(readOnly = true)
+	@PostMapping(value = "/preview")
+	@ResponseStatus(OK)
+	@ApiOperation("Get widget preview")
+	public Map<String, ?> getWidgetPreview(@PathVariable String projectName, @RequestBody @Validated WidgetPreviewRQ previewRQ,
+			Principal principal) {
+		return getWidgetHandler.getWidgetPreview(normalizeId(projectName), principal.getName(), previewRQ);
+	}
+
+	@Transactional(readOnly = true)
+	@GetMapping(value = "/names/all")
+	@ResponseStatus(OK)
+	@ApiOperation("Load all widget names which belong to a user")
+	public List<String> getWidgetNames(@PathVariable String projectName, Principal principal) {
+		return getWidgetHandler.getWidgetNames(normalizeId(projectName), principal.getName());
+	}
+
+	@Transactional(readOnly = true)
+	@GetMapping(value = "/shared")
+	@ResponseStatus(OK)
+	@ApiOperation("Load shared widgets")
+	public Iterable<WidgetResource> getSharedWidgetsList(Principal principal, @PathVariable String projectName, Pageable pageable) {
+		return getWidgetHandler.getSharedWidgetsList(principal.getName(), normalizeId(projectName), pageable);
+	}
+
+	@Transactional(readOnly = true)
+	@GetMapping(value = "/shared/search")
+	@ResponseStatus(OK)
+	@ApiOperation("Search shared widgets by name")
+	public Iterable<WidgetResource> searchSharedWidgets(@RequestParam("term") String term, @PathVariable String projectName,
+			Pageable pageable) {
+		return getWidgetHandler.searchSharedWidgets(term, normalizeId(projectName), pageable);
 	}
 
 }

2e053373f673ab0ba97691c2175f2566dd5b67cf
pbortnik <rrrizon@gmail.com>
2018-09-27 16:36:00 +0300

    add missed transactional

--- src/main/java/com/epam/ta/reportportal/ws/controller/UserController.java
+++ src/main/java/com/epam/ta/reportportal/ws/controller/UserController.java
@@ -10,6 +10,7 @@ import com.epam.ta.reportportal.ws.model.user.UserResource;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Transactional(readOnly = true)
 	@GetMapping(value = { "", "/" })
 	@ApiOperation("Return information about current logged-in user")
 	public UserResource getMyself(@AuthenticationPrincipal ReportPortalUser user) {

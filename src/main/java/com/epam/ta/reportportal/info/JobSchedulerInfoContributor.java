23ee484d140acd3e8f5ea1ad9c2e59f53c85a3c8
pbortnik <rrrizon@gmail.com>
2018-10-02 11:50:46 +0300

    refactoring of streams in job info contributor

--- src/main/java/com/epam/ta/reportportal/info/JobSchedulerInfoContributor.java
+++ src/main/java/com/epam/ta/reportportal/info/JobSchedulerInfoContributor.java
@@ -1,19 +1,41 @@
+/*
+ * Copyright 2018 EPAM Systems
+ *
+ *
+ * This file is part of EPAM Report Portal.
+ * https://github.com/reportportal/service-api
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.epam.ta.reportportal.info;
 
 import com.epam.ta.reportportal.exception.ReportPortalException;
-import org.quartz.JobKey;
+import org.apache.commons.collections.MapUtils;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.impl.matchers.GroupMatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Collects info about existing jobs and how much it is left
@@ -24,32 +46,35 @@ import java.util.stream.Collectors;
 @Component
 public class JobSchedulerInfoContributor implements InfoContributor {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(JobSchedulerInfoContributor.class);
+
 	@Autowired
 	private Scheduler scheduler;
 
 	@Override
 	public void contribute(Info.Builder builder) {
 		try {
-			List<String> triggerGroupNames = scheduler.getTriggerGroupNames();
-			List<JobKey> jobKeys = triggerGroupNames.stream().flatMap(name -> {
+			Map<String, Map<String, Long>> triggersInfo = scheduler.getTriggerGroupNames().stream().flatMap(name -> {
 				try {
 					return scheduler.getJobKeys(GroupMatcher.groupEquals(name)).stream();
 				} catch (SchedulerException e) {
-					throw new ReportPortalException(e.getMessage());
+					LOGGER.warn(e.getMessage());
+					return Stream.empty();
 				}
-			}).collect(Collectors.toList());
-			Map<String, Map<String, Long>> triggersInfo = jobKeys.stream().flatMap(key -> {
+			}).collect(Collectors.toList()).stream().flatMap(key -> {
 				try {
 					return scheduler.getTriggersOfJob(key).stream();
 				} catch (SchedulerException e) {
-					throw new ReportPortalException(e.getMessage());
+					LOGGER.warn(e.getMessage());
+					return Stream.empty();
 				}
 			}).collect(Collectors.toMap(
 					t -> t.getKey().getName(),
 					t -> Collections.singletonMap("triggersIn", t.getNextFireTime().getTime() - System.currentTimeMillis())
 			));
-
-			builder.withDetail("jobs", triggersInfo);
+			if (MapUtils.isNotEmpty(triggersInfo)) {
+				builder.withDetail("jobs", triggersInfo);
+			}
 		} catch (SchedulerException e) {
 			throw new ReportPortalException(e.getMessage());
 		}

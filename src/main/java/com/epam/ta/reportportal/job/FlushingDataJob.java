99e3f9400e2760e39072cd1f631e7fdadac6ad81
pbortnik <rrrizon@gmail.com>
2018-10-02 11:41:01 +0300

    add missed flushing for some repos

--- src/main/java/com/epam/ta/reportportal/job/FlushingDataJob.java
+++ src/main/java/com/epam/ta/reportportal/job/FlushingDataJob.java
@@ -1,5 +1,6 @@
 package com.epam.ta.reportportal.job;
 
+import com.epam.ta.reportportal.core.analyzer.ILogIndexer;
 import com.epam.ta.reportportal.database.DataStorage;
 import com.epam.ta.reportportal.database.dao.*;
 import com.epam.ta.reportportal.events.handler.AddDemoProjectEventHandler;
@@ -8,10 +9,11 @@ import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Profile("demo")
@@ -33,7 +35,10 @@ public class FlushingDataJob implements Job {
 	private UserRepository userRepository;
 
 	@Autowired
-	private ApplicationContext applicationContext;
+	private UserPreferenceRepository preferenceRepository;
+
+	@Autowired
+	private ILogIndexer logIndexer;
 
 	@Autowired
 	private AddDemoProjectEventHandler addDemoProjectEventHandler;
@@ -49,12 +54,21 @@ public class FlushingDataJob implements Job {
 	@Override
 	public void execute(JobExecutionContext context) {
 		LOGGER.info("Start flushing all existing data!");
+
 		mongoOperations.dropCollection("launchMetaInfo");
+		mongoOperations.dropCollection("oauth_access_token");
+		mongoOperations.dropCollection("oauth_refresh_token");
+		mongoOperations.dropCollection("sessions");
+
 		passwordBidRepository.deleteAll();
 		creationBidRepository.deleteAll();
+
+		List<String> allProjectNames = projectRepository.findAllProjectNames();
+		projectRepository.delete(allProjectNames);
+		allProjectNames.forEach(name -> logIndexer.deleteIndex(name));
 		serverSettingsRepository.deleteAll();
-		projectRepository.delete(projectRepository.findAllProjectNames());
 		userRepository.deleteAll();
+		preferenceRepository.deleteAll();
 		dataStorage.deleteAll();
 		addDemoProjectEventHandler.onApplicationEvent(null);
 		LOGGER.info("Finish flushing all existing data!");

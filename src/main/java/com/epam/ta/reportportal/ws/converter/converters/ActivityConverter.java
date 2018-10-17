5baf958171bd3dcc8dd0257d37a7c651e5197e05
ihar-kahadouski <igor.kagadowski@gmail.com>
2018-10-17 18:21:37 +0300

    Use converter from the EntityUtils

--- src/main/java/com/epam/ta/reportportal/ws/converter/converters/ActivityConverter.java
+++ src/main/java/com/epam/ta/reportportal/ws/converter/converters/ActivityConverter.java
@@ -3,9 +3,9 @@ package com.epam.ta.reportportal.ws.converter.converters;
 import com.epam.ta.reportportal.entity.Activity;
 import com.epam.ta.reportportal.ws.model.ActivityResource;
 
-import java.sql.Date;
-import java.time.ZoneId;
 import java.util.function.Function;
+
+import static com.epam.ta.reportportal.commons.EntityUtils.TO_DATE;
 
 public final class ActivityConverter {
 
@@ -16,8 +16,8 @@ public final class ActivityConverter {
 	public static final Function<Activity, ActivityResource> TO_RESOURCE = activity -> {
 		ActivityResource resource = new ActivityResource();
 		resource.setActivityId(activity.getId().toString());
-		resource.setLastModifiedDate(Date.from(activity.getCreatedAt().atZone(ZoneId.systemDefault()).toInstant()));
-		resource.setObjectType(activity.getEntity().name());
+		resource.setLastModifiedDate(TO_DATE.apply(activity.getCreatedAt()));
+		resource.setObjectType(activity.getActivityEntityType().name());
 		resource.setActionType(activity.getAction());
 		resource.setProjectRef(activity.getProjectId().toString());
 		resource.setUserRef(activity.getUserId().toString());

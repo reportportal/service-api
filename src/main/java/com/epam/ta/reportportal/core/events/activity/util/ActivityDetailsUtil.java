2f4e692480d6c21dbcef520928e48948f6bc4323
ihar-kahadouski <igor.kagadowski@gmail.com>
2018-10-18 18:54:56 +0300

    Fix headers

--- src/main/java/com/epam/ta/reportportal/core/events/activity/util/ActivityDetailsUtil.java
+++ src/main/java/com/epam/ta/reportportal/core/events/activity/util/ActivityDetailsUtil.java
@@ -1,5 +1,23 @@
+/*
+ * Copyright 2018 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.epam.ta.reportportal.core.events.activity.util;
 
+/**
+ * @author Ihar Kahadouski
+ */
 import com.epam.ta.reportportal.entity.ActivityDetails;
 import com.epam.ta.reportportal.entity.HistoryField;
 import com.google.common.base.Strings;

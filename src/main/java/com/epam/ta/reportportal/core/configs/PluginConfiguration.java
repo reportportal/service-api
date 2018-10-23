aceff397eadae776ca8e92dabbde6f9edcd2d73c
avarabyeu <andrei.varabyeu@gmail.com>
2018-10-23 12:14:45 +0300

    fix issue with incorrect plugin manager bootstrap
    Signed-off-by: avarabyeu <andrei.varabyeu@gmail.com>

--- src/main/java/com/epam/ta/reportportal/core/configs/PluginConfiguration.java
+++ src/main/java/com/epam/ta/reportportal/core/configs/PluginConfiguration.java
@@ -2,15 +2,25 @@ package com.epam.ta.reportportal.core.configs;
 
 import com.epam.ta.reportportal.core.plugin.PluginBox;
 import com.epam.ta.reportportal.plugin.P4jPluginManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PluginConfiguration {
 
+	@Autowired
+	private AutowireCapableBeanFactory context;
+
+	@Autowired
+	@Value("${rp.plugins.path}")
+	private String pluginsPath;
+
 	@Bean
-	public PluginBox p4jPluginBox(){
-		P4jPluginManager manager = new P4jPluginManager();
+	public PluginBox p4jPluginBox() {
+		P4jPluginManager manager = new P4jPluginManager(pluginsPath, context);
 		manager.startAsync();
 		return manager;
 	}

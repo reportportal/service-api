4a502a04d0f37b4d84fca5a96e3ecbd704869201
yumfriez <budaevqwerty@gmail.com>
2018-11-21 13:11:19 +0300

    TestItemConverter retries support has been implemented

--- src/main/java/com/epam/ta/reportportal/ws/converter/converters/TestItemConverter.java
+++ src/main/java/com/epam/ta/reportportal/ws/converter/converters/TestItemConverter.java
@@ -22,9 +22,10 @@ import com.epam.ta.reportportal.entity.item.TestItemTag;
 import com.epam.ta.reportportal.ws.model.TestItemResource;
 import com.epam.ta.reportportal.ws.model.activity.TestItemActivityResource;
 
-import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static java.util.Optional.ofNullable;
 
 /**
  * Converts internal DB model to DTO
@@ -47,7 +48,7 @@ public final class TestItemConverter {
 		if (null != item.getParameters()) {
 			resource.setParameters(item.getParameters().stream().map(ParametersConverter.TO_RESOURCE).collect(Collectors.toList()));
 		}
-		Optional.ofNullable(item.getItemResults().getIssue()).ifPresent(i -> resource.setIssue(IssueConverter.TO_MODEL.apply(i)));
+		ofNullable(item.getItemResults().getIssue()).ifPresent(i -> resource.setIssue(IssueConverter.TO_MODEL.apply(i)));
 		resource.setName(item.getName());
 		resource.setStartTime(EntityUtils.TO_DATE.apply(item.getStartTime()));
 		resource.setStatus(item.getItemResults().getStatus() != null ? item.getItemResults().getStatus().toString() : null);
@@ -59,9 +60,10 @@ public final class TestItemConverter {
 		if (item.getParent() != null) {
 			resource.setParent(item.getParent().getItemId());
 		}
-		resource.setLaunchId(item.getLaunch().getId());
+		ofNullable(item.getLaunch()).ifPresent(l -> resource.setLaunchId(l.getId()));
 		resource.setPath(item.getPath());
 		resource.setStatisticsResource(StatisticsConverter.TO_RESOURCE.apply(item.getItemResults().getStatistics()));
+		resource.setRetries(item.getRetries().stream().map(TestItemConverter.TO_RESOURCE).collect(Collectors.toList()));
 		return resource;
 	};
 

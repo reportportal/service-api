dcc1cbc5be3212b1ac9c497610bb22043a89bf47
ihar-kahadouski <igor.kagadowski@gmail.com>
2018-11-24 15:46:55 +0300

    Update last launch and activities widgets

--- src/main/java/com/epam/ta/reportportal/core/project/impl/GetProjectInfoHandlerImpl.java
+++ src/main/java/com/epam/ta/reportportal/core/project/impl/GetProjectInfoHandlerImpl.java
@@ -30,15 +30,13 @@ import com.epam.ta.reportportal.entity.enums.LaunchModeEnum;
 import com.epam.ta.reportportal.entity.launch.Launch;
 import com.epam.ta.reportportal.entity.project.Project;
 import com.epam.ta.reportportal.entity.project.email.ProjectInfoWidget;
-import com.epam.ta.reportportal.entity.statistics.Statistics;
 import com.epam.ta.reportportal.exception.ReportPortalException;
 import com.epam.ta.reportportal.ws.converter.PagedResourcesAssembler;
 import com.epam.ta.reportportal.ws.converter.converters.ProjectConverter;
+import com.epam.ta.reportportal.ws.model.ActivityResource;
 import com.epam.ta.reportportal.ws.model.launch.Mode;
 import com.epam.ta.reportportal.ws.model.project.ProjectInfoResource;
-import com.epam.ta.reportportal.ws.model.widget.ChartObject;
 import com.google.common.collect.Sets;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -59,10 +57,8 @@ import static com.epam.ta.reportportal.commons.querygen.constant.ActivityCriteri
 import static com.epam.ta.reportportal.commons.querygen.constant.ActivityCriteriaConstant.CRITERIA_CREATION_DATE;
 import static com.epam.ta.reportportal.commons.querygen.constant.GeneralCriteriaConstant.CRITERIA_PROJECT_ID;
 import static com.epam.ta.reportportal.core.events.activity.ActivityAction.*;
-import static com.epam.ta.reportportal.dao.constant.WidgetContentRepositoryConstants.*;
-import static com.epam.ta.reportportal.ws.model.ErrorType.BAD_REQUEST_ERROR;
-import static com.epam.ta.reportportal.ws.model.ErrorType.PROJECT_NOT_FOUND;
-import static java.util.Optional.ofNullable;
+import static com.epam.ta.reportportal.ws.converter.converters.ActivityConverter.TO_RESOURCE;
+import static com.epam.ta.reportportal.ws.model.ErrorType.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
@@ -118,8 +114,7 @@ public class GetProjectInfoHandlerImpl implements GetProjectInfoHandler {
 	}
 
 	@Override
-	public Map<String, List<ChartObject>> getProjectInfoWidgetContent(ReportPortalUser.ProjectDetails projectDetails, String interval,
-			String widgetCode) {
+	public Map<String, ?> getProjectInfoWidgetContent(ReportPortalUser.ProjectDetails projectDetails, String interval, String widgetCode) {
 		Project project = projectRepository.findById(projectDetails.getProjectId())
 				.orElseThrow(() -> new ReportPortalException(PROJECT_NOT_FOUND, projectDetails.getProjectId()));
 		InfoInterval infoInterval = InfoInterval.findByInterval(interval)
@@ -132,7 +127,7 @@ public class GetProjectInfoHandlerImpl implements GetProjectInfoHandler {
 				LaunchModeEnum.DEFAULT
 		);
 
-		Map<String, List<ChartObject>> result;
+		Map<String, ?> result;
 
 		switch (widgetType) {
 			case INVESTIGATED:
@@ -161,42 +156,9 @@ public class GetProjectInfoHandlerImpl implements GetProjectInfoHandler {
 		return result;
 	}
 
-	private Map<String, List<ChartObject>> getLastLaunchStatistics(Long projectId) {
+	private Map<String, ?> getLastLaunchStatistics(Long projectId) {
 		Optional<Launch> launchOptional = launchRepository.findLastRun(projectId, Mode.DEFAULT.name());
-		if (!launchOptional.isPresent()) {
-			return Collections.emptyMap();
-		}
-		Launch lastLaunch = launchOptional.get();
-		Set<Statistics> statistics = lastLaunch.getStatistics();
-
-		ChartObject chartObject = new ChartObject();
-		chartObject.setValues(new HashMap<String, String>() {
-			{
-				put(EXECUTIONS_TOTAL, getStatisticsCount(statistics, EXECUTIONS_TOTAL).orElse(0).toString());
-				put(EXECUTIONS_FAILED, getStatisticsCount(statistics, EXECUTIONS_FAILED).orElse(0).toString());
-				put(EXECUTIONS_PASSED, getStatisticsCount(statistics, EXECUTIONS_PASSED).orElse(0).toString());
-				put(EXECUTIONS_SKIPPED, getStatisticsCount(statistics, EXECUTIONS_SKIPPED).orElse(0).toString());
-				put(DEFECTS_AUTOMATION_BUG_TOTAL, getStatisticsCount(statistics, DEFECTS_PRODUCT_BUG_TOTAL).orElse(0).toString());
-				put(DEFECTS_TO_INVESTIGATE_TOTAL, getStatisticsCount(statistics, DEFECTS_TO_INVESTIGATE_TOTAL).orElse(0).toString());
-				put(DEFECTS_PRODUCT_BUG_TOTAL, getStatisticsCount(statistics, DEFECTS_PRODUCT_BUG_TOTAL).orElse(0).toString());
-				put(DEFECTS_SYSTEM_ISSUE_TOTAL, getStatisticsCount(statistics, DEFECTS_SYSTEM_ISSUE_TOTAL).orElse(0).toString());
-			}
-
-		});
-		chartObject.setName(lastLaunch.getName());
-		chartObject.setId(String.valueOf(lastLaunch.getId()));
-		chartObject.setNumber(String.valueOf(lastLaunch.getNumber()));
-		chartObject.setStartTime(String.valueOf(Timestamp.valueOf(LocalDateTime.now()).getTime()));
-
-		return Collections.singletonMap("result", Collections.singletonList(chartObject));
-	}
-
-	private Optional<Integer> getStatisticsCount(Set<Statistics> statistics, String param) {
-		return statistics.stream()
-				.filter(it -> ofNullable(it.getStatisticsField()).isPresent() && StringUtils.isNotEmpty(it.getStatisticsField().getName()))
-				.filter(it -> it.getCounter() > 0 && it.getStatisticsField().getName().equals(param))
-				.map(Statistics::getCounter)
-				.findFirst();
+		return launchOptional.isPresent() ? Collections.singletonMap("result", launchOptional.get()) : Collections.emptyMap();
 	}
 
 	/**
@@ -212,13 +174,14 @@ public class GetProjectInfoHandlerImpl implements GetProjectInfoHandler {
 	private static final Predicate<ActivityAction> ACTIVITIES_PROJECT_FILTER = it -> it == UPDATE_DEFECT || it == DELETE_DEFECT
 			|| it == LINK_ISSUE || it == LINK_ISSUE_AA || it == UNLINK_ISSUE || it == UPDATE_ITEM;
 
-	private Map<String, List<ChartObject>> getActivities(Long projectId, InfoInterval infoInterval) {
+	private Map<String, List<ActivityResource>> getActivities(Long projectId, InfoInterval infoInterval) {
 		String value = Arrays.stream(ActivityAction.values())
 				.filter(not(ACTIVITIES_PROJECT_FILTER))
 				.map(ActivityAction::getValue)
 				.collect(joining(","));
 		Filter filter = new Filter(Activity.class, Sets.newHashSet(new FilterCondition(IN, false, value, CRITERIA_ACTION),
-				new FilterCondition(EQUALS, false, String.valueOf(projectId), CRITERIA_PROJECT_ID), new FilterCondition(
+				new FilterCondition(EQUALS, false, String.valueOf(projectId), CRITERIA_PROJECT_ID),
+				new FilterCondition(
 						GREATER_THAN_OR_EQUALS,
 						false,
 						String.valueOf(Timestamp.valueOf(getStartIntervalDate(infoInterval)).getTime()),
@@ -228,33 +191,11 @@ public class GetProjectInfoHandlerImpl implements GetProjectInfoHandler {
 		List<Activity> activities = activityRepository.findByFilter(filter, PageRequest.of(0, LIMIT, Sort.by(Sort.Direction.DESC,
 				filter.getTarget()
 						.getCriteriaByFilter(CRITERIA_CREATION_DATE)
-						.orElseThrow(() -> new ReportPortalException(""))
+						.orElseThrow(() -> new ReportPortalException(UNCLASSIFIED_REPORT_PORTAL_ERROR))
 						.getQueryCriteria()
 		))).getContent();
 
-		List<ChartObject> chartObjects = activities.stream().map(it -> {
-			ChartObject chartObject = new ChartObject();
-			chartObject.setId(String.valueOf(it.getId()));
-			HashMap<String, String> values = new HashMap<>();
-			values.put("actionType", it.getAction());
-			values.put("lastModified", String.valueOf(Timestamp.valueOf(it.getCreatedAt()).getTime()));
-			values.put("objectType", it.getActivityEntityType().name());
-			values.put("projectRef", String.valueOf(it.getProjectId()));
-			values.put("userRef", String.valueOf(it.getUserId()));
-			if (it.getObjectId() != null) {
-				values.put("loggedObjectRef", String.valueOf(it.getObjectId()));
-			}
-			if (it.getDetails().getObjectName() != null) {
-				values.put("name", it.getDetails().getObjectName());
-			}
-			it.getDetails().getHistory().forEach(val -> {
-				values.put(val.getField() + "$oldValue", val.getOldValue());
-				values.put(val.getField() + "$newValue", val.getNewValue());
-			});
-			chartObject.setValues(values);
-			return chartObject;
-		}).collect(toList());
-		return Collections.singletonMap("result", chartObjects);
+		return Collections.singletonMap("result", activities.stream().map(TO_RESOURCE).collect(toList()));
 	}
 
 }

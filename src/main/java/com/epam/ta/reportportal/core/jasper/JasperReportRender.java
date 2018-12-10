69b0030a5fd4093a0465f5137ec022df0c0f6255
ihar-kahadouski <igor.kagadowski@gmail.com>
2018-12-10 13:42:34 +0300

    initial

---
+++ src/main/java/com/epam/ta/reportportal/core/jasper/JasperReportRender.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2016 EPAM Systems
+ *
+ *
+ * This file is part of EPAM Report Portal.
+ * https://github.com/reportportal/service-api
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * This file is part of Report Portal.
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.epam.ta.reportportal.core.jasper;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+/**
+ * Jasper Report render based on provided JRXML template.<br>
+ *
+ * @author Andrei_Ramanchuk
+ * @author Andrei Varabyeu
+ * Performance improvements. Load JasperReport only once since it is immutable
+ */
+@Service("jasperRender")
+class JasperReportRender {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(JasperReportRender.class);
+	private static final String REPORT_JRXML_TEMPLATE = "classpath:/templates/report/report.jrxml";
+
+	private JasperReport jasperReport;
+
+	@Autowired
+	public JasperReportRender(ResourceLoader resourceLoader) throws JRException, IOException {
+		Resource reportTemplate = resourceLoader.getResource(REPORT_JRXML_TEMPLATE);
+		com.google.common.base.Preconditions.checkArgument(reportTemplate.exists());
+		InputStream inputStream = reportTemplate.getInputStream();
+		JasperDesign jasperDesign = JRXmlLoader.load(inputStream);
+		this.jasperReport = JasperCompileManager.compileReport(jasperDesign);
+
+	}
+
+	JasperPrint generateReportPrint(Map<String, Object> params, JRDataSource datasource) {
+		try {
+			return JasperFillManager.fillReport(jasperReport, params, datasource);
+		} catch (JRException e) {
+			LOGGER.error("Unable to generate Report", e);
+			return new JasperPrint();
+		}
+	}
+}
---
+++ src/main/java/com/epam/ta/reportportal/core/jasper/TestItemPojo.java
@@ -0,0 +1,216 @@
+
+/*
+ * Copyright 2018 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.ta.reportportal.core.jasper;
+
+import com.epam.ta.reportportal.entity.item.TestItem;
+import com.epam.ta.reportportal.entity.statistics.Statistics;
+
+import java.time.Duration;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.Set;
+
+import static com.epam.ta.reportportal.core.events.activity.util.ActivityDetailsUtil.EMPTY_STRING;
+import static com.epam.ta.reportportal.dao.constant.WidgetContentRepositoryConstants.*;
+import static java.util.Optional.ofNullable;
+
+/**
+ * Jasper Reports collection {@link TestItem} POJO
+ *
+ * @author Andrei_Ramanchuk
+ */
+public class TestItemPojo {
+	private String type;
+	private String name;
+	private String status;
+	private Double duration;
+	private Integer total;
+	private Integer passed;
+	private Integer failed;
+	private Integer skipped;
+	private Integer automationBug;
+	private Integer productBug;
+	private Integer systemIssue;
+	private Integer noDefect;
+	private Integer toInvestigate;
+
+	public TestItemPojo(TestItem input) {
+		this.type = input.getType().name();
+		Optional<String> issueDescription = Optional.empty();
+		if (input.getItemResults().getIssue() != null) {
+			issueDescription = ofNullable(input.getItemResults().getIssue().getIssueDescription()).map(it -> "\r\n" + " DEFECT COMMENT: "
+					+ it);
+		}
+
+		Optional<String> description = ofNullable(input.getDescription()).map(it -> "\r\n" + " ITEM DESCRIPTION: " + it);
+
+		this.name = input.getName() + description.orElse(EMPTY_STRING) + issueDescription.orElse(EMPTY_STRING);
+		this.status = input.getItemResults().getStatus().name();
+
+		this.duration = Duration.between(input.getStartTime(), input.getItemResults().getEndTime()).toMillis()
+				/ (double) org.apache.commons.lang.time.DateUtils.MILLIS_PER_SECOND;
+
+		Set<Statistics> statistics = input.getItemResults().getStatistics();
+
+		this.total = getStatisticsCounter(statistics, EXECUTIONS_TOTAL).orElse(0);
+		this.passed = getStatisticsCounter(statistics, EXECUTIONS_PASSED).orElse(0);
+		this.failed = getStatisticsCounter(statistics, EXECUTIONS_FAILED).orElse(0);
+		this.skipped = getStatisticsCounter(statistics, EXECUTIONS_SKIPPED).orElse(0);
+
+		this.automationBug = getStatisticsCounter(statistics, DEFECTS_AUTOMATION_BUG_TOTAL).orElse(0);
+		this.productBug = getStatisticsCounter(statistics, DEFECTS_PRODUCT_BUG_TOTAL).orElse(0);
+		this.systemIssue = getStatisticsCounter(statistics, DEFECTS_SYSTEM_ISSUE_TOTAL).orElse(0);
+		this.noDefect = getStatisticsCounter(statistics, DEFECTS_NO_DEFECT_TOTAL).orElse(0);
+		this.toInvestigate = getStatisticsCounter(statistics, DEFECTS_TO_INVESTIGATE_TOTAL).orElse(0);
+	}
+
+	private OptionalInt getStatisticsCounter(Set<Statistics> statistics, String statisticsFieldName) {
+		return statistics.stream()
+				.filter(it -> it.getStatisticsField().getName().equals(statisticsFieldName))
+				.mapToInt(Statistics::getCounter)
+				.findAny();
+	}
+
+	public void setType(String value) {
+		this.type = value;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setName(String value) {
+		this.name = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setStatus(String value) {
+		this.status = value;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setTotal(Integer value) {
+		this.total = value;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setPased(Integer value) {
+		this.passed = value;
+	}
+
+	public Integer getPassed() {
+		return passed;
+	}
+
+	public void setFailed(Integer value) {
+		this.failed = value;
+	}
+
+	public Integer getFailed() {
+		return failed;
+	}
+
+	public void setSkipped(Integer value) {
+		this.skipped = value;
+	}
+
+	public Integer getSkipped() {
+		return skipped;
+	}
+
+	public void setAutomationBug(Integer value) {
+		this.automationBug = value;
+	}
+
+	public Integer getAutomationBug() {
+		return automationBug;
+	}
+
+	public void setProductBug(Integer value) {
+		this.productBug = value;
+	}
+
+	public Integer getProductBug() {
+		return productBug;
+	}
+
+	public void setSystemIssue(Integer value) {
+		this.systemIssue = value;
+	}
+
+	public Integer getSystemIssue() {
+		return systemIssue;
+	}
+
+	public void setNoDefect(Integer value) {
+		this.noDefect = value;
+	}
+
+	public Integer getNoDefect() {
+		return noDefect;
+	}
+
+	public void setToInvestigate(Integer value) {
+		this.toInvestigate = value;
+	}
+
+	public Integer getToInvestigate() {
+		return toInvestigate;
+	}
+
+	public Double getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Double duration) {
+		this.duration = duration;
+	}
+
+	public void setPassed(Integer passed) {
+		this.passed = passed;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("TestItemPojo{");
+		sb.append("type='").append(type).append('\'');
+		sb.append(", name='").append(name).append('\'');
+		sb.append(", status='").append(status).append('\'');
+		sb.append(", duration=").append(duration);
+		sb.append(", total=").append(total);
+		sb.append(", passed=").append(passed);
+		sb.append(", failed=").append(failed);
+		sb.append(", skipped=").append(skipped);
+		sb.append(", automationBug=").append(automationBug);
+		sb.append(", productBug=").append(productBug);
+		sb.append(", systemIssue=").append(systemIssue);
+		sb.append(", noDefect=").append(noDefect);
+		sb.append(", toInvestigate=").append(toInvestigate);
+		sb.append('}');
+		return sb.toString();
+	}
+}

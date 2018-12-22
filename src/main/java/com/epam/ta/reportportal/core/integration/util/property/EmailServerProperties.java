3bd80785637cbac48744535fcb434e7cf79243e8
yumfriez <budaevqwerty@gmail.com>
2018-12-22 17:34:26 +0300

    Redundant EmailServerProperties.class has been removed

--- src/main/java/com/epam/ta/reportportal/core/integration/util/property/EmailServerProperties.java
+++
@@ -1,7 +0,0 @@
-package com.epam.ta.reportportal.core.integration.util.property;
-
-/**
- * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
- */
-public class EmailServerProperties {
-}

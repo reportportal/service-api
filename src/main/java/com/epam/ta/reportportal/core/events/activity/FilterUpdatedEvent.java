4822170f0a217bc2e1311d0031cd45aaf498e10c
ihar-kahadouski <igor.kagadowski@gmail.com>
2018-12-22 12:01:51 +0300

    Merge branch 'ps-migrations' of github.com:reportportal/service-api into unit-tests
    # Conflicts:
    #	build.gradle
    #	src/main/java/com/epam/ta/reportportal/core/events/activity/FilterUpdatedEvent.java

--- src/main/java/com/epam/ta/reportportal/core/events/activity/FilterUpdatedEvent.java
+++ src/main/java/com/epam/ta/reportportal/core/events/activity/FilterUpdatedEvent.java
@@ -21,9 +21,7 @@ import com.epam.ta.reportportal.entity.activity.Activity;
 import com.epam.ta.reportportal.ws.converter.builders.ActivityBuilder;
 import com.epam.ta.reportportal.ws.model.activity.UserFilterActivityResource;
 
-import static com.epam.ta.reportportal.core.events.activity.util.ActivityDetailsUtil.processDescription;
-import static com.epam.ta.reportportal.core.events.activity.util.ActivityDetailsUtil.processName;
-import static com.epam.ta.reportportal.core.events.activity.util.ActivityDetailsUtil.processShared;
+import static com.epam.ta.reportportal.core.events.activity.util.ActivityDetailsUtil.*;
 import static com.epam.ta.reportportal.entity.activity.Activity.ActivityEntityType.FILTER;
 import static com.epam.ta.reportportal.entity.activity.ActivityAction.UPDATE_FILTER;
 

4c5ca1826be86d98d0bb27c79c074d5ccf65d67b
pavel_bortnik <pavel_bortnik@epam.com>
2019-01-25 18:06:45 +0300

    more mock tests

--- src/test/java/com/epam/ta/reportportal/core/analyzer/impl/LogIndexerServiceTest.java
+++ src/test/java/com/epam/ta/reportportal/core/analyzer/impl/LogIndexerServiceTest.java
@@ -23,6 +23,7 @@ import com.epam.ta.reportportal.core.analyzer.model.IndexRsItem;
 import com.epam.ta.reportportal.dao.LaunchRepository;
 import com.epam.ta.reportportal.dao.LogRepository;
 import com.epam.ta.reportportal.dao.TestItemRepository;
+import com.epam.ta.reportportal.entity.enums.LaunchModeEnum;
 import com.epam.ta.reportportal.entity.enums.LogLevel;
 import com.epam.ta.reportportal.entity.enums.TestItemIssueGroup;
 import com.epam.ta.reportportal.entity.item.TestItem;
@@ -44,7 +45,6 @@ import java.util.Optional;
 
 import static com.epam.ta.reportportal.entity.AnalyzeMode.ALL_LAUNCHES;
 import static com.epam.ta.reportportal.entity.enums.TestItemIssueGroup.PRODUCT_BUG;
-import static com.epam.ta.reportportal.entity.enums.TestItemIssueGroup.TO_INVESTIGATE;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
 
@@ -94,21 +94,23 @@ public class LogIndexerServiceTest {
 		verifyZeroInteractions(logRepository, analyzerServiceClient);
 	}
 
-	//
-	//	@Test
-	//	public void testIndexLog() {
-	//		Log log = createLog("3");
-	//		TestItem ti = createTestItem("3");
-	//		Launch launch = createLaunch("3");
-	//		when(testItemRepository.findOne(eq(log.getTestItemRef()))).thenReturn(ti);
-	//		when(launchRepository.findOne(eq(ti.getLaunchRef()))).thenReturn(launch);
-	//		logIndexerService.indexLog(log);
-	//		verify(testItemRepository).findOne(eq(log.getTestItemRef()));
-	//		verify(launchRepository).findOne(eq(ti.getLaunchRef()));
-	//		verify(analyzerServiceClient).index(anyListOf(IndexLaunch.class));
-	//		verifyZeroInteractions(mongoOperations, logRepository);
-	//	}
-	//
+	@Test
+	public void testIndexLog() {
+		Log log = createLog(3L);
+		TestItem ti = createTestItem(3L, PRODUCT_BUG);
+		Launch launch = createLaunch(3L);
+		ti.setLaunch(launch);
+		log.setTestItem(ti);
+		when(testItemRepository.findById(log.getId())).thenReturn(Optional.of(ti));
+		when(launchRepository.findById(launch.getId())).thenReturn(Optional.of(launch));
+		logIndexerService.indexLog(log);
+
+		verify(testItemRepository).findById(ti.getItemId());
+		verify(launchRepository).findById(launch.getId());
+		verify(analyzerServiceClient).index(any());
+		verifyZeroInteractions(logRepository);
+	}
+
 	@Test
 	public void testIndexLogsWithNonExistentLaunchId() {
 		Long launchId = 1L;
@@ -125,136 +127,6 @@ public class LogIndexerServiceTest {
 		verifyZeroInteractions(testItemRepository, logRepository);
 	}
 
-	//
-	//	@Test
-	//	public void testIndexLogsTestItemsWithoutLogs() {
-	//		String launchId = "3";
-	//		when(launchRepository.findOne(eq(launchId))).thenReturn(createLaunch(launchId));
-	//		when(logRepository.findGreaterOrEqualLevel(anyListOf(String.class), eq(LogLevel.ERROR))).thenReturn(Collections.emptyList());
-	//		int testItemCount = 10;
-	//		logIndexerService.indexLogs(launchId, createTestItems(testItemCount));
-	//		verify(logRepository, times(testItemCount)).findGreaterOrEqualLevel(anyListOf(String.class), eq(LogLevel.ERROR));
-	//		verifyZeroInteractions(mongoOperations, analyzerServiceClient);
-	//	}
-	//
-	//	@Test
-	//	public void testIndexLogs() {
-	//		String launchId = "4";
-	//		Launch launch = createLaunch(launchId);
-	//		when(launchRepository.findOne(eq(launchId))).thenReturn(launch);
-	//		when(logRepository.findGreaterOrEqualLevel(anyListOf(String.class), eq(LogLevel.ERROR))).thenReturn(
-	//				Collections.singletonList(createLog("id")));
-	//		int testItemCount = 2;
-	//		when(projectRepository.findOne(launch.getProjectRef())).thenReturn(new Project());
-	//		when(analyzerServiceClient.index(anyListOf(IndexLaunch.class))).thenReturn(Collections.singletonList(createIndexRs(testItemCount)));
-	//		logIndexerService.indexLogs(launchId, createTestItems(testItemCount));
-	//		verify(logRepository, times(testItemCount)).findGreaterOrEqualLevel(anyListOf(String.class), eq(LogLevel.ERROR));
-	//		verify(analyzerServiceClient).index(anyListOf(IndexLaunch.class));
-	//		verifyZeroInteractions(mongoOperations);
-	//	}
-	//
-	//	@Test
-	//	public void testIndexWithIgnoreFlag() {
-	//		String launchId = "5";
-	//		TestItem testItem = createTestItem("id");
-	//		TestItemIssue issue = testItem.getIssue();
-	//		issue.setIgnoreAnalyzer(true);
-	//		when(launchRepository.findOne(eq(launchId))).thenReturn(createLaunch(launchId));
-	//		logIndexerService.indexLogs(launchId, Collections.singletonList(testItem));
-	//		verifyZeroInteractions(mongoOperations, testItemRepository, logRepository, analyzerServiceClient);
-	//	}
-	//
-	//	@Test
-	//
-	//	public void testIndexInDebug() {
-	//		String launchId = "6";
-	//		Launch launch = createLaunch(launchId);
-	//		launch.setMode(Mode.DEBUG);
-	//		when(launchRepository.findOne(eq(launchId))).thenReturn(launch);
-	//		logIndexerService.indexLogs(launchId, Collections.emptyList());
-	//		verifyZeroInteractions(mongoOperations, testItemRepository, logRepository, analyzerServiceClient);
-	//	}
-	//
-	//	@Test(expected = ReportPortalException.class)
-	//	public void testIndexAllLogsNegative() {
-	//		DBCollection checkpointColl = mock(DBCollection.class);
-	//		when(mongoOperations.getCollection(eq("logIndexingCheckpoint"))).thenReturn(checkpointColl);
-	//		when(analyzerServiceClient.hasClients()).thenReturn(false);
-	//		logIndexerService.indexAllLogs();
-	//		verify(analyzerServiceClient, times(1)).hasClients();
-	//		verifyZeroInteractions(testItemRepository, launchRepository, logRepository);
-	//	}
-	//
-	//	@Test
-	//	public void testIndexAllLogsWithoutLogs() {
-	//		DBCollection checkpointColl = mock(DBCollection.class);
-	//		when(mongoOperations.getCollection(eq("logIndexingCheckpoint"))).thenReturn(checkpointColl);
-	//		when(checkpointColl.findOne(any(Query.class))).thenReturn(null);
-	//		when(mongoOperations.stream(any(Query.class), eq(Log.class))).thenReturn(createLogIterator(0));
-	//		when(analyzerServiceClient.hasClients()).thenReturn(true);
-	//		logIndexerService.indexAllLogs();
-	//		verifyZeroInteractions(launchRepository, testItemRepository);
-	//		verify(analyzerServiceClient, times(0)).index(any());
-	//		verify(analyzerServiceClient, times(1)).hasClients();
-	//	}
-	//
-	//	@Test
-	//	public void testIndexAllLogsWithoutLaunches() {
-	//		DBCollection checkpointColl = mock(DBCollection.class);
-	//		when(mongoOperations.getCollection(eq("logIndexingCheckpoint"))).thenReturn(checkpointColl);
-	//		when(checkpointColl.findOne(any(Query.class))).thenReturn(null);
-	//		when(mongoOperations.stream(any(Query.class), eq(Log.class))).thenReturn(createLogIterator(0));
-	//		when(launchRepository.findOne(anyString())).thenReturn(null);
-	//		when(analyzerServiceClient.hasClients()).thenReturn(true);
-	//		logIndexerService.indexAllLogs();
-	//		verifyZeroInteractions(testItemRepository);
-	//		verify(analyzerServiceClient, times(0)).index(any());
-	//		verify(analyzerServiceClient, times(1)).hasClients();
-	//	}
-	//
-	//	@Test
-	//	public void testIndexAllLogsWithoutTestItems() {
-	//		DBCollection checkpointColl = mock(DBCollection.class);
-	//		when(mongoOperations.getCollection(eq("logIndexingCheckpoint"))).thenReturn(checkpointColl);
-	//		when(checkpointColl.findOne(any(Query.class))).thenReturn(null);
-	//		when(mongoOperations.stream(any(Query.class), eq(Log.class))).thenReturn(createLogIterator(0));
-	//		when(launchRepository.findOne(anyString())).thenReturn(createLaunch("launchId"));
-	//		when(testItemRepository.findOne(anyString())).thenReturn(createTestItem("testItemId"));
-	//		when(analyzerServiceClient.hasClients()).thenReturn(true);
-	//		logIndexerService.indexAllLogs();
-	//		verify(analyzerServiceClient, times(1)).hasClients();
-	//		verify(analyzerServiceClient, times(0)).index(any());
-	//	}
-	//
-	//	@Test
-	//	public void testIndexAllLogs() {
-	//		DBCollection checkpointColl = mock(DBCollection.class);
-	//		when(mongoOperations.getCollection(eq("logIndexingCheckpoint"))).thenReturn(checkpointColl);
-	//		when(checkpointColl.findOne(any(Query.class))).thenReturn(null);
-	//		int batchCount = 5;
-	//		int logCount = batchCount * BATCH_SIZE;
-	//		when(mongoOperations.stream(any(Query.class), eq(Log.class))).thenReturn(createLogIterator(logCount));
-	//		when(launchRepository.findOne(anyString())).thenReturn(createLaunch("launchId"));
-	//		when(testItemRepository.findOne(anyString())).thenReturn(createTestItem("testItemId"));
-	//		when(analyzerServiceClient.hasClients()).thenReturn(true);
-	//		logIndexerService.indexAllLogs();
-	//		verify(checkpointColl, times(batchCount)).save(any(DBObject.class));
-	//		verify(analyzerServiceClient, times(batchCount)).index(anyListOf(IndexLaunch.class));
-	//	}
-	//
-	//	@Test
-	//	public void testIndexTIItems() {
-	//		DBCollection checkpointColl = mock(DBCollection.class);
-	//		when(mongoOperations.getCollection(eq("logIndexingCheckpoint"))).thenReturn(checkpointColl);
-	//		when(checkpointColl.findOne(any(Query.class))).thenReturn(null);
-	//		when(mongoOperations.stream(any(Query.class), eq(Log.class))).thenReturn(createLogIterator(5));
-	//		when(testItemRepository.findOne(anyString())).thenReturn(createToInvestigateItem("testItemId"));
-	//		when(analyzerServiceClient.hasClients()).thenReturn(true);
-	//		logIndexerService.indexAllLogs();
-	//		verify(checkpointColl, times(0)).save(any(DBObject.class));
-	//		verify(analyzerServiceClient, times(0)).index(anyListOf(IndexLaunch.class));
-	//	}
-	//
 
 	private AnalyzerConfig analyzerConfig() {
 		AnalyzerConfig analyzerConfig = new AnalyzerConfig();
@@ -265,6 +137,7 @@ public class LogIndexerServiceTest {
 	private Launch createLaunch(Long id) {
 		Launch l = new Launch();
 		l.setId(id);
+		l.setMode(LaunchModeEnum.DEFAULT);
 		l.setName("launch" + id);
 		return l;
 	}
@@ -291,10 +164,10 @@ public class LogIndexerServiceTest {
 		return l;
 	}
 
-	private List<TestItem> createTestItems(int count) {
+	private List<TestItem> createTestItems(int count, TestItemIssueGroup issueGroup) {
 		List<TestItem> testItems = new ArrayList<>(count);
 		for (int i = 0; i < count; i++) {
-			testItems.add(createTestItem((long) i, TO_INVESTIGATE));
+			testItems.add(createTestItem((long) i, issueGroup));
 		}
 		return testItems;
 	}

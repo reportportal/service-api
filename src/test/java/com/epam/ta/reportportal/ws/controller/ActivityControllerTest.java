bf52f64688defa4329175994879d7906ada65124
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-01-31 12:42:09 +0300

    add tests

--- src/test/java/com/epam/ta/reportportal/ws/controller/ActivityControllerTest.java
+++ src/test/java/com/epam/ta/reportportal/ws/controller/ActivityControllerTest.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2018 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.ta.reportportal.ws.controller;
 
 import com.epam.ta.reportportal.ws.BaseMvcTest;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -11,8 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * @author <a href="mailto:ihar_kahadouski@epam.com">Ihar Kahadouski</a>
  */
-@Sql("classpath:db/activity/activity-fill.sql")
-@Ignore
+@Sql("/db/activity/activity-fill.sql")
 public class ActivityControllerTest extends BaseMvcTest {
 
 	@Test
---
+++ src/test/resources/db/project/project-fill.sql
@@ -0,0 +1,23 @@
+insert into project (id, name, project_type, organization, creation_date)
+values (3, 'test_project', 'INTERNAL', 'org', now());
+
+insert into users(id, login, password, email, attachment, attachment_thumbnail, role, type, expired, default_project_id, full_name, metadata)
+values (3, 'test_user', '179AD45C6CE2CB97CF1029E212046E81', 'test@domain.com', null, null, 'USER', 'INTERNAL', false, 3, 'test full name', '{"metadata": {"last_login": "now"}}');
+
+insert into project_user(user_id, project_id, project_role) values (3, 3, 'MEMBER');
+insert into project_user(user_id, project_id, project_role) values (1, 3, 'PROJECT_MANAGER');
+
+insert into shareable_entity(id, shared, owner, project_id) values (1, true, 'superadmin', 3);
+insert into filter(id, name, target, description) values (1, 'test filter', 'Launch', 'decription');
+insert into filter_sort(filter_id, field) values (1, 'name');
+insert into filter_condition(id, filter_id, condition, value, search_criteria, negative) values (1, 1, 'CONTAINS', 'asdf', 'name', false);
+
+insert into user_preference(project_id, user_id, filter_id) values (3, 1, 1);
+
+insert into shareable_entity(id, shared, owner, project_id) values (2, true, 'superadmin', 3);
+insert into filter(id, name, target, description) values (2, 'test filter2', 'Launch', 'decription');
+insert into filter_sort(filter_id, field) values (2, 'name');
+insert into filter_condition(id, filter_id, condition, value, search_criteria, negative) values (2, 2, 'CONTAINS', 'kek', 'name', false);
+
+alter sequence project_id_seq restart with 4;
+alter sequence users_id_seq restart with 4;

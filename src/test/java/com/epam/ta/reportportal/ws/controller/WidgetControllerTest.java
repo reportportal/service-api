50948b77091bbfee52563c13b861a4e0ebf39e31
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-02-22 19:50:08 +0300

    add some tests

--- src/test/java/com/epam/ta/reportportal/ws/controller/WidgetControllerTest.java
+++ src/test/java/com/epam/ta/reportportal/ws/controller/WidgetControllerTest.java
@@ -7,6 +7,8 @@ import com.epam.ta.reportportal.ws.model.EntryCreatedRS;
 import com.epam.ta.reportportal.ws.model.widget.ContentParameters;
 import com.epam.ta.reportportal.ws.model.widget.WidgetPreviewRQ;
 import com.epam.ta.reportportal.ws.model.widget.WidgetRQ;
+import com.epam.ta.reportportal.ws.model.widget.WidgetResource;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * @author <a href="mailto:ihar_kahadouski@epam.com">Ihar Kahadouski</a>
@@ -133,5 +135,105 @@ class WidgetControllerTest extends BaseMvcTest {
 		mockMvc.perform(post(DEFAULT_PROJECT_BASE_URL + "/widget/preview").with(token(oAuthHelper.getDefaultToken()))
 				.contentType(APPLICATION_JSON)
 				.content(objectMapper.writeValueAsBytes(request))).andExpect(status().isOk());
+	}
+
+	@Sql("/db/widget/launch-statistics.sql")
+	@Test
+	void getLaunchStatisticsWidget() throws Exception {
+		mockMvc.perform(get(SUPERADMIN_PROJECT_BASE_URL + "/widget/2").with(token(oAuthHelper.getSuperadminToken())))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$.name").value("launch stats"))
+				.andExpect(jsonPath("$.widgetType").value("launchStatistics"))
+				.andExpect(jsonPath("$.content.result[0].name").value("test launch"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$defects$automation_bug$ab001").value("1"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$defects$product_bug$pb001").value("1"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$defects$to_investigate$ti001").value("1"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$executions$failed").value("3"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$executions$passed").value("2"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$executions$total").value("5"));
+	}
+
+	@Sql("/db/widget/not-passed.sql")
+	@Test
+	void getNotPassedWidget() throws Exception {
+		mockMvc.perform(get(SUPERADMIN_PROJECT_BASE_URL + "/widget/2").with(token(oAuthHelper.getSuperadminToken())))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$.name").value("not passed"))
+				.andExpect(jsonPath("$.widgetType").value("notPassed"))
+				.andExpect(jsonPath("$.content.result[0].name").value("test launch"))
+				.andExpect(jsonPath("$.content.result[0].values.*").value("60.0"));
+	}
+
+	@Sql("/db/widget/launches-comparison-chart.sql")
+	@Test
+	void getLaunchesComparisonWidget() throws Exception {
+		mockMvc.perform(get(SUPERADMIN_PROJECT_BASE_URL + "/widget/2").with(token(oAuthHelper.getSuperadminToken())))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$.name").value("launch comparison"))
+				.andExpect(jsonPath("$.widgetType").value("launchesComparisonChart"))
+				.andExpect(jsonPath("$.content.result[0].name").value("test launch"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$defects$automation_bug$ab001").value("33.33"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$defects$product_bug$pb001").value("33.33"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$defects$to_investigate$ti001").value("33.33"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$executions$failed").value("60.0"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$executions$passed").value("40.0"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$executions$total").value("5.0"))
+				.andExpect(jsonPath("$.content.result[1].name").value("test launch"))
+				.andExpect(jsonPath("$.content.result[1].values.statistics$defects$product_bug$pb001").value("33.33"))
+				.andExpect(jsonPath("$.content.result[1].values.statistics$defects$to_investigate$ti001").value("66.67"))
+				.andExpect(jsonPath("$.content.result[1].values.statistics$executions$failed").value("60.0"))
+				.andExpect(jsonPath("$.content.result[1].values.statistics$executions$passed").value("20.0"))
+				.andExpect(jsonPath("$.content.result[1].values.statistics$executions$skipped").value("20.0"))
+				.andExpect(jsonPath("$.content.result[1].values.statistics$executions$total").value("5.0"));
+	}
+
+	@Sql("/db/widget/launches-duration-chart.sql")
+	@Test
+	void getLaunchesDurationWidget() throws Exception {
+		mockMvc.perform(get(SUPERADMIN_PROJECT_BASE_URL + "/widget/2").with(token(oAuthHelper.getSuperadminToken())))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$.name").value("launches duration"))
+				.andExpect(jsonPath("$.widgetType").value("launchesDurationChart"))
+				.andExpect(jsonPath("$.content.result[0].name").value("test launch"))
+				.andExpect(jsonPath("$.content.result[0].duration").value("540000"))
+				.andExpect(jsonPath("$.content.result[1].name").value("test launch"))
+				.andExpect(jsonPath("$.content.result[1].duration").value("660000"));
+	}
+
+	@Sql("/db/widget/bug-trend.sql")
+	@Test
+	void getBugTrendWidget() throws Exception {
+		mockMvc.perform(get(SUPERADMIN_PROJECT_BASE_URL + "/widget/2").with(token(oAuthHelper.getSuperadminToken())))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$.name").value("bug trend"))
+				.andExpect(jsonPath("$.widgetType").value("bugTrend"))
+				.andExpect(jsonPath("$.content.result[0].name").value("test launch"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$defects$automation_bug$total").value("1"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$defects$product_bug$total").value("1"))
+				.andExpect(jsonPath("$.content.result[0].values.statistics$defects$to_investigate$total").value("1"))
+				.andExpect(jsonPath("$.content.result[0].values.total").value("3"))
+				.andExpect(jsonPath("$.content.result[1].name").value("test launch"))
+				.andExpect(jsonPath("$.content.result[1].values.statistics$defects$product_bug$total").value("1"))
+				.andExpect(jsonPath("$.content.result[1].values.statistics$defects$to_investigate$total").value("2"))
+				.andExpect(jsonPath("$.content.result[1].values.total").value("3"));
+	}
+
+	@Sql("/db/widget/launches-table.sql")
+	@Test
+	void getLaunchesTableWidget() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(get(SUPERADMIN_PROJECT_BASE_URL + "/widget/2").with(token(oAuthHelper.getSuperadminToken())))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andReturn();
+
+		WidgetResource response = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<WidgetResource>() {
+		});
+
+		System.out.println();
 	}
 }
---
+++ src/test/resources/db/widget/launches-duration-chart.sql
@@ -0,0 +1,27 @@
+-- First launch
+insert into launch(id, uuid, project_id, user_id, name, description, start_time, end_time, number, last_modified, mode, status, has_retries)
+values (1, 'uuid', 1, 1, 'test launch', 'desc', now() - interval '9 minute', now(), 1, now(), 'DEFAULT', 'FAILED', false);
+
+-- Second launch
+insert into launch(id, uuid, project_id, user_id, name, description, start_time, end_time, number, last_modified, mode, status, has_retries)
+values (2, 'uuid2', 1, 1, 'test launch', 'desc', now() - interval '11 minute', now(), 2, now(), 'DEFAULT', 'FAILED', false);
+
+-- Filter and widget
+INSERT INTO public.shareable_entity (id, shared, owner, project_id)
+VALUES (1, false, 'superadmin', 1),
+       (2, false, 'superadmin', 1);
+
+INSERT INTO public.filter (id, name, target, description)
+VALUES (1, 'Admin Filter', 'Launch', null);
+
+INSERT INTO public.filter_sort (id, filter_id, field, direction)
+VALUES (1, 1, 'name', 'ASC');
+
+INSERT INTO public.filter_condition (id, filter_id, condition, value, search_criteria, negative)
+VALUES (1, 1, 'CONTAINS', 'test', 'name', false);
+
+INSERT INTO public.widget (id, name, description, widget_type, items_count, widget_options)
+VALUES (2, 'launches duration', null, 'launchesDurationChart', 20, '{"options": {}}');
+
+insert into widget_filter(widget_id, filter_id)
+values (2, 1);

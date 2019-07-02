7401f75e4eaed15c45660452f67cb8df6cce98c0
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-07-02 11:52:54 +0300

    add tests

--- src/test/java/com/epam/ta/reportportal/core/launch/rerun/RerunHandlerImplTest.java
+++ src/test/java/com/epam/ta/reportportal/core/launch/rerun/RerunHandlerImplTest.java
@@ -22,11 +22,17 @@ import com.epam.ta.reportportal.core.events.activity.LaunchStartedEvent;
 import com.epam.ta.reportportal.core.item.UniqueIdGenerator;
 import com.epam.ta.reportportal.dao.LaunchRepository;
 import com.epam.ta.reportportal.dao.TestItemRepository;
+import com.epam.ta.reportportal.entity.enums.StatusEnum;
+import com.epam.ta.reportportal.entity.enums.TestItemTypeEnum;
+import com.epam.ta.reportportal.entity.item.TestItem;
+import com.epam.ta.reportportal.entity.item.TestItemResults;
 import com.epam.ta.reportportal.entity.launch.Launch;
 import com.epam.ta.reportportal.entity.project.ProjectRole;
 import com.epam.ta.reportportal.entity.user.UserRole;
 import com.epam.ta.reportportal.exception.ReportPortalException;
+import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
 import com.epam.ta.reportportal.ws.model.attribute.ItemAttributesRQ;
+import com.epam.ta.reportportal.ws.model.item.ItemCreatedRS;
 import com.epam.ta.reportportal.ws.model.launch.Mode;
 import com.epam.ta.reportportal.ws.model.launch.StartLaunchRQ;
 import com.epam.ta.reportportal.ws.model.launch.StartLaunchRS;
@@ -122,6 +128,91 @@ class RerunHandlerImplTest {
 		assertNotNull(responseOptional.get().getId());
 		assertNotNull(responseOptional.get().getUuid());
 
+	}
+
+	@Test
+	void returnEmptyOptionalWhenRootItemNotFound() {
+		StartTestItemRQ request = new StartTestItemRQ();
+		request.setLaunchId("launch_uuid");
+		request.setType("STEP");
+		String itemName = "name";
+		request.setName(itemName);
+		Launch launch = getLaunch("uuid");
+
+		when(testItemRepository.findByNameAndLaunchWithoutParents(itemName, launch.getId())).thenReturn(Optional.empty());
+
+		Optional<ItemCreatedRS> rerunCreatedRS = rerunHandler.handleRootItem(request, launch);
+
+		assertFalse(rerunCreatedRS.isPresent());
+	}
+
+	@Test
+	void happyRerunRootItem() {
+		StartTestItemRQ request = new StartTestItemRQ();
+		request.setLaunchId("launch_uuid");
+		request.setType("STEP");
+		String itemName = "name";
+		request.setName(itemName);
+		Launch launch = getLaunch("uuid");
+
+		when(testItemRepository.findByNameAndLaunchWithoutParents(itemName, launch.getId())).thenReturn(Optional.of(getItem(itemName,
+				launch
+		)));
+
+		Optional<ItemCreatedRS> rerunCreatedRS = rerunHandler.handleRootItem(request, launch);
+
+		assertTrue(rerunCreatedRS.isPresent());
+	}
+
+	@Test
+	void returnEmptyOptionalWhenChildItemNotFound() {
+		StartTestItemRQ request = new StartTestItemRQ();
+		request.setLaunchId("launch_uuid");
+		request.setType("STEP");
+		String itemName = "name";
+		request.setName(itemName);
+		Launch launch = getLaunch("uuid");
+		TestItem parent = new TestItem();
+		parent.setPath("1.2");
+
+		when(testItemRepository.findByNameAndLaunchUnderPath(itemName, launch.getId(), parent.getPath())).thenReturn(Optional.empty());
+
+		Optional<ItemCreatedRS> rerunCreatedRS = rerunHandler.handleChildItem(request, launch, parent);
+
+		assertFalse(rerunCreatedRS.isPresent());
+	}
+
+	@Test
+	void happyRerunChildItem() {
+		StartTestItemRQ request = new StartTestItemRQ();
+		request.setLaunchId("launch_uuid");
+		request.setType("STEP");
+		String itemName = "name";
+		request.setName(itemName);
+		Launch launch = getLaunch("uuid");
+		TestItem parent = new TestItem();
+		parent.setPath("1.2");
+
+		when(testItemRepository.findByNameAndLaunchUnderPath(itemName, launch.getId(), parent.getPath())).thenReturn(Optional.of(getItem(itemName,
+				launch
+		)));
+
+		Optional<ItemCreatedRS> rerunCreatedRS = rerunHandler.handleChildItem(request, launch, parent);
+
+		assertTrue(rerunCreatedRS.isPresent());
+	}
+
+	private TestItem getItem(String name, Launch launch) {
+		TestItem item = new TestItem();
+		item.setItemId(1L);
+		item.setName(name);
+		item.setLaunch(launch);
+		item.setDescription("desc");
+		item.setType(TestItemTypeEnum.STEP);
+		TestItemResults itemResults = new TestItemResults();
+		itemResults.setStatus(StatusEnum.PASSED);
+		item.setItemResults(itemResults);
+		return item;
 	}
 
 	private Launch getLaunch(String uuid) {

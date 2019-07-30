ba5f88b144adf120cb7a7e603a7a880331cc4d00
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2019-07-30 16:59:22 +0300

    fix merge errors

--- src/test/resources/db/migration/V003__integration_type.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO integration_type (enabled, name, auth_flow, creation_date, group_type) VALUES (TRUE, 'rally', 'OAUTH', now(), 'BTS') ;
-
-INSERT INTO integration_type (enabled, name, auth_flow, creation_date, group_type) VALUES (TRUE, 'jira', 'BASIC', now(), 'BTS');

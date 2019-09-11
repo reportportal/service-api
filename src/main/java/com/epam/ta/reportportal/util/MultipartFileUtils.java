9f214640c53311ed445b1ec8161e7f7ee3637aed
pavel_bortnik <pavel_bortnik@epam.com>
2019-09-11 15:24:39 +0300

    update multipart file utils

--- src/main/java/com/epam/ta/reportportal/util/MultipartFileUtils.java
+++ src/main/java/com/epam/ta/reportportal/util/MultipartFileUtils.java
@@ -9,7 +9,9 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author <a href="mailto:ihar_kahadouski@epam.com">Ihar Kahadouski</a>
@@ -21,15 +23,16 @@ public class MultipartFileUtils {
 	}
 
 	public static CommonsMultipartFile getMultipartFile(String path) throws IOException {
-		File file = new ClassPathResource(path).getFile();
-		FileItem fileItem = new DiskFileItem("mainFile",
-				Files.probeContentType(file.toPath()),
+		ClassPathResource classPathResource = new ClassPathResource(path);
+		FileItem fileItem = new DiskFileItem(
+				"mainFile",
+				Files.probeContentType(Paths.get(classPathResource.getURI())),
 				false,
-				file.getName(),
-				(int) file.length(),
-				file.getParentFile()
+				classPathResource.getFilename(),
+				(int) classPathResource.getInputStream().available(),
+				null
 		);
-		IOUtils.copy(new FileInputStream(file), fileItem.getOutputStream());
+		IOUtils.copy(classPathResource.getInputStream(), fileItem.getOutputStream());
 		return new CommonsMultipartFile(fileItem);
 	}
 }

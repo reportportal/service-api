cc43c9a7abbf58fde20b82800b385b2843f541d5
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-11-01 11:37:12 +0300

    define issue types in case insensitive way

--- src/main/java/com/epam/ta/reportportal/core/item/impl/IssueTypeHandler.java
+++ src/main/java/com/epam/ta/reportportal/core/item/impl/IssueTypeHandler.java
@@ -22,9 +22,9 @@ import com.epam.ta.reportportal.exception.ReportPortalException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static com.epam.ta.reportportal.commons.EntityUtils.normalizeId;
 import static com.epam.ta.reportportal.commons.validation.Suppliers.formattedSupplier;
 import static com.epam.ta.reportportal.ws.model.ErrorType.FAILED_TEST_ITEM_ISSUE_TYPE_DEFINITION;
-import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -49,9 +49,7 @@ public class IssueTypeHandler {
 	 * @return verified issue type
 	 */
 	public IssueType defineIssueType(Long projectId, String locator) {
-		return testItemRepository.selectIssueTypeByLocator(projectId,
-				ofNullable(locator).orElseThrow(() -> new ReportPortalException("Locator should not be null"))
-		)
+		return testItemRepository.selectIssueTypeByLocator(projectId, normalizeId(locator))
 				.orElseThrow(() -> new ReportPortalException(FAILED_TEST_ITEM_ISSUE_TYPE_DEFINITION, formattedSupplier(
 						"Invalid test item issue type definition '{}' is requested. Valid issue types' locators are: {}",
 						locator,

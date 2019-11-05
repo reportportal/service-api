a9ab33b3e94c5c1cd18633b47e017fbc324a9b8f
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-11-05 12:01:24 +0300

    change exception type

--- src/main/java/com/epam/ta/reportportal/core/item/impl/IssueTypeHandler.java
+++ src/main/java/com/epam/ta/reportportal/core/item/impl/IssueTypeHandler.java
@@ -16,15 +16,16 @@
 
 package com.epam.ta.reportportal.core.item.impl;
 
+import com.epam.ta.reportportal.commons.EntityUtils;
 import com.epam.ta.reportportal.dao.TestItemRepository;
 import com.epam.ta.reportportal.entity.item.issue.IssueType;
 import com.epam.ta.reportportal.exception.ReportPortalException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static com.epam.ta.reportportal.commons.EntityUtils.normalizeId;
 import static com.epam.ta.reportportal.commons.validation.Suppliers.formattedSupplier;
 import static com.epam.ta.reportportal.ws.model.ErrorType.FAILED_TEST_ITEM_ISSUE_TYPE_DEFINITION;
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -49,12 +50,14 @@ public class IssueTypeHandler {
 	 * @return verified issue type
 	 */
 	public IssueType defineIssueType(Long projectId, String locator) {
-		return testItemRepository.selectIssueTypeByLocator(projectId, normalizeId(locator))
-				.orElseThrow(() -> new ReportPortalException(FAILED_TEST_ITEM_ISSUE_TYPE_DEFINITION, formattedSupplier(
-						"Invalid test item issue type definition '{}' is requested. Valid issue types' locators are: {}",
-						locator,
-						testItemRepository.selectIssueLocatorsByProject(projectId).stream().map(IssueType::getLocator).collect(toList())
-				)));
+		return testItemRepository.selectIssueTypeByLocator(
+				projectId,
+				ofNullable(locator).map(EntityUtils::normalizeId).orElseThrow(() -> new ReportPortalException("Locator should not be null"))
+		).orElseThrow(() -> new ReportPortalException(FAILED_TEST_ITEM_ISSUE_TYPE_DEFINITION, formattedSupplier(
+				"Invalid test item issue type definition '{}' is requested. Valid issue types' locators are: {}",
+				locator,
+				testItemRepository.selectIssueLocatorsByProject(projectId).stream().map(IssueType::getLocator).collect(toList())
+		)));
 	}
 
 }

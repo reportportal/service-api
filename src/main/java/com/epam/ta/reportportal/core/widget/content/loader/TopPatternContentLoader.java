6b721c7b6b159d4643ced1125053a9f6a5fed231
yumfriez <budaevqwerty@gmail.com>
2019-12-24 17:27:25 +0300

    NULL-key case impl

--- src/main/java/com/epam/ta/reportportal/core/widget/content/loader/TopPatternContentLoader.java
+++ src/main/java/com/epam/ta/reportportal/core/widget/content/loader/TopPatternContentLoader.java
@@ -17,7 +17,6 @@
 package com.epam.ta.reportportal.core.widget.content.loader;
 
 import com.epam.ta.reportportal.commons.querygen.Filter;
-import com.epam.ta.reportportal.commons.validation.BusinessRule;
 import com.epam.ta.reportportal.core.widget.content.MultilevelLoadContentStrategy;
 import com.epam.ta.reportportal.core.widget.util.WidgetOptionUtil;
 import com.epam.ta.reportportal.dao.WidgetContentRepository;
@@ -25,7 +24,6 @@ import com.epam.ta.reportportal.entity.widget.WidgetOptions;
 import com.epam.ta.reportportal.entity.widget.content.TopPatternTemplatesContent;
 import com.epam.ta.reportportal.ws.model.ErrorType;
 import org.apache.commons.collections.MapUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -56,7 +54,6 @@ public class TopPatternContentLoader implements MultilevelLoadContentStrategy {
 			String[] attributes, Map<String, String> params, int limit) {
 
 		validateFilterSortMapping(filterSortMapping);
-		validateWidgetOptions(widgetOptions);
 
 		Filter filter = GROUP_FILTERS.apply(filterSortMapping.keySet());
 		Sort sort = GROUP_SORTS.apply(filterSortMapping.values());
@@ -82,15 +79,5 @@ public class TopPatternContentLoader implements MultilevelLoadContentStrategy {
 		expect(MapUtils.isNotEmpty(filterSortMapping), equalTo(true)).verify(ErrorType.BAD_REQUEST_ERROR,
 				"Filter-Sort mapping should not be empty"
 		);
-	}
-
-	/**
-	 * Validate provided widget options. For current widget launch name should be specified.
-	 *
-	 * @param widgetOptions Map of stored widget options.
-	 */
-	private void validateWidgetOptions(WidgetOptions widgetOptions) {
-		BusinessRule.expect(WidgetOptionUtil.getValueByKey(ATTRIBUTE_KEY, widgetOptions), StringUtils::isNotEmpty)
-				.verify(ErrorType.UNABLE_LOAD_WIDGET_CONTENT, ATTRIBUTE_KEY + " should be specified for widget.");
 	}
 }

2a2296627ac2019051645ff9e8d919d740ef24c9
ihar-kahadouski <igor.kagadowski@gmail.com>
2020-03-17 11:32:47 +0300

    update dependencies

--- src/main/java/com/epam/ta/reportportal/job/CleanLogsJob.java
+++ src/main/java/com/epam/ta/reportportal/job/CleanLogsJob.java
@@ -32,6 +32,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -51,6 +52,7 @@ import static java.time.Duration.ofDays;
  * @author Pavel Borntik
  */
 @Service
+@Profile("!unittest")
 public class CleanLogsJob implements Job {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CleanLogsJob.class);

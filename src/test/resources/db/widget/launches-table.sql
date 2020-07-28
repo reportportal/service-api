abac64691444950dff2313cdb4c09164716ab866
pavel_bortnik <pavel_bortnik@epam.com>
2020-07-28 18:46:38 +0300

    fix issue with launch table parameters

--- src/test/resources/db/widget/launches-table.sql
+++ src/test/resources/db/widget/launches-table.sql
@@ -2,6 +2,9 @@
 insert into launch(id, uuid, project_id, user_id, name, description, start_time, end_time, number, last_modified, mode, status, has_retries)
 values (2, 'uuid2', 1, 1, 'test launch', 'desc', now(), null, 2, now(), 'DEFAULT', 'FAILED', false);
 
+insert into item_attribute(key, value, launch_id) VALUES ('key', 'value', 2);
+insert into item_attribute(key, value, launch_id) VALUES ('key1', 'value1', 2);
+
 insert into test_item(test_case_hash, item_id, uuid, name, type, start_time, description, unique_id, last_modified, path, parent_id, launch_id)
 values (6, 6,'uuid6',  'test item 1', 'STEP', now(), 'desc', 'uuid6', now(), '6', null, 2);
 insert into test_item_results(result_id, status)

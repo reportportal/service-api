3f7524439961c30ce163ea6b0511496d6358f7b0
pavel_bortnik <pavel_bortnik@epam.com>
2020-11-17 00:41:24 +0300

    Refactor hibernate update filter while sharing

--- src/main/java/com/epam/ta/reportportal/core/filter/impl/UpdateUserFilterHandlerImpl.java
+++ src/main/java/com/epam/ta/reportportal/core/filter/impl/UpdateUserFilterHandlerImpl.java
@@ -41,6 +41,7 @@ import com.epam.ta.reportportal.ws.model.OperationCompletionRS;
 import com.epam.ta.reportportal.ws.model.activity.UserFilterActivityResource;
 import com.epam.ta.reportportal.ws.model.filter.BulkUpdateFilterRQ;
 import com.epam.ta.reportportal.ws.model.filter.UpdateUserFilterRQ;
+import com.google.common.collect.Lists;
 import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -146,12 +147,15 @@ public class UpdateUserFilterHandlerImpl implements UpdateUserFilterHandler {
 
 	@Override
 	public void updateSharing(Collection<UserFilter> filters, Long projectId, boolean isShared) {
+		List<UserFilter> filtersToSave = Lists.newLinkedList();
 		filters.forEach(filter -> {
 			if (filter.isShared() != isShared) {
 				filter.setShared(isShared);
 				aclHandler.updateAcl(filter, projectId, filter.isShared());
+				filtersToSave.add(filter);
 			}
 		});
+		userFilterRepository.saveAll(filtersToSave);
 	}
 
 	/**

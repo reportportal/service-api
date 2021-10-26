8097a3dd4866143d04059bde0f4d8d1a51c38b42
Ivan_Budayeu <Skileton2018>
2021-10-26 15:58:30 +0300

    License/author headers provided

--- src/main/java/com/epam/ta/reportportal/core/launch/cluster/ClusterGeneratorImpl.java
+++ src/main/java/com/epam/ta/reportportal/core/launch/cluster/ClusterGeneratorImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.ta.reportportal.core.launch.cluster;
 
 import com.epam.ta.reportportal.core.analyzer.auto.client.AnalyzerServiceClient;
@@ -16,6 +32,9 @@ import java.util.function.Predicate;
 
 import static com.epam.ta.reportportal.commons.validation.BusinessRule.expect;
 
+/**
+ * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
+ */
 @Service
 public class ClusterGeneratorImpl implements ClusterGenerator {
 
@@ -75,9 +94,7 @@ public class ClusterGeneratorImpl implements ClusterGenerator {
 		} catch (Exception ex) {
 			LOGGER.error(ex.getMessage(), ex);
 		} finally {
-			analyzerStatusCache.analyzeFinished(AnalyzerStatusCache.CLUSTER_KEY,
-					generateClustersRq.getLaunchId()
-			);
+			analyzerStatusCache.analyzeFinished(AnalyzerStatusCache.CLUSTER_KEY, generateClustersRq.getLaunchId());
 		}
 	}
 

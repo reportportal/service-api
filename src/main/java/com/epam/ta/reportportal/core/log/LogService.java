eaf6405643f93866b14c062095471581ea9f000b
Maksim Antonov <Maksim_Antonov@epam.com>
2022-03-14 02:12:38 +0200

    Add logservice wo elastic

--- src/main/java/com/epam/ta/reportportal/core/log/LogService.java
+++ src/main/java/com/epam/ta/reportportal/core/log/LogService.java
@@ -1,44 +1,17 @@
 package com.epam.ta.reportportal.core.log;
 
 import com.epam.ta.reportportal.entity.log.Log;
-import com.epam.ta.reportportal.entity.log.LogMessage;
-import org.apache.commons.collections.CollectionUtils;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 
-import static com.epam.ta.reportportal.core.configs.rabbit.BackgroundProcessingConfiguration.LOG_MESSAGE_SAVING_ROUTING_KEY;
-import static com.epam.ta.reportportal.core.configs.rabbit.BackgroundProcessingConfiguration.PROCESSING_EXCHANGE_NAME;
+public interface LogService {
 
-@Service
-public class LogService {
-    private final AmqpTemplate amqpTemplate;
-
-    public LogService(@Qualifier(value = "rabbitTemplate") AmqpTemplate amqpTemplate) {
-        this.amqpTemplate = amqpTemplate;
-    }
-
-    public void saveLogMessageToElasticSearch(Log log) {
-        if (Objects.isNull(log)) return;
-        amqpTemplate.convertAndSend(PROCESSING_EXCHANGE_NAME, LOG_MESSAGE_SAVING_ROUTING_KEY, convertLogToLogMessage(log));
-    }
+    void saveLogMessageToElasticSearch(Log log);
 
     /**
      * Used only for generation demo data, that send all per message to avoid some object/collection wrapping
      * during reporting.
      * @param logList
      */
-    public void saveLogMessageListToElasticSearch(List<Log> logList) {
-        if (CollectionUtils.isEmpty(logList)) return;
-        logList.stream().filter(Objects::nonNull).forEach(this::saveLogMessageToElasticSearch);
-    }
-
-    private LogMessage convertLogToLogMessage(Log log) {
-        Long itemId = Objects.nonNull(log.getTestItem()) ? log.getTestItem().getItemId() : null;
-        Long launchId = Objects.nonNull(log.getLaunch()) ? log.getLaunch().getId() : null;
-        return new LogMessage(log.getId(), log.getLogTime(), log.getLogMessage(), itemId, launchId, log.getProjectId());
-    }
+    void saveLogMessageListToElasticSearch(List<Log> logList);
 }

bb3505f00c6f1d213da2a53eb29dc397dfd6117c
Pavel Bortnik <pavel_bortnik@epam.com>
2022-05-19 01:08:01 +0300

    5.7.1 || Merge 5.7.1 to master (#1576)
    * EPMRPP-77071 || DAO dependency updated
    
    * EPMRPP-76503 || Added launchId to processed object for log message
    
    Co-authored-by: Ivan_Budayeu <skileton2018>
    Co-authored-by: Maksim Antonov <Maksim_Antonov@epam.com>

--- src/main/java/com/epam/ta/reportportal/core/log/LogService.java
+++ src/main/java/com/epam/ta/reportportal/core/log/LogService.java
@@ -6,12 +6,16 @@ import java.util.List;
 
 public interface LogService {
 
-    void saveLogMessageToElasticSearch(Log log);
+    /**
+     * launchId - temporary, need to bring log launch/testItem to normal value.
+     */
+    void saveLogMessageToElasticSearch(Log log, Long launchId);
 
     /**
      * Used only for generation demo data, that send all per message to avoid some object/collection wrapping
      * during reporting.
      * @param logList
+     * @param launchId - temporary, need to bring log launch/testItem to normal value.
      */
-    void saveLogMessageListToElasticSearch(List<Log> logList);
+    void saveLogMessageListToElasticSearch(List<Log> logList, Long launchId);
 }
--- src/main/java/com/epam/ta/reportportal/core/log/impl/CreateLogHandlerImpl.java
+++ src/main/java/com/epam/ta/reportportal/core/log/impl/CreateLogHandlerImpl.java
@@ -104,7 +104,7 @@ public class CreateLogHandlerImpl implements CreateLogHandler {
 
 		final Log log = logBuilder.get();
 		logRepository.save(log);
-		logService.saveLogMessageToElasticSearch(log);
+		logService.saveLogMessageToElasticSearch(log, launch.getId());
 
 		ofNullable(file).ifPresent(f -> saveBinaryData(f, launch, log));
 

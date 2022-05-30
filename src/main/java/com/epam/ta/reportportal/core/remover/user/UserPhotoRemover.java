f0f706c93df6239f6f2a27f44cd994c42b94731e
Chingiskhan <Chingiskhan_Kalanov@epam.com>
2022-05-30 17:17:06 +0300

    EPMRPP-65572 || review fixes

--- src/main/java/com/epam/ta/reportportal/core/remover/user/UserPhotoRemover.java
+++ src/main/java/com/epam/ta/reportportal/core/remover/user/UserPhotoRemover.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.ta.reportportal.core.remover.user;
 
 import com.epam.ta.reportportal.core.remover.ContentRemover;
@@ -14,6 +30,9 @@ import java.util.Optional;
 
 import static java.util.Optional.ofNullable;
 
+/**
+ * @author <a href="mailto:chingiskhan_kalanov@epam.com">Chingiskhan Kalanov</a>
+ */
 @Service
 public class UserPhotoRemover implements ContentRemover<User> {
 	private static final String ATTACHMENT_CONTENT_TYPE = "attachmentContentType";
@@ -23,13 +42,6 @@ public class UserPhotoRemover implements ContentRemover<User> {
 	@Autowired
 	public UserPhotoRemover(AttachmentRepository attachmentRepository) {
 		this.attachmentRepository = attachmentRepository;
-	}
-
-	private Long prepareAttachmentAndGetId(String fileId) {
-		Attachment attachment = new Attachment();
-		attachment.setFileId(fileId);
-		attachment.setCreationDate(LocalDateTime.now());
-		return attachmentRepository.save(attachment).getId();
 	}
 
 	@Override
@@ -45,5 +57,12 @@ public class UserPhotoRemover implements ContentRemover<User> {
 			ofNullable(user.getMetadata()).ifPresent(metadata -> metadata.getMetadata().remove(ATTACHMENT_CONTENT_TYPE));
 			attachmentRepository.moveForDeletion(attachmentsIds);
 		});
+	}
+
+	private Long prepareAttachmentAndGetId(String fileId) {
+		Attachment attachment = new Attachment();
+		attachment.setFileId(fileId);
+		attachment.setCreationDate(LocalDateTime.now());
+		return attachmentRepository.save(attachment).getId();
 	}
 }
--- src/test/java/com/epam/ta/reportportal/core/remover/user/UserPhotoRemoverTest.java
+++ src/test/java/com/epam/ta/reportportal/core/remover/user/UserPhotoRemoverTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.ta.reportportal.core.remover.user;
 
 import com.epam.ta.reportportal.dao.AttachmentRepository;
@@ -9,6 +25,9 @@ import java.util.List;
 
 import static org.mockito.Mockito.*;
 
+/**
+ * @author <a href="mailto:chingiskhan_kalanov@epam.com">Chingiskhan Kalanov</a>
+ */
 class UserPhotoRemoverTest {
 	private static final String USER_PHOTO_ID = "SOME_PHOTO_ID";
 	private static final String USER_THUMBNAIL_ID = "SOME_THUMBNAIL_ID";

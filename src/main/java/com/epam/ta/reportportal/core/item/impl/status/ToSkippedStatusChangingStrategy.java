6c4dece274815aea09d09b96a8e4e82fd28e39b5
APiankouski <109206864+APiankouski@users.noreply.github.com>
2022-07-26 13:29:02 +0300

    EPMRPP-64803 || Make async remove test items from index (#1605)
    Co-authored-by: Andrei Piankouski <andrei.piankouski@ukg.com>

--- src/main/java/com/epam/ta/reportportal/core/item/impl/status/ToSkippedStatusChangingStrategy.java
+++ src/main/java/com/epam/ta/reportportal/core/item/impl/status/ToSkippedStatusChangingStrategy.java
@@ -104,7 +104,7 @@ public class ToSkippedStatusChangingStrategy extends AbstractStatusChangingStrat
 
 			List<Long> itemsToReindex = changeParentsStatuses(testItem, launch, true, user);
 			itemsToReindex.add(testItem.getItemId());
-			logIndexer.indexItemsRemoveAsync(project.getId(), itemsToReindex);
+			logIndexer.indexItemsRemove(project.getId(), itemsToReindex);
 
 			if (!issueRequired) {
 				itemsToReindex.remove(itemsToReindex.size() - 1);

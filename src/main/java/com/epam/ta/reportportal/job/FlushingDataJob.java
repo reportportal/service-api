6d2e1b647b9768606a3cff2c8bd58142ffbcfa94
Ivan_Kustau <Ivan_Kustau@epam.com>
2023-02-09 01:42:01 +0300

    EPMRPP-82061|| Add postfix for bucket name

--- src/main/java/com/epam/ta/reportportal/job/FlushingDataJob.java
+++ src/main/java/com/epam/ta/reportportal/job/FlushingDataJob.java
@@ -91,6 +91,9 @@ public class FlushingDataJob implements Job {
 	@Value("${datastore.minio.bucketPrefix}")
 	private String bucketPrefix;
 
+	@Value("${datastore.minio.bucketPostfix}")
+	private String bucketPostfix;
+
 	@Override
 	@Transactional(isolation = Isolation.READ_UNCOMMITTED)
 	public void execute(JobExecutionContext context) {
@@ -162,10 +165,11 @@ public class FlushingDataJob implements Job {
 		projectRepository.delete(project);
 		analyzerServiceClient.removeSuggest(project.getId());
 		issueTypeRepository.deleteAll(issueTypesToRemove);
+		String bucketName= bucketPrefix + project.getId() + bucketPostfix;
 		try {
-			blobStore.deleteContainer(bucketPrefix + project.getId());
+			blobStore.deleteContainer(bucketName);
 		} catch (Exception e) {
-			LOGGER.warn("Cannot delete attachments bucket " + bucketPrefix + project.getId());
+			LOGGER.warn("Cannot delete attachments bucket " + bucketName);
 		}
 		logIndexer.deleteIndex(project.getId());
 		projectRepository.flush();

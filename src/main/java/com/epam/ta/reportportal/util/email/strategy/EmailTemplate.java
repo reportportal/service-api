1d0775af9ae19a11c0c306a7007b751fea534e57
Bam6ycha <84175555+Bam6ycha@users.noreply.github.com>
2023-08-02 19:01:40 +0500

    EPMRPP-83085 || Implement delete account notification email (#1739)
    * EPMRPP-83085 || Implement delete account notification email
    
    * EPMRPP-83085 || Implement user notification about account deletion due to retention policy
    
    * EPMRPP-83085 || Update template
    
    * EPMRPP-83085 || Add user deletion notification
    
    * EPMRPP-83085 || Fix checkstyle
    
    ---------
    
    Co-authored-by: Andrei Piankouski <andrei_piankouski@epam.com>
    Co-authored-by: APiankouski <109206864+APiankouski@users.noreply.github.com>

---
+++ src/main/java/com/epam/ta/reportportal/util/email/strategy/EmailTemplate.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.ta.reportportal.util.email.strategy;
+
+import com.epam.ta.reportportal.exception.ReportPortalException;
+import com.epam.ta.reportportal.ws.model.ErrorType;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Enum of email templates.
+ *
+ * @author Andrei Piankouski
+ */
+public enum EmailTemplate {
+
+  USER_EXPIRATION_NOTIFICATION("userExpirationNotification"),
+  USER_DELETION_NOTIFICATION("userDeletionNotification");
+
+  private final String name;
+
+  EmailTemplate(String name) {
+    this.name = name;
+  }
+
+  public static EmailTemplate fromString(String type) {
+    return Arrays.stream(EmailTemplate.values())
+        .filter(it -> it.getName().equalsIgnoreCase(type))
+        .findFirst()
+        .orElseThrow(() -> new ReportPortalException(
+            ErrorType.INCORRECT_REQUEST,
+            "Incorrect analyzer type. Allowed are: " + Arrays.stream(EmailTemplate.values())
+                .map(EmailTemplate::getName)
+                .collect(Collectors.toList())
+        ));
+  }
+
+  public String getName() {
+    return name;
+  }
+}

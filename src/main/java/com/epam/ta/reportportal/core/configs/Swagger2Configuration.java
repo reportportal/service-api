5d1c21017c173a9a84b5ed08e399c3c67d007626
raikbitters <raikbitters@gmail.com>
2023-10-09 20:19:03 +0400

    Add a check to avoid duplicate names in path

--- src/main/java/com/epam/ta/reportportal/core/configs/Swagger2Configuration.java
+++ src/main/java/com/epam/ta/reportportal/core/configs/Swagger2Configuration.java
@@ -98,9 +98,6 @@ public class Swagger2Configuration {
   @Value("${info.build.version}")
   private String buildVersion;
 
-  @Value("${rp.swagger.use-path-prefix}")
-  private boolean usePathPrefix;
-
 	@Bean
 	public Docket docket() {
 		/* For more information see default params at {@link ApiInfo} */
@@ -137,7 +134,7 @@ public class Swagger2Configuration {
     return new RelativePathProvider(servletContext) {
       @Override
       public String getApplicationBasePath() {
-        if (!usePathPrefix) {
+        if (super.getApplicationBasePath().contains(applicationName)) {
           return super.getApplicationBasePath();
         }
         return "/" + applicationName + super.getApplicationBasePath();

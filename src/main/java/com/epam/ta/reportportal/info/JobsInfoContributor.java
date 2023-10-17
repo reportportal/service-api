b29a8e5fc75860aede75572c09daf7ed676b3bcf
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2023-10-17 11:47:50 +0300

    EPMRPP-86812 || extended actuator with "jobs" module info (#1832)

---
+++ src/main/java/com/epam/ta/reportportal/info/JobsInfoContributor.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2023 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.ta.reportportal.info;
+
+import com.epam.ta.reportportal.exception.ReportPortalException;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.info.Info.Builder;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+
+/**
+ * Picks actuator info from <i>jobs</i> service and shows it as part of <i>service-api</i>.
+ *
+ * @author Siarhei Hrabko
+ */
+@Component
+public class JobsInfoContributor implements InfoContributor {
+
+  private final RestTemplate restTemplate;
+
+  @Value("${rp.jobs.baseUrl}")
+  private String jobsBaseUrl;
+
+  public JobsInfoContributor() {
+    this.restTemplate = new RestTemplate();
+  }
+
+  @Override
+  public void contribute(Builder builder) {
+    try {
+      var jobsInfoRs = restTemplate.getForObject(jobsBaseUrl + "/info", Map.class);
+      builder
+          .withDetail("jobsInfo", jobsInfoRs)
+          .build();
+    } catch (Exception e) {
+      throw new ReportPortalException(e.getMessage());
+    }
+  }
+}

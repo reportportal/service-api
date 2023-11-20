60f3a16ff1fc046bfcf30b9f6e7a5c71e45626f1
Pavel_Bortnik <pavel_bortnik@epam.com>
2023-11-20 16:07:34 +0300

    Consistent Hash || Add more tests

---
+++ src/test/java/com/epam/ta/reportportal/reporting/async/producer/ItemStartProducerTest.java
@@ -0,0 +1,48 @@
+package com.epam.ta.reportportal.reporting.async.producer;
+
+import static com.epam.ta.reportportal.ReportPortalUserUtil.getRpUser;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+import com.epam.ta.reportportal.commons.ReportPortalUser;
+import com.epam.ta.reportportal.entity.project.ProjectRole;
+import com.epam.ta.reportportal.entity.user.UserRole;
+import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.core.AmqpTemplate;
+
+@ExtendWith(MockitoExtension.class)
+class ItemStartProducerTest {
+
+  @Mock
+  AmqpTemplate amqpTemplate;
+
+  @InjectMocks
+  ItemStartProducer itemStartProducer;
+
+  @Test
+  void startRootItem() {
+    StartTestItemRQ request = new StartTestItemRQ();
+    ReportPortalUser user = getRpUser("test", UserRole.ADMINISTRATOR, ProjectRole.PROJECT_MANAGER,
+        1L);
+
+    itemStartProducer.startRootItem(user, user.getProjectDetails().get("test_project"),
+        request);
+    verify(amqpTemplate).convertAndSend(any(), any(), any(), any());
+  }
+
+  @Test
+  void startChildItem() {
+    StartTestItemRQ request = new StartTestItemRQ();
+    ReportPortalUser user = getRpUser("test", UserRole.ADMINISTRATOR, ProjectRole.PROJECT_MANAGER,
+        1L);
+
+    itemStartProducer.startChildItem(user, user.getProjectDetails().get("test_project"),
+        request, "123");
+    verify(amqpTemplate).convertAndSend(any(), any(), any(), any());
+  }
+}

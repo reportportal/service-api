437b086df09c0bbcbb9ab43b84909705e7bde353
siarhei_hrabko <siarhei_hrabko@epam.com>
2024-03-05 09:06:02 +0300

    EPMRPP-89396 || fix failing tests

--- src/test/java/com/epam/ta/reportportal/ws/controller/ProjectControllerTest.java
+++ src/test/java/com/epam/ta/reportportal/ws/controller/ProjectControllerTest.java
@@ -19,6 +19,7 @@ package com.epam.ta.reportportal.ws.controller;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.eq;

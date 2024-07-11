65d9763fb201bbb9fe81c64d426775f2c77a1786
siarhei_hrabko <siarhei_hrabko@epam.com>
2024-07-11 16:39:26 +0300

    EPMRPP-92350 fix organization projects for member

--- src/main/java/com/epam/ta/reportportal/core/project/impl/OrganizationProjectHandlerImpl.java
+++ src/main/java/com/epam/ta/reportportal/core/project/impl/OrganizationProjectHandlerImpl.java
@@ -16,23 +16,21 @@
 
 package com.epam.ta.reportportal.core.project.impl;
 
+import static com.epam.ta.reportportal.commons.querygen.constant.GeneralCriteriaConstant.CRITERIA_PROJECT_ID;
 import static com.epam.ta.reportportal.util.OffsetUtils.withOffsetData;
 
 import com.epam.reportportal.api.model.OrganizationProjectsPage;
 import com.epam.reportportal.api.model.ProjectProfile;
-import com.epam.reportportal.rules.exception.ErrorType;
-import com.epam.reportportal.rules.exception.ReportPortalException;
 import com.epam.ta.reportportal.commons.ReportPortalUser;
 import com.epam.ta.reportportal.commons.querygen.Condition;
 import com.epam.ta.reportportal.commons.querygen.Filter;
 import com.epam.ta.reportportal.commons.querygen.FilterCondition;
 import com.epam.ta.reportportal.core.project.OrganizationProjectHandler;
-import com.epam.ta.reportportal.dao.organization.OrganizationUserRepository;
+import com.epam.ta.reportportal.dao.ProjectUserRepository;
 import com.epam.ta.reportportal.dao.project.OrganizationProjectRepository;
 import com.epam.ta.reportportal.entity.organization.OrganizationRole;
 import com.epam.ta.reportportal.entity.user.UserRole;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -40,23 +38,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrganizationProjectHandlerImpl implements OrganizationProjectHandler {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(
-      OrganizationProjectHandlerImpl.class);
-
-  private final OrganizationUserRepository organizationUserRepository;
   private final OrganizationProjectRepository organizationProjectRepository;
+  private final ProjectUserRepository projectUserRepository;
 
-  public OrganizationProjectHandlerImpl(OrganizationUserRepository organizationUserRepository,
-      OrganizationProjectRepository organizationProjectRepository) {
-    this.organizationUserRepository = organizationUserRepository;
+  public OrganizationProjectHandlerImpl(OrganizationProjectRepository organizationProjectRepository,
+      ProjectUserRepository projectUserRepository) {
     this.organizationProjectRepository = organizationProjectRepository;
+    this.projectUserRepository = projectUserRepository;
   }
 
+  /**
+   * This method returns a page of projects for a particular organization based on the provided
+   * filter and pagination details.
+   *
+   * @param user     the {@link ReportPortalUser} whose details are used to apply the additional
+   *                 filters
+   * @param orgId    the id of the organization whose projects are queried
+   * @param filter   the {@link Filter} with condition(s) to be applied on the project querying
+   * @param pageable the {@link Pageable} to define the pagination details for the result
+   * @return an {@link OrganizationProjectsPage} representing a page of projects for the provided
+   * organization
+   */
   @Override
   public OrganizationProjectsPage getOrganizationProjectsList(ReportPortalUser user, Long orgId,
-      Filter filter,
-      Pageable pageable) {
-    Page<ProjectProfile> projectProfileList = getProjectProfileList(user, orgId, filter, pageable);
+      Filter filter, Pageable pageable) {
+    addOrganizationMemberFilter(user, filter, orgId);
+    Page<ProjectProfile> projectProfileList = organizationProjectRepository.getProjectProfileListByFilter(
+        filter, pageable);
 
     OrganizationProjectsPage organizationProjectsPage =
         new OrganizationProjectsPage()
@@ -65,27 +73,28 @@ public class OrganizationProjectHandlerImpl implements OrganizationProjectHandle
     return withOffsetData(organizationProjectsPage, projectProfileList);
   }
 
-  Page<ProjectProfile> getProjectProfileList(ReportPortalUser user, Long orgId, Filter filter,
-      Pageable pageable) {
 
-    if (!user.getUserRole().equals(UserRole.ADMINISTRATOR)) {
-      LOGGER.debug("Checking if the user '{}' is affiliated with organization '{}'",
-          user.getUserId(),
-          orgId);
+  /**
+   * This method modifies the given filter based on the user's role
+   * <p>
+   * If the user is NOT an ADMINISTRATOR and their role in the organization is MEMBER, the filter is
+   * modified to add a condition that filters by projects that the user is part of.
+   *
+   * @param user   the {@link ReportPortalUser} logged-in user info
+   * @param filter the {@link Filter} that is to be modified
+   * @param orgId  the id of the organization that the user is part of
+   */
+  private void addOrganizationMemberFilter(ReportPortalUser user, Filter filter, Long orgId) {
+    if (!user.getUserRole().equals(UserRole.ADMINISTRATOR)
+        && user.getOrganizationDetails().get(orgId.toString()).getOrgRole()
+        .equals(OrganizationRole.MEMBER)) {
 
-      organizationUserRepository.findByUserIdAndOrganization_Id(user.getUserId(), orgId)
-          .map(orgUser -> {
-            if (orgUser.getOrganizationRole().equals(OrganizationRole.MEMBER)) {
-              filter.withCondition(
-                  new FilterCondition(Condition.EQUALS, false, user.getUserId().toString(),
-                      "user_id"));
-            }
-            return filter;
-          })
-          .orElseThrow(() -> new ReportPortalException(ErrorType.ACCESS_DENIED,
-              "The user does not belong to the organization."));
+      var projectIds = projectUserRepository.findProjectIdsByUserId(orgId)
+          .stream()
+          .map(Object::toString)
+          .collect(Collectors.joining(","));
+      filter.withCondition(
+          new FilterCondition(Condition.IN, false, projectIds, CRITERIA_PROJECT_ID));
     }
-    return organizationProjectRepository.getProjectProfileListByFilter(filter, pageable);
-
   }
 }

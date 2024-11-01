3190bc48c8607dd5660be3a6bbba5e48bca531a8
APiankouski <109206864+APiankouski@users.noreply.github.com>
2024-11-01 12:44:03 +0300

    EPMRPP-95621 || Pattern-Analysis doesn't work on Launch finish when AA finished working

--- src/main/java/com/epam/ta/reportportal/core/events/subscriber/impl/delegate/ProjectConfigDelegatingSubscriber.java
+++ src/main/java/com/epam/ta/reportportal/core/events/subscriber/impl/delegate/ProjectConfigDelegatingSubscriber.java
@@ -20,14 +20,19 @@ import com.epam.ta.reportportal.core.events.ProjectIdAwareEvent;
 import com.epam.ta.reportportal.core.events.handler.ConfigurableEventHandler;
 import com.epam.ta.reportportal.core.events.subscriber.EventSubscriber;
 import com.epam.ta.reportportal.core.project.config.ProjectConfigProvider;
+import com.epam.ta.reportportal.ws.converter.resource.handler.attribute.launch.LaunchResourceAttributeLogger;
 import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
  */
 public class ProjectConfigDelegatingSubscriber<T extends ProjectIdAwareEvent> implements
     EventSubscriber<T> {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ProjectConfigDelegatingSubscriber.class);
 
   private final ProjectConfigProvider projectConfigProvider;
   private final List<ConfigurableEventHandler<T, Map<String, String>>> eventHandlers;
@@ -41,6 +46,12 @@ public class ProjectConfigDelegatingSubscriber<T extends ProjectIdAwareEvent> im
   @Override
   public void handleEvent(T event) {
     final Map<String, String> projectConfig = projectConfigProvider.provide(event.getProjectId());
-    eventHandlers.forEach(h -> h.handle(event, projectConfig));
+    eventHandlers.forEach(h -> {
+      try {
+        h.handle(event, projectConfig);
+      } catch (Exception e) {
+        LOGGER.debug("Error while processing event: " + e.getMessage());
+      }
+    });
   }
 }

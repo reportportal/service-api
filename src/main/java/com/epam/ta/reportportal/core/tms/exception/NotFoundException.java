4c929c733779b7120435c5b661e83c0d6a826952
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-02-18 17:00:08 +0300

    Initial TMS migration

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/exception/NotFoundException.java
@@ -0,0 +1,14 @@
+package com.epam.ta.reportportal.core.tms.exception;
+
+import java.text.MessageFormat;
+import java.util.function.Supplier;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(final String templateMessage, final Object... params) {
+        super(MessageFormat.format(templateMessage, params));
+    }
+    
+    public static Supplier<NotFoundException> supplier(final String templateMessage, final Object... params) {
+        return () -> new NotFoundException(templateMessage, params);
+    }
+}

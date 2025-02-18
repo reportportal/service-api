4f0764bbc84384af779993d57ef362f6a021beb8
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-02-18 17:00:08 +0300

    Initial TMS migration

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/service/ProductVersionService.java
@@ -0,0 +1,7 @@
+package com.epam.ta.reportportal.core.tms.service;
+
+import com.epam.ta.reportportal.core.tms.dto.ProductVersionRQ;
+import com.epam.ta.reportportal.core.tms.dto.ProductVersionRS;
+
+public interface ProductVersionService extends CrudService<ProductVersionRQ, ProductVersionRS, Long> {
+}

7f61a97ff7994379e565353818d654ead2a22bf5
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-02-18 17:00:08 +0300

    Initial TMS migration

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/db/entity/TmsAttribute.java
@@ -0,0 +1,34 @@
+package com.epam.ta.reportportal.core.tms.db.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tms_attribute", schema = "public")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TmsAttribute implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    
+    // TODO: Do we actually need to mark this field unique=true and nullable=false ?
+    @Column(name = "key", nullable = false, unique = true)
+    private String key;
+    
+    //TODO: override equals and hashCode methods
+}

8439693a8c742b8963c740d2a85c89422531ec2b
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-02-18 17:00:08 +0300

    Initial TMS migration

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/db/entity/TmsManualScenarioAttributeId.java
@@ -0,0 +1,37 @@
+package com.epam.ta.reportportal.core.tms.db.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TmsManualScenarioAttributeId implements Serializable {
+    
+    @Column(name = "manual_scenario_id")
+    private Long manualScenarioId;
+    
+    @Column(name = "attribute_id")
+    private Long attributeId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        TmsManualScenarioAttributeId that = (TmsManualScenarioAttributeId) o;
+        return Objects.equals(manualScenarioId, that.manualScenarioId) && Objects.equals(attributeId, that.attributeId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(manualScenarioId, attributeId);
+    }
+}

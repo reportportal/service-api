ba0c9bc7b0e3cd11e2236731a0957700a40a11b3
Reingold Shekhtel <raikbitters@gmail.com>
2025-02-21 18:02:45 +0100

    Implement GroupsApi interface in GroupController and add createGroup method stub

--- src/main/java/com/epam/ta/reportportal/ws/controller/GroupController.java
+++ src/main/java/com/epam/ta/reportportal/ws/controller/GroupController.java
@@ -1,5 +1,21 @@
 package com.epam.ta.reportportal.ws.controller;
 
-public class GroupController {
+import com.epam.reportportal.api.GroupsApi;
+import com.epam.reportportal.api.model.CreateGroupRequest;
+import com.epam.reportportal.api.model.Group;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Reing
+ */
+@RestController
+public class GroupController implements GroupsApi {
+
+  @Override
+  public ResponseEntity<Group> createGroup(CreateGroupRequest createGroupRequest) {
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
 
 }

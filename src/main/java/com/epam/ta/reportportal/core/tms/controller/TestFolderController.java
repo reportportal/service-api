d469c8f74ff0e17b9675758e1ba20c27234e61c4
Konstantin_Shaplyko <konstantin_shaplyko@epam.com>
2025-02-24 16:14:24 +0300

    add permissios to tms controllers

--- src/main/java/com/epam/ta/reportportal/core/tms/controller/TestFolderController.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/controller/TestFolderController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static com.epam.ta.reportportal.auth.permissions.Permissions.ASSIGNED_TO_PROJECT;
+
 @RestController
 @RequestMapping("/project/{projectId}/tms/folder")
 @Tag(name = "Test Folder", description = "Test Folder API collection")
@@ -24,14 +27,16 @@ import java.util.List;
 public class TestFolderController {
     
     private final TestFolderService testFolderService;
-    
+
+    @PreAuthorize(ASSIGNED_TO_PROJECT)
     @PostMapping
     @Operation(summary = "Create Test Folder")
     TestFolderRS createTestFolder(@PathVariable("projectId") final long projectId,
                                   @RequestBody final TestFolderRQ inputDto) {
         return testFolderService.createFolder(projectId,inputDto);
     }
-    
+
+    @PreAuthorize(ASSIGNED_TO_PROJECT)
     @PutMapping("/{folderId}")
     @Operation(summary = "Update Test Folder")
     TestFolderRS updateTestFolder(@PathVariable("projectId") final long projectId,
@@ -39,13 +44,16 @@ public class TestFolderController {
                                   @RequestBody final TestFolderRQ inputDto) {
         return testFolderService.updateFolder(projectId, folderId, inputDto);
     }
-    
+
+    @PreAuthorize(ASSIGNED_TO_PROJECT)
     @GetMapping("/{folderId}")
     @Operation(summary = "Get Test Folder by ID")
     TestFolderRS getTestFolderById(@PathVariable("projectId") final long projectId,
                                    @PathVariable("folderId") final long folderId) {
         return testFolderService.getFolderById(folderId);
     }
+
+    @PreAuthorize(ASSIGNED_TO_PROJECT)
     @GetMapping("/")
     @Operation(summary = "Get Test Folders by project ID")
     List<TestFolderRS> getTestFolderByProjectId(@PathVariable("projectId") final long projectId) {

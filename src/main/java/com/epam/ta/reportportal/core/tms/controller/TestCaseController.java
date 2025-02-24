8f245847be183da3017e0472c6c3b97fbd6cc1e3
Konstantin_Shaplyko <konstantin_shaplyko@epam.com>
2025-02-24 16:14:24 +0300

    Integration and Unit Tests for existing Controllers

--- src/main/java/com/epam/ta/reportportal/core/tms/controller/TestCaseController.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/controller/TestCaseController.java
@@ -1,12 +1,14 @@
 package com.epam.ta.reportportal.core.tms.controller;
 
+import static com.epam.ta.reportportal.auth.permissions.Permissions.ADMIN_ONLY;
+
 import com.epam.ta.reportportal.core.tms.dto.TestCaseRQ;
 import com.epam.ta.reportportal.core.tms.dto.TestCaseRS;
 import com.epam.ta.reportportal.core.tms.service.TestCaseService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,29 +19,33 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TestCaseController {
 
-    private final TestCaseService testCaseService;
+  private final TestCaseService testCaseService;
 
-    @GetMapping("/{testCaseId}")
-    TestCaseRS getTestCaseById(@PathVariable("projectId") final long projectId,
-                               @PathVariable("testCaseId") final long testCaseId) {
-        return testCaseService.getTestCaseById(projectId,testCaseId);
-    }
-    @GetMapping("/")
-    List<TestCaseRS> getTestCaseByProjectId(@PathVariable("projectId") final long projectId) {
-        return testCaseService.getTestCaseByProjectId(projectId);
-    }
+  @PreAuthorize(ADMIN_ONLY)
+  @GetMapping("/{testCaseId}")
+  TestCaseRS getTestCaseById(@PathVariable("projectId") final long projectId,
+                             @PathVariable("testCaseId") final long testCaseId) {
+    return testCaseService.getTestCaseById(projectId, testCaseId);
+  }
 
-    @PostMapping
-    TestCaseRS createTestCase(@PathVariable("projectId") final long projectId,
-                              @RequestBody @Valid final TestCaseRQ inputDto) {
-        return testCaseService.createTestCase(inputDto);
-    }
+  @PreAuthorize(ADMIN_ONLY)
+  @GetMapping("/")
+  List<TestCaseRS> getTestCaseByProjectId(@PathVariable("projectId") final long projectId) {
+    return testCaseService.getTestCaseByProjectId(projectId);
+  }
 
-    @PutMapping("/{testCaseId}")
-    TestCaseRS updateTestCase(@PathVariable("projectId") final long projectId,
-                              @PathVariable("testCaseId") final long testCaseId,
-                              @RequestBody final TestCaseRQ inputDto) {
-        return testCaseService.updateTestCase(testCaseId, inputDto);
-    }
+  @PreAuthorize(ADMIN_ONLY)
+  @PostMapping
+  TestCaseRS createTestCase(@PathVariable("projectId") final long projectId,
+                            @RequestBody @Valid final TestCaseRQ inputDto) {
+    return testCaseService.createTestCase(inputDto);
+  }
 
+  @PreAuthorize(ADMIN_ONLY)
+  @PutMapping("/{testCaseId}")
+  TestCaseRS updateTestCase(@PathVariable("projectId") final long projectId,
+                            @PathVariable("testCaseId") final long testCaseId,
+                            @RequestBody final TestCaseRQ inputDto) {
+    return testCaseService.updateTestCase(testCaseId, inputDto);
+  }
 }

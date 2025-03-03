31bb61bcb0b78ee5041eb6694159d9294472b2d2
Pavel_Bortnik <pavel_bortnik@epam.com>
2025-03-03 12:41:58 +0300

    EPMRPP-98912 || Update widget validations

--- src/main/java/com/epam/ta/reportportal/core/configs/WidgetConfig.java
+++ src/main/java/com/epam/ta/reportportal/core/configs/WidgetConfig.java
@@ -28,6 +28,7 @@ import com.epam.ta.reportportal.core.widget.content.loader.BugTrendChartContentL
 import com.epam.ta.reportportal.core.widget.content.loader.CasesTrendContentLoader;
 import com.epam.ta.reportportal.core.widget.content.loader.ChartInvestigatedContentLoader;
 import com.epam.ta.reportportal.core.widget.content.loader.ComponentHealthCheckContentLoader;
+import com.epam.ta.reportportal.core.widget.content.loader.EmptyContentLoader;
 import com.epam.ta.reportportal.core.widget.content.loader.FlakyCasesTableContentLoader;
 import com.epam.ta.reportportal.core.widget.content.loader.LaunchExecutionAndIssueStatisticsContentLoader;
 import com.epam.ta.reportportal.core.widget.content.loader.LaunchesComparisonContentLoader;
@@ -125,6 +126,7 @@ public class WidgetConfig implements ApplicationContextAware {
             applicationContext.getBean(ProductStatusContentLoaderManager.class))
         .put(WidgetType.MOST_TIME_CONSUMING,
             applicationContext.getBean(MostTimeConsumingContentLoader.class))
+        .put(WidgetType.TEST_CASE_SEARCH, applicationContext.getBean(EmptyContentLoader.class))
         .build();
   }
 
@@ -194,7 +196,7 @@ public class WidgetConfig implements ApplicationContextAware {
         )
         .put(WidgetType.COMPONENT_HEALTH_CHECK_TABLE,
             (GeneralLaunchFilterStrategy) applicationContext.getBean("generalLaunchFilterStrategy")
-        ).put(WidgetType.TOP_TEST_CASES,
+        ).put(WidgetType.TEST_CASE_SEARCH,
             (GeneralLaunchFilterStrategy) applicationContext.getBean("generalLaunchFilterStrategy")
         )
         .build();
@@ -227,6 +229,7 @@ public class WidgetConfig implements ApplicationContextAware {
         .add(WidgetType.PASSING_RATE_PER_LAUNCH)
         .add(WidgetType.MOST_TIME_CONSUMING)
         .add(WidgetType.FLAKY_TEST_CASES)
+        .add(WidgetType.TEST_CASE_SEARCH)
         .build();
   }
 

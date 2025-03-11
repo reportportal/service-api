9fc7d61d1113d6ee679527a51d4703e5ea41c233
Reingold Shekhtel <raikbitters@gmail.com>
2025-03-11 15:20:57 +0100

    Add GeneratedProjectController to handle project group requests and refactor ProjectController

---
+++ src/main/java/com/epam/ta/reportportal/ws/controller/GeneratedProjectController.java
@@ -0,0 +1,46 @@
+package com.epam.ta.reportportal.ws.controller;
+
+import com.epam.reportportal.api.ProjectsApi;
+import com.epam.reportportal.api.model.ProjectGroupsPage;
+import com.epam.ta.reportportal.core.group.GroupExtensionPoint;
+import org.pf4j.PluginManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+/**
+ * Controller for handling project collection-related requests.
+ *
+ * @author <a href="mailto:reingold_shekhtel@epam.com">Reingold Shekhtel</a>
+ **/
+@RestController
+public class GeneratedProjectController implements ProjectsApi {
+
+  private final PluginManager pluginManager;
+
+  @Autowired
+  public GeneratedProjectController(PluginManager pluginManager) {
+    this.pluginManager = pluginManager;
+  }
+
+  @Override
+  public ResponseEntity<ProjectGroupsPage> getGroupsOfProject(
+      Long projectId,
+      Integer offset,
+      Integer limit
+  ) {
+    var extension = pluginManager.getExtensions(GroupExtensionPoint.class)
+        .stream()
+        .findFirst()
+        .orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.PAYMENT_REQUIRED)
+        );
+    return ResponseEntity.ok(extension.getProjectGroups(projectId, offset, limit));
+  }
+
+  public ResponseEntity<ProjectGroupsPage> getProjectGroups(Long projectId, Integer offset, Integer limit) {
+    return getGroupsOfProject(projectId, offset, limit);
+  }
+}

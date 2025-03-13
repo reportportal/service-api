3338d9220d9239399737866e13775fda4cb0c2b9
Konstantin_Shaplyko <konstantin_shaplyko@epam.com>
2025-03-13 15:56:13 +0300

    fix existing integration tests

--- src/test/resources/db/tms/tms-product-version/tms-test-case-fill.sql
+++ src/test/resources/db/tms/tms-product-version/tms-test-case-fill.sql
@@ -1,3 +1,6 @@
+insert into project (id, "name", project_type, creation_date, metadata, allocated_storage)
+values (3, 'test_personal' , 'PERSONAL', '2025-02-17 16:07:59.076', '{"metadata": {"additional_info": ""}}', 0);
+
 insert into tms_test_folder (id, project_id, description, "name")
 values (3, 3, 'description_folder3', 'name_folder3');
 

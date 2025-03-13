64ecbd6d2df9bbe5b9aeb34dcd457fa5f211b745
Konstantin_Shaplyko <konstantin_shaplyko@epam.com>
2025-03-13 15:56:13 +0300

    fix existing integration tests

--- src/test/resources/db/tms/tms-product-version/tms-test-folder-fill.sql
+++ src/test/resources/db/tms/tms-product-version/tms-test-folder-fill.sql
@@ -1,8 +1,14 @@
-insert into tms_test_folder (id, project_id, description, "name")
-values (3, 3, 'description_folder3', 'name_folder3');
+insert into project (id, "name", project_type, creation_date, metadata, allocated_storage)
+values (31, 'test_personal31' , 'PERSONAL', '2025-02-17 16:07:59.076', '{"metadata": {"additional_info": ""}}', 0);
 
 insert into tms_test_folder (id, project_id, description, "name")
-values (4, 4, 'description_folder4', 'name_folder4');
+values (3, 31, 'description_folder3', 'name_folder3');
 
 insert into tms_test_folder (id, project_id, description, "name")
-values (5, 5, 'description_folder5', 'name_folder5');
+values (4, 31, 'description_folder4', 'name_folder4');
+
+insert into project (id, "name", project_type, creation_date, metadata, allocated_storage)
+values (35, 'test_persona35' , 'PERSONAL', '2025-02-17 16:07:59.076', '{"metadata": {"additional_info": ""}}', 0);
+
+insert into tms_test_folder (id, project_id, description, "name")
+values (5, 35, 'description_folder5', 'name_folder5');

68abfbee7606967d1a22d7cc084bc8054de6b320
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-03-20 00:13:56 +0300

    Implementation of tms_test_plan CRUD

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/dto/TmsTestPlanAttributeRS.java
@@ -0,0 +1,12 @@
+package com.epam.ta.reportportal.core.tms.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class TmsTestPlanAttributeRS {
+
+    private String value;
+    private TmsAttributeRS attribute;
+}
---
+++ src/main/java/com/epam/ta/reportportal/core/tms/mapper/TmsEnvironmentMapper.java
@@ -0,0 +1,13 @@
+package com.epam.ta.reportportal.core.tms.mapper;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsEnvironment;
+import com.epam.ta.reportportal.core.tms.mapper.config.CommonMapperConfig;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = CommonMapperConfig.class)
+public abstract class TmsEnvironmentMapper {
+
+    @Mapping(target = "id", source = "tmsEnvironmentId")
+    public abstract TmsEnvironment convertToTmsEnvironment(Long tmsEnvironmentId);
+}

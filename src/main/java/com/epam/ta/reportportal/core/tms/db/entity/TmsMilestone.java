3ac7c660a53aa1f564a5d29fd75a9c13d7298fba
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-03-20 00:13:56 +0300

    Implementation of tms_test_plan CRUD

--- src/main/java/com/epam/ta/reportportal/core/tms/db/entity/TmsMilestone.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/db/entity/TmsMilestone.java
@@ -8,42 +8,41 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Entity
-@Table(name = "tms_environment", schema = "public")
+@Table(name = "tms_milestone", schema = "public")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class TmsMilestone {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    
+
     @Column(name = "name")
     private String name;
-    
+
     @Column(name = "type")
     private String type;
-    
+
     @Column(name = "start_date", columnDefinition = "TIMESTAMP")
     private LocalDateTime startDate;
-    
+
     @Column(name = "end_date", columnDefinition = "TIMESTAMP")
     private LocalDateTime endDate;
-    
+
     @ManyToOne
     @JoinColumn(name = "product_version_id", nullable = false)
     private TmsProductVersion productVersion;
-    
+
     @ManyToOne
     @JoinColumn(name = "test_plan_id")
     private TmsTestPlan testPlan;
---
+++ src/main/java/com/epam/ta/reportportal/core/tms/db/repository/TmsTestPlanRepository.java
@@ -0,0 +1,47 @@
+package com.epam.ta.reportportal.core.tms.db.repository;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsTestPlan;
+import com.epam.ta.reportportal.dao.ReportPortalRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TmsTestPlanRepository extends ReportPortalRepository<TmsTestPlan, Long> {
+
+    @Query("SELECT tp FROM TmsTestPlan tp " +
+        "JOIN FETCH tp.environment e " +
+        "JOIN FETCH tp.productVersion pv " +
+        "LEFT JOIN FETCH tp.milestones ms " +
+        "LEFT JOIN FETCH ms.productVersion mspv " +
+        "LEFT JOIN FETCH tp.attributes atr " +
+        "LEFT JOIN FETCH atr.attribute " +
+        "WHERE tp.project.id = :projectId AND tp.id = :id"
+    )
+    Optional<TmsTestPlan> findByIdAndProjectId(Long id, Long projectId);
+
+    @Modifying
+    void deleteByIdAndProject_Id(Long id, Long projectId);
+
+    @Query("SELECT tp FROM TmsTestPlan tp " +
+        "JOIN FETCH tp.environment e " +
+        "JOIN FETCH tp.productVersion pv " +
+        "LEFT JOIN FETCH tp.milestones ms " +
+        "LEFT JOIN FETCH ms.productVersion mspv " +
+        "LEFT JOIN FETCH tp.attributes atr " +
+        "LEFT JOIN FETCH atr.attribute " +
+        "WHERE tp.project.id = :projectId AND" +
+        "(:environmentIds IS NULL OR tp.environment.id IN :environmentIds) AND " +
+        "(:productVersionIds IS NULL OR tp.productVersion.id IN :productVersionIds)"
+    )
+    Page<TmsTestPlan> findByCriteria(
+        @Param("projectId") Long projectId,
+        @Param("environmentIds") List<Long> environmentIds,
+        @Param("productVersionIds") List<Long> productVersionIds,
+        Pageable pageable);
+}

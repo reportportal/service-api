b88d86d0396eab76134c2dae02499c2f4579cd0e
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-03-20 00:13:56 +0300

    Implementation of tms_test_plan CRUD

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/service/TmsTestPlanAttributeServiceImpl.java
@@ -0,0 +1,66 @@
+package com.epam.ta.reportportal.core.tms.service;
+
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsTestPlan;
+import com.epam.ta.reportportal.core.tms.db.repository.TmsTestPlanAttributeRepository;
+import com.epam.ta.reportportal.core.tms.dto.TmsTestPlanAttributeRQ;
+import com.epam.ta.reportportal.core.tms.mapper.TmsTestPlanAttributeMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TmsTestPlanAttributeServiceImpl implements TmsTestPlanAttributeService {
+
+    private final TmsTestPlanAttributeMapper tmsTestPlanAttributeMapper;
+    private final TmsTestPlanAttributeRepository tmsTestPlanAttributeRepository;
+
+    @Override
+    @Transactional
+    public void createTestPlanAttributes(TmsTestPlan tmsTestPlan,
+        List<TmsTestPlanAttributeRQ> attributes) {
+        if (isEmpty(attributes)) {
+            return;
+        }
+        var tmsTestPlanAttributes = tmsTestPlanAttributeMapper.convertToTmsTestPlanAttributes(
+            attributes);
+        tmsTestPlan.setAttributes(tmsTestPlanAttributes);
+        tmsTestPlanAttributes.forEach(
+            tmsTestPlanAttribute -> tmsTestPlanAttribute.setTestPlan(tmsTestPlan));
+        tmsTestPlanAttributeRepository.saveAll(tmsTestPlanAttributes);
+    }
+
+    @Override
+    @Transactional
+    public void updateTestPlanAttributes(TmsTestPlan existingTestPlan,
+        List<TmsTestPlanAttributeRQ> attributes) {
+        tmsTestPlanAttributeRepository.deleteAllById_TestPlanId(existingTestPlan.getId());
+        if (isEmpty(attributes)) {
+            return;
+        }
+        var tmsTestPlanAttributes = tmsTestPlanAttributeMapper.convertToTmsTestPlanAttributes(
+            attributes);
+        existingTestPlan.setAttributes(tmsTestPlanAttributes);
+        tmsTestPlanAttributes.forEach(
+            tmsTestPlanAttribute -> tmsTestPlanAttribute.setTestPlan(existingTestPlan));
+        tmsTestPlanAttributeRepository.saveAll(tmsTestPlanAttributes);
+    }
+
+    @Override
+    @Transactional
+    public void patchTestPlanAttributes(TmsTestPlan existingTestPlan,
+        List<TmsTestPlanAttributeRQ> attributes) {
+        if (isEmpty(attributes)) {
+            return;
+        }
+        var tmsTestPlanAttributes = tmsTestPlanAttributeMapper.convertToTmsTestPlanAttributes(
+            attributes);
+        existingTestPlan.getAttributes().addAll(tmsTestPlanAttributes);
+        tmsTestPlanAttributes.forEach(
+            tmsTestPlanAttribute -> tmsTestPlanAttribute.setTestPlan(existingTestPlan));
+        tmsTestPlanAttributeRepository.saveAll(tmsTestPlanAttributes);
+    }
+}

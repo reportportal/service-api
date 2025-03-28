297de04c32656871333e744e785f59b7d3121de8
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-03-28 14:33:31 +0300

    Add dataset to environment API

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/mapper/TmsEnvironmentDatasetMapper.java
@@ -0,0 +1,43 @@
+package com.epam.ta.reportportal.core.tms.mapper;
+
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsDataset;
+import com.epam.ta.reportportal.core.tms.db.entity.TmsEnvironment;
+import com.epam.ta.reportportal.core.tms.db.entity.TmsEnvironmentDataset;
+import com.epam.ta.reportportal.core.tms.dto.TmsEnvironmentDatasetRQ;
+import com.epam.ta.reportportal.core.tms.dto.TmsEnvironmentDatasetRS;
+import com.epam.ta.reportportal.core.tms.mapper.config.CommonMapperConfig;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = CommonMapperConfig.class)
+public abstract class TmsEnvironmentDatasetMapper {
+
+  public Set<TmsEnvironmentDataset> convertToEnvironmentDatasets(
+      TmsDataset tmsDataset, Collection<TmsEnvironmentDatasetRQ> environmentDatasetRQs) {
+    if (isEmpty(environmentDatasetRQs)) {
+      return null;
+    }
+    return environmentDatasetRQs
+        .stream()
+        .map(environmentDatasetRQ -> convertToEnvironmentDataset(tmsDataset, environmentDatasetRQ))
+        .collect(Collectors.toSet());
+  }
+
+  @Mapping(target = "dataset", source = "tmsDataset")
+  @Mapping(target = "environment.id", source = "environmentDatasetRQ.environmentId")
+  @Mapping(target = "datasetType", source = "environmentDatasetRQ.datasetType")
+  @Mapping(target = "id.datasetId", source = "tmsDataset.id")
+  @Mapping(target = "id.environmentId", source = "environmentDatasetRQ.environmentId")
+  public abstract TmsEnvironmentDataset convertToEnvironmentDataset(TmsDataset tmsDataset,
+      TmsEnvironmentDatasetRQ environmentDatasetRQ);
+
+  @Mapping(target = "environmentId", source = "id.environmentId")
+  public abstract TmsEnvironmentDatasetRS convertToTmsEnvironmentDatasetRS(TmsEnvironmentDataset tmsEnvironmentDataset);
+}

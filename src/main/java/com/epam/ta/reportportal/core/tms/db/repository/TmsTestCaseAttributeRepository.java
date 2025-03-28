949501861c19b2d11d459ae5675d55e7a04f41f8
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-03-28 13:27:19 +0300

    Adjust Test Case API

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/db/repository/TmsTestCaseAttributeRepository.java
@@ -0,0 +1,12 @@
+package com.epam.ta.reportportal.core.tms.db.repository;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsTestCaseAttribute;
+import com.epam.ta.reportportal.core.tms.db.entity.TmsTestCaseAttributeId;
+import com.epam.ta.reportportal.dao.ReportPortalRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+public interface TmsTestCaseAttributeRepository extends ReportPortalRepository<TmsTestCaseAttribute, TmsTestCaseAttributeId> {
+    @Modifying
+    void deleteAllById_TestCaseId(Long testCaseId);
+
+}
---
+++ src/main/java/com/epam/ta/reportportal/core/tms/dto/TmsTestCaseRQ.java
@@ -0,0 +1,28 @@
+package com.epam.ta.reportportal.core.tms.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TmsTestCaseRQ {
+
+    @NotEmpty
+    private String name;
+
+    private String description;
+
+    @NotNull
+    private Long testFolderId;
+
+    private List<TmsTestCaseAttributeRQ> tags;
+
+    private Long datasetId;
+}

e2a4f0e106669e0df2dab30548f3fdda4a19b1c8
Konstantin_Shaplyko <konstantin_shaplyko@epam.com>
2025-03-28 15:00:30 +0300

    adjust product version API

--- src/main/java/com/epam/ta/reportportal/core/tms/db/repository/ProductVersionRepository.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/db/repository/ProductVersionRepository.java
@@ -2,9 +2,14 @@ package com.epam.ta.reportportal.core.tms.db.repository;
 
 import com.epam.ta.reportportal.core.tms.db.entity.TmsProductVersion;
 import com.epam.ta.reportportal.dao.ReportPortalRepository;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductVersionRepository extends ReportPortalRepository<TmsProductVersion, Long> {
+
+  Optional<TmsProductVersion> findByProjectIdAndId(Long projectId, Long id);
+
+  void deleteByIdAndProjectId(Long id, Long projectId);
     
 }

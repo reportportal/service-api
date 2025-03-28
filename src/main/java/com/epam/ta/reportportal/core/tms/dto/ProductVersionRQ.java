30a11ed4e5ca9b37b36598ba50431538430f82b7
Konstantin_Shaplyko <konstantin_shaplyko@epam.com>
2025-03-28 15:00:30 +0300

    adjust product version API

--- src/main/java/com/epam/ta/reportportal/core/tms/dto/ProductVersionRQ.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/dto/ProductVersionRQ.java
@@ -1,22 +1,15 @@
 package com.epam.ta.reportportal.core.tms.dto;
 
-
-import java.util.Set;
-
-// TODO add DTOs for TestPlan and Milestones
-
 /**
  * Input DTO for milestone
  *
  * @param id
  * @param version
  * @param documentation
- * @param testPlans
- * @param milestones
+ * @param projectId
  *
  * @author Andrei Varabyeu andrei_varabyeu@epam.com
  */
-public record ProductVersionRQ(Long id, String version, String documentation, Set<Long> testPlans,
-                               Set<Long> milestones) {
+public record ProductVersionRQ(Long id, String version, String documentation, Long projectId) {
 
 }

4631bd7e2a0a5129d72402d8e8e098e542c03336
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-03-28 13:27:19 +0300

    Adjust Test Case API

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/dto/TmsTestCaseRS.java
@@ -0,0 +1,26 @@
+package com.epam.ta.reportportal.core.tms.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class TmsTestCaseRS {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Set<TmsTestCaseVersionRS> versions;
+
+    private Set<TmsTestPlanAttributeRS> tags;
+}

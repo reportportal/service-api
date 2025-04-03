1ec9e7849a12ec55a71be4bc311bb3b99f35de5b
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-04-04 00:54:39 +0300

    Feature/orgs changes

--- src/main/java/com/epam/ta/reportportal/core/tms/mapper/TmsTestCaseMapper.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/mapper/TmsTestCaseMapper.java
@@ -10,10 +10,8 @@ import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.control.MappingControl;
 
 @Mapper(config = CommonMapperConfig.class)
 public abstract class TmsTestCaseMapper implements DtoMapper<TmsTestCase, TmsTestCaseRS> {

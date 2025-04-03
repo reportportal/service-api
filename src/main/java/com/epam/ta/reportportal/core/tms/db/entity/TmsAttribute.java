555f1a45f210836668978d0464b06d3ae021e526
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-04-04 00:54:39 +0300

    Feature/orgs changes

--- src/main/java/com/epam/ta/reportportal/core/tms/db/entity/TmsAttribute.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/db/entity/TmsAttribute.java
@@ -6,12 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.io.Serializable;
 
 @Entity
 @Table(name = "tms_attribute", schema = "public")
--- src/test/resources/db/tms/tms-test-case/tms-test-case-fill.sql
+++ src/test/resources/db/tms/tms-test-case/tms-test-case-fill.sql
@@ -1,11 +1,14 @@
-insert into project (id, "name", project_type, creation_date, metadata, allocated_storage)
-values (3, 'test_personal3', 'PERSONAL', '2025-02-17 16:07:59.076', '{"metadata": {"additional_info": ""}}', 0);
+insert into organization (id, name, slug, organization_type)
+values (101, 'test Org', 'org', 'INTERNAL');
+
+insert into project (id, name, organization, organization_id, created_at)
+values (3, 'test_project3', 'org', 101, now());
 
 insert into tms_test_folder (id, "name", description, project_id)
 values (3, 'Test Folder 3', 'Description for test folder 3', 3);
 
-insert into project (id, "name", project_type, creation_date, metadata, allocated_storage)
-values (4, 'test_personal4', 'PERSONAL', '2025-02-17 16:07:59.076', '{"metadata": {"additional_info": ""}}', 0);
+insert into project (id, name, organization, organization_id, created_at)
+values (4, 'test_project4', 'org', 101, now());
 
 insert into tms_test_folder (id, "name", description, project_id)
 values (4, 'Test Folder 4', 'Description for test folder 4', 4);
@@ -13,8 +16,8 @@ values (4, 'Test Folder 4', 'Description for test folder 4', 4);
 insert into tms_test_case (id, "name", description, test_folder_id)
 values (4, 'Test Case 4', 'Description for test case 4', 4);
 
-insert into project (id, "name", project_type, creation_date, metadata, allocated_storage)
-values (5, 'test_personal5', 'PERSONAL', '2025-02-17 16:07:59.076', '{"metadata": {"additional_info": ""}}', 0);
+insert into project (id, name, organization, organization_id, created_at)
+values (5, 'test_project5', 'org', 101, now());
 
 insert into tms_test_folder (id, "name", description, project_id)
 values (5, 'Test Folder 5', 'Description for test folder 5', 5);
@@ -22,8 +25,8 @@ values (5, 'Test Folder 5', 'Description for test folder 5', 5);
 insert into tms_test_case (id, "name", description, test_folder_id)
 values (5, 'Test Case 5', 'Description for test case 5', 5);
 
-insert into project (id, "name", project_type, creation_date, metadata, allocated_storage)
-values (6, 'test_personal6', 'PERSONAL', '2025-02-17 16:07:59.076', '{"metadata": {"additional_info": ""}}', 0);
+insert into project (id, name, organization, organization_id, created_at)
+values (6, 'test_project6', 'org', 101, now());
 
 insert into tms_test_folder (id, "name", description, project_id)
 values (6, 'Test Folder 6', 'Description for test folder 6', 6);

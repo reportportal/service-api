7e22192fcab1f19ac39f83a08ceada61ae668af3
siarhei_hrabko <siarhei_hrabko@epam.com>
2025-04-14 10:35:55 +0300

    EPMRPP-99434 || fix authentication error response

--- src/main/java/com/epam/ta/reportportal/auth/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/epam/ta/reportportal/auth/CustomAuthenticationEntryPoint.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.ta.reportportal.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,6 +32,14 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
   @Autowired
   ObjectMapper objectMapper;
 
+  /**
+   * Handles an authentication failure by sending an HTTP 401 Unauthorized response with a JSON body containing error details.
+   *
+   * @param request       the {@link HttpServletRequest} that resulted in an {@link AuthenticationException}
+   * @param response      the {@link HttpServletResponse} to send the error response
+   * @param authException the exception that caused the authentication failure
+   * @throws IOException if an input or output error occurs while writing the response
+   */
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
       throws IOException {

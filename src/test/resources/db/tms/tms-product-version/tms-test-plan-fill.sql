35634bc1779f55eca6c4b6863587ec6d36c4d052
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-04-16 01:24:54 +0300

    Adjust controllers paths to support new orgs functionality

--- src/test/resources/db/tms/tms-product-version/tms-test-plan-fill.sql
+++ src/test/resources/db/tms/tms-product-version/tms-test-plan-fill.sql
@@ -1,75 +1,59 @@
-insert into organization (id, name, slug, organization_type)
-values (101, 'test Org', 'org', 'INTERNAL');
-
-insert into project (id, name, organization, organization_id, created_at)
-values (3, 'test_project3', 'org', 101, now());
-
 insert into tms_product_version (id, documentation, "version", project_id)
-values (3, 'documentation3', 'version3', 3);
+values (3, 'documentation3', 'version3', 1);
 
 insert into tms_milestone (id, "name", start_date, end_date, "type", product_version_id)
 values (3, 'milestone3', '2025-02-17 16:07:59.076', '2025-02-17 16:07:59.076', 'type3', 3);
 
 insert into tms_environment (id, "name", project_id)
-values (3, 'name3', 3);
+values (3, 'name3', 1);
 
 insert into tms_attribute (id, "key")
 values (3, 'value3');
 
 
-
-insert into project (id, name, organization, organization_id, created_at)
-values (4, 'test_project4', 'org', 101, now());
-
 insert into tms_product_version (id, documentation, "version", project_id)
-values (4, 'documentation4', 'version4', 4);
+values (4, 'documentation4', 'version4', 1);
 
 insert into tms_milestone (id, "name", start_date, end_date, "type", product_version_id)
 values (4, 'milestone4', '2025-02-17 16:07:59.076', '2025-02-17 16:07:59.076', 'type4', 4);
 
 insert into tms_environment (id, "name", project_id)
-values (4, 'name4', 4);
+values (4, 'name4', 1);
 
 insert into tms_attribute (id, "key")
 values (4, 'value4');
 
 insert into tms_test_plan (id, "name", description, project_id, environment_id, product_version_id)
-values (4, 'name4', 'description4', 4, 4, 4);
+values (4, 'name4', 'description4', 1, 4, 4);
 
-
-insert into project (id, name, organization, organization_id, created_at)
-values (5, 'test_project5', 'org', 101, now());
 
 insert into tms_product_version (id, documentation, "version", project_id)
-values (5, 'documentation5', 'version5', 5);
+values (5, 'documentation5', 'version5', 1);
 
 insert into tms_milestone (id, "name", start_date, end_date, "type", product_version_id)
 values (5, 'milestone5', '2025-02-17 16:07:59.076', '2025-02-17 16:07:59.076', 'type4', 5);
 
 insert into tms_environment (id, "name", project_id)
-values (5, 'name5', 5);
+values (5, 'name5', 1);
 
 insert into tms_attribute (id, "key")
 values (5, 'value5');
 
 insert into tms_test_plan (id, "name", description, project_id, environment_id, product_version_id)
-values (5, 'name5', 'description5', 5, 5, 5);
+values (5, 'name5', 'description5', 1, 5, 5);
 
-
-insert into project (id, name, organization, organization_id, created_at)
-values (6, 'test_project6', 'org', 101, now());
 
 insert into tms_product_version (id, documentation, "version", project_id)
-values (6, 'documentation6', 'version6', 6);
+values (6, 'documentation6', 'version6', 1);
 
 insert into tms_milestone (id, "name", start_date, end_date, "type", product_version_id)
 values (6, 'milestone6', '2025-02-17 16:07:59.076', '2025-02-17 16:07:59.076', 'type6', 6);
 
 insert into tms_environment (id, "name", project_id)
-values (6, 'name6', 6);
+values (6, 'name6', 1);
 
 insert into tms_attribute (id, "key")
 values (6, 'value6');
 
 insert into tms_test_plan (id, "name", description, project_id, environment_id, product_version_id)
-values (6, 'name6', 'description6', 6, 6, 6);
+values (6, 'name6', 'description6', 1, 6, 6);
--- src/test/resources/db/tms/tms-test-case/tms-test-case-fill.sql
+++ src/test/resources/db/tms/tms-test-case/tms-test-case-fill.sql
@@ -1,35 +1,20 @@
-insert into organization (id, name, slug, organization_type)
-values (101, 'test Org', 'org', 'INTERNAL');
-
-insert into project (id, name, organization, organization_id, created_at)
-values (3, 'test_project3', 'org', 101, now());
+insert into tms_test_folder (id, "name", description, project_id)
+values (3, 'Test Folder 3', 'Description for test folder 3', 1);
 
 insert into tms_test_folder (id, "name", description, project_id)
-values (3, 'Test Folder 3', 'Description for test folder 3', 3);
-
-insert into project (id, name, organization, organization_id, created_at)
-values (4, 'test_project4', 'org', 101, now());
-
-insert into tms_test_folder (id, "name", description, project_id)
-values (4, 'Test Folder 4', 'Description for test folder 4', 4);
+values (4, 'Test Folder 4', 'Description for test folder 4', 1);
 
 insert into tms_test_case (id, "name", description, test_folder_id)
 values (4, 'Test Case 4', 'Description for test case 4', 4);
 
-insert into project (id, name, organization, organization_id, created_at)
-values (5, 'test_project5', 'org', 101, now());
-
 insert into tms_test_folder (id, "name", description, project_id)
-values (5, 'Test Folder 5', 'Description for test folder 5', 5);
+values (5, 'Test Folder 5', 'Description for test folder 5', 1);
 
 insert into tms_test_case (id, "name", description, test_folder_id)
 values (5, 'Test Case 5', 'Description for test case 5', 5);
 
-insert into project (id, name, organization, organization_id, created_at)
-values (6, 'test_project6', 'org', 101, now());
-
 insert into tms_test_folder (id, "name", description, project_id)
-values (6, 'Test Folder 6', 'Description for test folder 6', 6);
+values (6, 'Test Folder 6', 'Description for test folder 6', 1);
 
 insert into tms_test_case (id, "name", description, test_folder_id)
 values (6, 'Test Case 6', 'Description for test case 6', 6);

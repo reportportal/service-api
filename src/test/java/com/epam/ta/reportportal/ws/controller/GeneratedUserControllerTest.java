3040a91ac3c9013ea5de5763f9e42a1efded0ea9
Pavel_Bortnik <pavel_bortnik@epam.com>
2025-05-21 20:10:58 +0300

    EPMRPP-96639 || Fix tests

--- src/test/java/com/epam/ta/reportportal/ws/controller/GeneratedUserControllerTest.java
+++ src/test/java/com/epam/ta/reportportal/ws/controller/GeneratedUserControllerTest.java
@@ -1,8 +1,6 @@
 package com.epam.ta.reportportal.ws.controller;
 
-import static com.epam.ta.reportportal.commons.EntityUtils.normalizeId;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -14,13 +12,11 @@ import com.epam.reportportal.api.model.NewUserRequest;
 import com.epam.reportportal.api.model.NewUserRequest.AccountTypeEnum;
 import com.epam.ta.reportportal.dao.UserRepository;
 import com.epam.ta.reportportal.entity.user.UserType;
-import com.epam.ta.reportportal.model.user.CreateUserRS;
 import com.epam.ta.reportportal.ws.BaseMvcTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.web.servlet.MvcResult;
 
 @Sql("/db/user/user-fill.sql")
 public class GeneratedUserControllerTest extends BaseMvcTest {
@@ -39,18 +35,12 @@ public class GeneratedUserControllerTest extends BaseMvcTest {
     rq.setActive(true);
     rq.setAccountType(AccountTypeEnum.SCIM);
 
-    MvcResult mvcResult = mockMvc.perform(
-            post("/users").with(token(oAuthHelper.getSuperadminToken()))
-                .contentType(APPLICATION_JSON)
-                .content(objectMapper.writeValueAsBytes(rq))).andExpect(status().isOk())
+    mockMvc.perform(post("/users").with(token(oAuthHelper.getSuperadminToken()))
+            .contentType(APPLICATION_JSON)
+            .content(objectMapper.writeValueAsBytes(rq))).andExpect(status().isOk())
         .andReturn();
 
-    CreateUserRS createUserRS = objectMapper.readValue(mvcResult.getResponse().getContentAsString(),
-        CreateUserRS.class);
-
-    assertNotNull(createUserRS.getId());
-    assertEquals(normalizeId(rq.getEmail()), createUserRS.getLogin());
-    var user = userRepository.findById(createUserRS.getId());
+    var user = userRepository.findByEmail(rq.getEmail());
     assertTrue(user.isPresent());
     assertEquals(UserType.SCIM, user.get().getUserType());
     assertNull(user.get().getPassword());

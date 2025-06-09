a72da26bbb77fd615b6d11bbce76432f8aba8f40
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-06-09 11:28:36 +0300

    Create Tms Test Case Controller Stubs according to API design

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/dto/TmsStepsManualScenarioRQ.java
@@ -0,0 +1,33 @@
+package com.epam.ta.reportportal.core.tms.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+/**
+ * Request DTO for step-based manual scenario operations.
+ * Contains information about steps of the manual scenario.
+ */
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TmsStepsManualScenarioRQ extends TmsManualScenarioRQ {
+
+  /**
+   * Steps of the manual scenario.
+   * Must not be empty.
+   */
+  @Valid
+  @NotEmpty(message = "Steps must not be empty")
+  private List<TmsManualScenarioStepRQ> steps;
+}

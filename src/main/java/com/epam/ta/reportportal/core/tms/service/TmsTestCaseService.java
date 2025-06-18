ad8cf805d219024d027cb97c462b1eef2a8f2dfb
Hleb <36200539+imashkanov@users.noreply.github.com>
2025-06-18 14:50:24 +0300

    TmsTestCase: Batch delete and batch patch operations (#2306)

--- src/main/java/com/epam/ta/reportportal/core/tms/service/TmsTestCaseService.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/service/TmsTestCaseService.java
@@ -2,11 +2,20 @@ package com.epam.ta.reportportal.core.tms.service;
 
 import com.epam.ta.reportportal.core.tms.dto.TmsTestCaseRQ;
 import com.epam.ta.reportportal.core.tms.dto.TmsTestCaseRS;
+import com.epam.ta.reportportal.core.tms.dto.batch.BatchDeleteTestCasesRQ;
+import com.epam.ta.reportportal.core.tms.dto.batch.BatchPatchTestCasesRQ;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public interface TmsTestCaseService extends CrudService<TmsTestCaseRQ, TmsTestCaseRS, Long> {
 
   List<TmsTestCaseRS> getTestCaseByProjectId(long projectId);
 
-  void deleteByTestFolderId(Long projectId, Long folderId);
+  void deleteByTestFolderId(long projectId, long folderId);
+
+  void delete(long projectId, @Valid BatchDeleteTestCasesRQ deleteRequest);
+
+  void patch(long projectId, @Valid BatchPatchTestCasesRQ patchRequest);
 }

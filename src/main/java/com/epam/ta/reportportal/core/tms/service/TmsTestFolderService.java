d3591ac64b632a8f180272bfee164f8274fb6c45
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-06-18 17:26:10 +0300

    TmsTestFolder API enhancements

--- src/main/java/com/epam/ta/reportportal/core/tms/service/TmsTestFolderService.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/service/TmsTestFolderService.java
@@ -3,8 +3,8 @@ package com.epam.ta.reportportal.core.tms.service;
 import com.epam.ta.reportportal.core.tms.dto.TmsTestFolderExportFileType;
 import com.epam.ta.reportportal.core.tms.dto.TmsTestFolderRQ;
 import com.epam.ta.reportportal.core.tms.dto.TmsTestFolderRS;
+import com.epam.ta.reportportal.model.Page;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface TmsTestFolderService extends CrudService<TmsTestFolderRQ, TmsTestFolderRS, Long> {

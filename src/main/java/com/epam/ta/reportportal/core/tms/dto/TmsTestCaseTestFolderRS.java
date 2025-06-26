f5c8b6eaa6a1d29a4de7ddab6b5cb7bd4a30ccb0
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-06-26 20:20:54 +0300

    TmsTestCase enhancements

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/dto/TmsTestCaseTestFolderRS.java
@@ -0,0 +1,16 @@
+package com.epam.ta.reportportal.core.tms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TmsTestCaseTestFolderRS {
+
+  private Long id;
+
+}
---
+++ src/main/java/com/epam/ta/reportportal/core/tms/mapper/TmsManualScenarioMapper.java
@@ -0,0 +1,40 @@
+package com.epam.ta.reportportal.core.tms.mapper;
+
+import com.epam.ta.reportportal.core.tms.db.entity.TmsManualScenario;
+import com.epam.ta.reportportal.core.tms.dto.TmsManualScenarioRQ;
+import com.epam.ta.reportportal.core.tms.mapper.config.CommonMapperConfig;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(config = CommonMapperConfig.class)
+public interface TmsManualScenarioMapper {
+
+  @Mapping(target = "executionEstimationTime", source = "executionEstimationTime")
+  @Mapping(target = "linkToRequirements", source = "linkToRequirements")
+  @Mapping(target = "preconditions", source = "preconditions.preconditions")
+  @Mapping(target = "attributes", ignore = true)
+  @Mapping(target = "steps", ignore = true)
+  TmsManualScenario createTmsManualScenario(TmsManualScenarioRQ manualScenarioRQ);
+
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "testCaseVersion", ignore = true)
+  @Mapping(target = "attributes", ignore = true)
+  @Mapping(target = "steps", ignore = true)
+  @BeanMapping(nullValuePropertyMappingStrategy =
+      NullValuePropertyMappingStrategy.SET_TO_NULL,
+      nullValueCheckStrategy = NullValueCheckStrategy.ON_IMPLICIT_CONVERSION
+  )
+  void update(@MappingTarget TmsManualScenario target, TmsManualScenario source);
+
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "testCaseVersion", ignore = true)
+  @Mapping(target = "attributes", ignore = true)
+  @Mapping(target = "steps", ignore = true)
+  @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+      nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+  void patch(@MappingTarget TmsManualScenario target, TmsManualScenario source);
+}

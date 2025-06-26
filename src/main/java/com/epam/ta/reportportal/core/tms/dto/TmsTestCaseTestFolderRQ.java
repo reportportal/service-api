ac3a1b290e64ae6bb7eb88c5578e02511e948a73
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-06-26 20:20:54 +0300

    TmsTestCase enhancements

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/dto/TmsTestCaseTestFolderRQ.java
@@ -0,0 +1,20 @@
+package com.epam.ta.reportportal.core.tms.dto;
+
+import com.epam.ta.reportportal.core.tms.validation.ValidTestFolderIdentifier;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ValidTestFolderIdentifier
+public class TmsTestCaseTestFolderRQ {
+
+  private Long testFolderId;
+
+  private String testFolderName;
+
+}

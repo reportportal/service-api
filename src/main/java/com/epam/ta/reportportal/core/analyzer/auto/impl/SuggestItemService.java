ceb06a520c683ed13de8204dbfd79c18a73defb0
Andrei Piankouski <andrei_piankouski@epam.com>
2025-07-17 12:00:54 +0300

    EPMRPP-104918 || Fix Unauthorized Access to testItem
    (cherry picked from commit eb26fa81376661f38dbaa53e18239e7b2a6ae127)

--- src/main/java/com/epam/ta/reportportal/core/analyzer/auto/impl/SuggestItemService.java
+++ src/main/java/com/epam/ta/reportportal/core/analyzer/auto/impl/SuggestItemService.java
@@ -101,6 +101,7 @@ public class SuggestItemService {
 
     Launch launch = getLaunch(testItem.getLaunchId(), membershipDetails, user);
     isItemUnderProject(membershipDetails, user, launch);
+    isItemUnderProject(projectDetails, user, launch);
     Project project = getProjectHandler.get(membershipDetails);
 
     SuggestRq suggestRq = prepareSuggestRq(testItem, launch, project);

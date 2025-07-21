d1dac9c1a80b9f6861f26c62dcbac9bccb7f7d51
raikbitters <raikbitters@gmail.com>
2025-07-21 15:31:07 +0200

    Refactor comments in MultiIdentityProviderConfig for clarity

--- src/main/java/com/epam/ta/reportportal/core/configs/security/MultiIdentityProviderConfig.java
+++ src/main/java/com/epam/ta/reportportal/core/configs/security/MultiIdentityProviderConfig.java
@@ -91,8 +91,8 @@ public class MultiIdentityProviderConfig {
   }
 
   /**
-   * Configuration properties for identity providers. This class holds the configuration for each JWT issuer, including
-   * the issuer URI, signing key, algorithm, and user details service.
+   * Configuration properties for identity providers. This class holds the configuration for each JWT issuer,
+   * including the issuer URI, signing key, algorithm, and user details service.
    */
   @ConfigurationProperties(prefix = "rp.oauth2")
   @Data

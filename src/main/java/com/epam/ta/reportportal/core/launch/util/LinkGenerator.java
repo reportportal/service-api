e44db03f951deaff7875e10916f2b898c85bc5ff
Pavel Bortnik <pavel_bortnik@epam.com>
2025-07-31 18:01:46 +0300

    5.14.2 || Add logging

--- src/main/java/com/epam/ta/reportportal/core/launch/util/LinkGenerator.java
+++ src/main/java/com/epam/ta/reportportal/core/launch/util/LinkGenerator.java
@@ -19,8 +19,8 @@ package com.epam.ta.reportportal.core.launch.util;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import java.net.URI;
-import java.util.Collections;
-import lombok.SneakyThrows;
+import java.net.URISyntaxException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -31,6 +31,7 @@ import org.springframework.web.util.ForwardedHeaderUtils;
  * @author <a href="mailto:ihar_kahadouski@epam.com">Ihar Kahadouski</a>
  */
 @Component
+@Slf4j
 public final class LinkGenerator {
 
   private static final String UI_PREFIX = "/ui/#";
@@ -41,11 +42,6 @@ public final class LinkGenerator {
   @Value("${server.servlet.context-path:/api}")
   private String pathValue;
 
-  @PostConstruct
-  public void init() {
-    LinkGenerator.path = this.pathValue;
-  }
-
   private LinkGenerator() {
     //static only
   }
@@ -54,17 +50,18 @@ public final class LinkGenerator {
     return StringUtils.isEmpty(baseUrl) ? null : baseUrl + UI_PREFIX + projectName + LAUNCHES + id;
   }
 
-  @SneakyThrows
   public static String composeBaseUrl(HttpServletRequest request) {
 
     String processedPath = "/".equals(path) ? null : path.replace("/api", "");
+    log.info("Processed Path: " + processedPath);
     /*
      * Use Uri components since they are aware of x-forwarded-host headers
      */
 
     HttpHeaders httpHeaders = new HttpHeaders();
     // Only include relevant forwarding headers
-    String[] forwardedHeaders = {"x-forwarded-host", "x-forwarded-proto", "x-forwarded-port", "x-forwarded-for", "forwarded"};
+    String[] forwardedHeaders = {"x-forwarded-host", "x-forwarded-proto", "x-forwarded-port", "x-forwarded-for",
+        "forwarded"};
     for (String headerName : forwardedHeaders) {
       String headerValue = request.getHeader(headerName);
       if (headerValue != null) {
@@ -72,13 +69,27 @@ public final class LinkGenerator {
       }
     }
 
-    URI uri = new URI(request.getRequestURI());
+    URI uri = null;
+    try {
+      uri = new URI(request.getRequestURI());
+    } catch (URISyntaxException e) {
+      log.info(e.getMessage());
+    }
 
-    return ForwardedHeaderUtils.adaptFromForwardedHeaders(uri, httpHeaders)
+    log.info("Uro: " + uri);
+
+    var res = ForwardedHeaderUtils.adaptFromForwardedHeaders(uri, httpHeaders)
         .replacePath(processedPath)
         .replaceQuery(null)
         .build()
         .toUri()
         .toASCIIString();
+    log.info("Compose base url: " + res);
+    return res;
+  }
+
+  @PostConstruct
+  public void init() {
+    LinkGenerator.path = this.pathValue;
   }
 }

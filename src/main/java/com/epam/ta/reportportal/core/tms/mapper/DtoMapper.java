1877960bddc6f53a087815ef4af48b1f238d3b10
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-08-18 23:21:30 +0300

    Add created_at and updated_at to TmsTestCase

--- src/main/java/com/epam/ta/reportportal/core/tms/mapper/DtoMapper.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/mapper/DtoMapper.java
@@ -1,6 +1,12 @@
 package com.epam.ta.reportportal.core.tms.mapper;
 
+import java.time.Instant;
+
 public interface DtoMapper<T, V> {
 
   V convert(T t);
+
+  default Long mapToEpochMilli(Instant instant) {
+    return instant.toEpochMilli();
+  }
 }

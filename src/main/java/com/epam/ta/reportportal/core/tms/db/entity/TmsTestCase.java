febbfd0b07b297cf1996dfcc72e7edba052468a5
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-08-18 23:21:30 +0300

    Add created_at and updated_at to TmsTestCase

--- src/main/java/com/epam/ta/reportportal/core/tms/db/entity/TmsTestCase.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/db/entity/TmsTestCase.java
@@ -1,8 +1,11 @@
 package com.epam.ta.reportportal.core.tms.db.entity;
 
+import com.epam.ta.reportportal.dao.converters.JpaInstantConverter;
 import com.epam.ta.reportportal.entity.item.TestItem;
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,20 +15,22 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "tms_test_case", schema = "public")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -51,6 +56,16 @@ public class TmsTestCase implements Serializable {
 
   @Column(name = "external_id")
   private String externalId;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false)
+  @Convert(converter = JpaInstantConverter.class)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  @Convert(converter = JpaInstantConverter.class)
+  private Instant updatedAt;
 
   @OneToMany(mappedBy = "testCase", fetch = FetchType.LAZY)
   @ToString.Exclude

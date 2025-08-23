4831187f09eb789165d8e801ac2b9ed674e55a3e
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-08-24 00:22:18 +0300

    Enahnce test plans - part 5: add capability of bulk adding and removal of many test cases to\from test plan

---
+++ src/main/java/com/epam/ta/reportportal/core/tms/dto/batch/BatchAddTestCasesToPlanRQ.java
@@ -0,0 +1,18 @@
+package com.epam.ta.reportportal.core.tms.dto.batch;
+
+import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BatchAddTestCasesToPlanRQ {
+
+  @NotEmpty(message = "Test case IDs list cannot be empty")
+  private List<Long> testCaseIds;
+}

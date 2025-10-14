57b34edb5a60de98b89e2a084050448b7ae6d240
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-10-14 19:22:59 +0300

    Add the contract of test plan execution statistic

--- src/main/java/com/epam/ta/reportportal/core/tms/dto/TmsTestPlanRS.java
+++ src/main/java/com/epam/ta/reportportal/core/tms/dto/TmsTestPlanRS.java
@@ -1,5 +1,6 @@
 package com.epam.ta.reportportal.core.tms.dto;
 
+import com.epam.ta.reportportal.entity.tms.TmsTestPlanExecutionStatisticRS;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
 import lombok.Data;
@@ -11,5 +12,6 @@ public class TmsTestPlanRS {
   private Long id;
   private String name;
   private String description;
+  private TmsTestPlanExecutionStatisticRS executionStatistic;
   private List<TmsTestPlanAttributeRS> attributes;
 }

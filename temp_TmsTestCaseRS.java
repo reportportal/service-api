bdc7e7ecd2f4fdd901b091909bfd9435ef7e4877
Hleb <36200539+imashkanov@users.noreply.github.com>
2025-10-19 12:51:11 +0300

    Add lastExecution field to TmsTestCaseRS

---
+++ temp_TmsTestCaseRS.java
@@ -0,0 +1,38 @@
+package com.epam.ta.reportportal.core.tms.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class TmsTestCaseRS {
+
+  private Long id;
+
+  private String name;
+
+  private String description;
+
+  private String priority;
+
+  private Long createdAt;
+
+  private Long updatedAt;
+
+  private Long lastExecutionAt;
+
+  private TmsTestCaseExecutionRS lastExecution;
+
+  private TmsTestCaseTestFolderRS testFolder;
+
+  private TmsManualScenarioRS manualScenario;
+
+  private Set<TmsTestCaseAttributeRS> attributes;
+}

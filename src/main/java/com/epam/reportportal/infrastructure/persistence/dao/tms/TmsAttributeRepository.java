1bca675ffc3a9f5290789671b6115a3e1b116cbb
H <36200539+imashkanov@users.noreply.github.com>
2026-01-19 14:10:27 +0300

    Add findAllByProject_IdAndKeyIn method to TmsAttributeRepository

--- src/main/java/com/epam/reportportal/infrastructure/persistence/dao/tms/TmsAttributeRepository.java
+++ src/main/java/com/epam/reportportal/infrastructure/persistence/dao/tms/TmsAttributeRepository.java
@@ -2,10 +2,14 @@ package com.epam.reportportal.infrastructure.persistence.dao.tms;
 
 import com.epam.reportportal.infrastructure.persistence.dao.ReportPortalRepository;
 import com.epam.reportportal.infrastructure.persistence.entity.tms.TmsAttribute;
+import java.util.List;
+import java.util.Set;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TmsAttributeRepository extends ReportPortalRepository<TmsAttribute, Long> {
 
   boolean existsByKey(String key);
+
+  List<TmsAttribute> findAllByProject_IdAndKeyIn(Long projectId, Set<String> keys);
 }

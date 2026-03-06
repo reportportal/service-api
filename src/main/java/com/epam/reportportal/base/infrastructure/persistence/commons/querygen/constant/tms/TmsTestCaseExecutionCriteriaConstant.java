987a360d9a34cd10d5b14d906d6e2442f796d117
H <36200539+imashkanov@users.noreply.github.com>
2026-03-06 15:19:34 +0300

    Add filtering by testFolderId (suite test item id) to TMS_TEST_CASE_EXECUTION_TARGET

--- src/main/java/com/epam/reportportal/base/infrastructure/persistence/commons/querygen/constant/tms/TmsTestCaseExecutionCriteriaConstant.java
+++ src/main/java/com/epam/reportportal/base/infrastructure/persistence/commons/querygen/constant/tms/TmsTestCaseExecutionCriteriaConstant.java
@@ -19,4 +19,5 @@ public class TmsTestCaseExecutionCriteriaConstant {
   public static final String CRITERIA_TMS_TEST_CASE_EXECUTION_NAME = "name";
   public static final String CRITERIA_TMS_TEST_CASE_EXECUTION_PRIORITY = "testCasePriority";
   public static final String CRITERIA_TMS_TEST_CASE_EXECUTION_TEST_CASE_ATTRIBUTE_KEY = "testCaseAttributeKey";
+  public static final String CRITERIA_TMS_TEST_CASE_EXECUTION_TEST_FOLDER_ID = "testFolderId";
 }

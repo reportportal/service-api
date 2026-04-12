9f17bd5794262bff7dbe9bac7359131c040c6c64
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2026-04-12 19:24:00 +0300

    EPMRPP-114304 || Refactor project user unassignment logic (#2639)

--- src/main/java/com/epam/reportportal/base/core/project/ProjectUserService.java
+++ src/main/java/com/epam/reportportal/base/core/project/ProjectUserService.java
@@ -1,10 +1,13 @@
 package com.epam.reportportal.base.core.project;
 
+import static com.epam.reportportal.base.util.SecurityContextUtils.getPrincipal;
+
 import com.epam.reportportal.base.core.events.domain.UnassignUserEvent;
 import com.epam.reportportal.base.core.user.UserService;
 import com.epam.reportportal.base.infrastructure.persistence.dao.ProjectUserRepository;
 import com.epam.reportportal.base.model.activity.UserActivityResource;
 import com.epam.reportportal.base.ws.converter.converters.UserConverter;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
@@ -31,4 +34,15 @@ public class ProjectUserService {
         });
   }
 
+  public void deleteByUserIdAndProjectIds(Long orgId, Long userId, List<Long> projectIdsToUnassign) {
+    var unassignedUser = userService.findById(userId);
+    projectUserRepository.deleteByUserIdAndProjectIds(userId, projectIdsToUnassign)
+        .forEach(projectId -> {
+          UserActivityResource userActivityResource =
+              UserConverter.TO_ACTIVITY_RESOURCE.apply(unassignedUser, projectId);
+          applicationEventPublisher.publishEvent(
+              new UnassignUserEvent(userActivityResource, getPrincipal().getUserId(), getPrincipal().getUsername(),
+                  orgId));
+        });
+  }
 }

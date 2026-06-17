40e96227a2efb0acc36219fc5168c23e9013b4aa
Evelina02 <evelina02.02.2000@gmail.com>
2026-06-17 15:36:16 +0300

    EPMRPP-115237 add 2 more fields

--- src/main/java/com/epam/reportportal/extension/SensitiveIntegrationParam.java
+++ src/main/java/com/epam/reportportal/extension/SensitiveIntegrationParam.java
@@ -17,11 +17,14 @@
 package com.epam.reportportal.extension;
 
 import java.util.Set;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 /**
  * Canonical names of integration parameters that are encrypted at rest. service-api encrypts values under these keys
  * before persisting. Plugins must decrypt them using BasicTextEncryptor when reading.
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class SensitiveIntegrationParam {
 
   public static final String PASSWORD = "password";
@@ -29,9 +32,9 @@ public final class SensitiveIntegrationParam {
   public static final String API_TOKEN = "apiToken";
   public static final String ACCESS_TOKEN = "accessToken";
   public static final String MANAGER_PASSWORD = "managerPassword";
+  public static final String CLIENT_SECRET = "clientSecret";
+  public static final String PASSWORD_ATTRIBUTE = "passwordAttribute";
 
-  public static final Set<String> ALL = Set.of(PASSWORD, OAUTH_ACCESS_KEY, API_TOKEN, ACCESS_TOKEN, MANAGER_PASSWORD);
-
-  private SensitiveIntegrationParam() {
-  }
+  public static final Set<String> ALL = Set.of(PASSWORD, OAUTH_ACCESS_KEY, API_TOKEN, ACCESS_TOKEN, MANAGER_PASSWORD,
+      CLIENT_SECRET, PASSWORD_ATTRIBUTE);
 }

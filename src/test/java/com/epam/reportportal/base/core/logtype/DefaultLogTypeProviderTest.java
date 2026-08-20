7ef4d2c404f834567432748586c7713ee42419ea
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2026-08-20 21:54:02 +0300

    EPMRPP-118742 || Change colour for mobitru log type provider
    Fix unit test for the change https://github.com/reportportal/service-api/commit/153868f0474206a24752ad9975c5e6eeb5d5022e

--- src/test/java/com/epam/reportportal/base/core/logtype/DefaultLogTypeProviderTest.java
+++ src/test/java/com/epam/reportportal/base/core/logtype/DefaultLogTypeProviderTest.java
@@ -23,7 +23,7 @@ class DefaultLogTypeProviderTest {
 
     // then
     assertThat(logTypes).hasSize(8);
-    assertLogType(logTypes.get(0), projectId, "mobitru", 90000, "#E3E7EC", false);
+    assertLogType(logTypes.get(0), projectId, "mobitru", 90000, "#23A6DE", false);
     assertLogType(logTypes.get(1), projectId, "unknown", 60000, "#E3E7EC", false);
     assertLogType(logTypes.get(2), projectId, "fatal", 50000, "#8B0000", true);
     assertLogType(logTypes.get(3), projectId, "error", 40000, "#DC5959", true);

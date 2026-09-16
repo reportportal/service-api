06f4c01c9d4745a76abe1b481c2491dcd86deb71
UU <36200539+imashkanov@users.noreply.github.com>
2026-09-16 13:14:59 +0300

    EPMRPP-121518: [TMS][UI][BE] No limit to the Execution comment field when changing the test case status from Manual Launches

--- src/main/java/com/epam/reportportal/base/core/tms/dto/TmsTestCaseExecutionCommentRQ.java
+++ src/main/java/com/epam/reportportal/base/core/tms/dto/TmsTestCaseExecutionCommentRQ.java
@@ -1,6 +1,9 @@
 package com.epam.reportportal.base.core.tms.dto;
 
+import static com.epam.reportportal.base.reporting.ValidationConstraints.MAX_TMS_TEST_CASE_EXECUTION_COMMENT_LENGTH;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +17,7 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class TmsTestCaseExecutionCommentRQ {
 
+  @Size(max = MAX_TMS_TEST_CASE_EXECUTION_COMMENT_LENGTH)
   private String comment;
   
   private List<TmsTestCaseExecutionCommentAttachmentRQ> attachments;
